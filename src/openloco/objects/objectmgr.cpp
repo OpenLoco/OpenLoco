4fcc877987a9c3071b36a609d09862ee64356405
Ted John <ted@brambles.org>
2018-01-21 16:38:29 +0000

    Add object types and counts

--- src/openloco/objects/objectmgr.cpp
+++ src/openloco/objects/objectmgr.cpp
@@ -17,4 +17,46 @@ namespace openloco::objectmgr
     {
         return _cargoObjects[id];
     }
+
+    size_t get_max_objects(object_type type)
+    {
+        static size_t counts[] =
+        {
+            1,      // interface,
+            128,    // sound,
+            1,      // currency,
+            32,     // steam,
+            8,      // rock,
+            1,      // water,
+            32,     // surface,
+            1,      // town_name,
+            32,     // cargo,
+            32,     // wall,
+            16,     // train_signal,
+            4,      // level_crossing,
+            1,      // street_light,
+            16,     // tunnel,
+            8,      // bridge,
+            16,     // train_station,
+            8,      // track_extra,
+            8,      // track,
+            16,     // road_station,
+            4,      // road_extra,
+            8,      // road,
+            8,      // airport,
+            8,      // dock,
+            224,    // vehicle,
+            64,     // tree,
+            1,      // snow,
+            1,      // climate,
+            1,      // hill_shapes,
+            128,    // building,
+            1,      // scaffolding,
+            16,     // industry,
+            1,      // region,
+            32,     // competitors,
+            1       // scenario_text,
+        };
+        return counts[(size_t)type];
+    };
 }
--- src/openloco/objects/objectmgr.h
+++ src/openloco/objects/objectmgr.h
@@ -2,6 +2,44 @@
 
 namespace openloco
 {
+    enum class object_type
+    {
+        interface_skin,
+        sound,
+        currency,
+        steam,
+        rock,
+        water,
+        land,
+        town_names,
+        cargo,
+        wall,
+        track_signal,
+        level_crossing,
+        street_light,
+        tunnel,
+        bridge,
+        track_station,
+        track_extra,
+        track,
+        road_station,
+        road_extra,
+        road,
+        airport,
+        dock,
+        vehicle,
+        tree,
+        snow,
+        climate,
+        hill_shapes,
+        building,
+        scaffolding,
+        industry,
+        region,
+        competitors,
+        scenario_text,
+    };
+
     struct cargo_object;
 }
 
@@ -9,4 +47,5 @@ namespace openloco::objectmgr
 {
     void load_index();
     cargo_object * get_cargo_object(size_t id);
+    size_t get_max_objects(object_type type);
 }
