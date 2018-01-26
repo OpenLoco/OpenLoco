f45b11e80e758e36990649b0a214b3f699cd697c
Ted John <ted@brambles.org>
2018-01-26 18:11:25 +0000

    Add extern declarations

--- src/openloco/companymgr.h
+++ src/openloco/companymgr.h
@@ -1,11 +1,13 @@
 #pragma once
 
 #include "company.h"
+#include <array>
 #include <cstddef>
 
 namespace openloco::companymgr
 {
     constexpr size_t max_companies = 15;
 
+    std::array<company, max_companies>& companies();
     company* get(company_id_t id);
 }
--- src/openloco/stationmgr.h
+++ src/openloco/stationmgr.h
@@ -1,12 +1,14 @@
 #pragma once
 
 #include "station.h"
+#include <array>
 #include <cstddef>
 
 namespace openloco::stationmgr
 {
     constexpr size_t max_stations = 1024;
 
+    std::array<station, max_stations>& stations();
     station* get(station_id_t id);
     void sub_48B244();
 }
