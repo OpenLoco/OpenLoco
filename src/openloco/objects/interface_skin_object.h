ac79e28196cd3ff1f5060eed824c8bc4d3ad6fb3
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2018-01-27 18:32:24 +0100

    Update interface skin object definition

--- src/openloco/objects/interface_skin_object.h
+++ src/openloco/objects/interface_skin_object.h
@@ -1,12 +1,17 @@
 #pragma once
 
+#include "../localisation/stringmgr.h"
+
 namespace openloco
 {
+#pragma pack(push, 1)
     struct interface_skin_object
     {
-        uint8_t pad_00[6];
+        string_id name;
+        uint32_t img;
         uint8_t colours_06[2];
         uint8_t pad_08[0x12 - 8];
         uint8_t colours_12[6];
     };
+#pragma pack(pop)
 }
