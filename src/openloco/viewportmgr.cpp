7728eb85d7c8700fa1ae8a6cefa3193143d07591
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2018-01-27 20:56:43 +0100

    Wrap viewport function

--- src/openloco/viewportmgr.cpp
+++ src/openloco/viewportmgr.cpp
@@ -16,4 +16,16 @@ namespace openloco::ui::viewportmgr
         auto arr = (std::array<viewport*, max_viewports>*)_viewports.get();
         return *arr;
     }
+
+    void create(window* window, int16_t x, int16_t y, uint16_t width, uint16_t height)
+    {
+        registers regs;
+        regs.edx = 0x17FF17FF;
+        regs.eax = (y << 16) | x;
+        regs.ebx = (height << 16) | width;
+        regs.ecx = 0x1e00000;
+        regs.esi = (uint32_t)window;
+        addr<0x00e3f0b8, int32_t>() = 0; // gCurrentRotation?
+        call(0x004ca2d0, regs);
+    }
 }
