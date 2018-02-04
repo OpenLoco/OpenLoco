0b9fc6366fbf853cadb35c9536d6fbcb759e219d
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2018-02-04 18:01:31 +0100

    Add constants for window flags

--- src/openloco/gui.cpp
+++ src/openloco/gui.cpp
@@ -34,7 +34,7 @@ namespace openloco::gui
             0,
             uiWidth,
             uiHeight,
-            (1 << 0),
+            ui::window_flags::flag_0,
             (void*)0x004FA1F4);
         window->widgets = _mainWindowWidgets;
         addr<0x00e3f0b8, int32_t>() = 0; // gCurrentRotation?
@@ -57,7 +57,7 @@ namespace openloco::gui
                 uiHeight - 117,
                 296,
                 92,
-                (1 << 1) | (1 << 4) | (1 << 5) | (1 << 6),
+                ui::window_flags::flag_1 | ui::window_flags::flag_4 | ui::window_flags::flag_5 | ui::window_flags::flag_6,
                 (void*)0x004f9ec8);
             window->widgets = (ui::widget*)0x00509df4;
             window->enabled_widgets = (1 << 0) | (1 << 1) | (1 << 2) | (1 << 3) | (1 << 4) | (1 << 5);
@@ -74,7 +74,7 @@ namespace openloco::gui
                 uiHeight - 28,
                 40,
                 28,
-                (1 << 1) | (1 << 4) | (1 << 5) | (1 << 6),
+                ui::window_flags::flag_1 | ui::window_flags::flag_4 | ui::window_flags::flag_5 | ui::window_flags::flag_6,
                 (void*)0x004f9f3c);
             window->widgets = (ui::widget*)0x00509e58;
             window->enabled_widgets = (1 << 0);
@@ -90,14 +90,14 @@ namespace openloco::gui
                 0,
                 298,
                 170,
-                1 << 1,
+                ui::window_flags::flag_1,
                 (void*)0x004f9fb0);
             window->widgets = (ui::widget*)0x00509e6c;
             window->enabled_widgets = (1 << 0);
 
             windowmgr::init_scroll_widgets(window);
 
-            window->var_42 |= 0x10;
+            window->flags |= ui::window_flags::flag_4;
             window->colours[0] = colour::translucent(colour::grey);
             window->colours[1] = colour::translucent(colour::grey);
         }
@@ -110,7 +110,7 @@ namespace openloco::gui
                 0,
                 uiWidth,
                 28,
-                (1 << 1) | (1 << 4) | (1 << 5),
+                ui::window_flags::flag_1 | ui::window_flags::flag_4 | ui::window_flags::flag_5,
                 (void*)0x4fa180);
             window->widgets = (ui::widget*)0x509c34;
             window->enabled_widgets = (1 << 0) | (1 << 1) | (1 << 2) | (1 << 3) | (1 << 4) | (1 << 5) | (1 << 6) | (1 << 7) | (1 << 8) | (1 << 9) | (1 << 10) | (1 << 11) | (1 << 12);
@@ -131,7 +131,7 @@ namespace openloco::gui
                 uiHeight - 27,
                 140,
                 27,
-                (1 << 1) | (1 << 4) | (1 << 5),
+                ui::window_flags::flag_1 | ui::window_flags::flag_4 | ui::window_flags::flag_5,
                 (void*)0x4fa024);
             window->widgets = (ui::widget*)0x509d08;
             window->enabled_widgets = (1 << 2) | (1 << 3) | (1 << 4);
@@ -150,7 +150,7 @@ namespace openloco::gui
                 uiHeight - 27,
                 140,
                 27,
-                (1 << 1) | (1 << 4) | (1 << 5),
+                ui::window_flags::flag_1 | ui::window_flags::flag_4 | ui::window_flags::flag_5,
                 (void*)0x4fa098);
             window->widgets = (ui::widget*)0x509d5c;
             window->enabled_widgets = (1 << 2) | (1 << 3) | (1 << 4) | (1 << 5) | (1 << 6) | (1 << 7);
@@ -173,7 +173,7 @@ namespace openloco::gui
                     uiHeight - 27,
                     uiWidth - 280,
                     27,
-                    (1 << 1) | (1 << 4) | (1 << 5),
+                    ui::window_flags::flag_1 | ui::window_flags::flag_4 | ui::window_flags::flag_5,
                     (void*)0x4fa10c);
                 window->widgets = (ui::widget*)0x509de0;
                 windowmgr::init_scroll_widgets(window);
