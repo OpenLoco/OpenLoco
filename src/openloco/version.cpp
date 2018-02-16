b68e78f543f66b38c82e6517105727bad3ce2fef
Aaron van Geffen <aaron@aaronweb.net>
2018-02-16 13:07:19 +0100

    Add git branch and short sha1 hash to version info line. (#107)

--- src/openloco/version.cpp
+++ src/openloco/version.cpp
@@ -3,10 +3,20 @@
 // clang-format off
 #define NAME            "OpenLoco"
 #define VERSION         "18.02"
-#define VERSION_INFO    NAME " " VERSION
+#define VERSION_INFO    NAME ", v" VERSION
 // clang-format on
 
 namespace openloco
 {
-    const char version[] = VERSION_INFO;
+    const char version[] = VERSION_INFO
+#ifdef OPENLOCO_BRANCH
+        "-" OPENLOCO_BRANCH
+#endif
+#ifdef OPENLOCO_COMMIT_SHA1_SHORT
+        " build " OPENLOCO_COMMIT_SHA1_SHORT
+#endif
+#ifndef NDEBUG
+        " (DEBUG)"
+#endif
+        ;
 }
