f7677fefa5cad1f3becd1e8db2e2831ef53a95fd
Aaron van Geffen <aaron@aaronweb.net>
2018-04-04 19:36:30 +0200

    Return 0 when popping a nullptr args.
    But why is it a nullptr to begin with?

--- src/openloco/localisation/argswrapper.hpp
+++ src/openloco/localisation/argswrapper.hpp
@@ -14,6 +14,9 @@ namespace openloco::stringmgr
 
         uint8_t pop8()
         {
+            if (args == nullptr)
+                return 0;
+
             uint8_t value = *(uint8_t*) args;
             args = (uint8_t*) args + 1;
             return value;
@@ -21,6 +24,9 @@ namespace openloco::stringmgr
 
         uint16_t pop16()
         {
+            if (args == nullptr)
+                return 0;
+
             uint16_t value = *(uint16_t*) args;
             args = (uint16_t*) args + 1;
             return value;
@@ -28,6 +34,9 @@ namespace openloco::stringmgr
 
         uint32_t pop32()
         {
+            if (args == nullptr)
+                return 0;
+
             uint32_t value = *(uint32_t*) args;
             args = (uint32_t*) args + 1;
             return value;
