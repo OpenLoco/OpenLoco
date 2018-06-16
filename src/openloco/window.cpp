b2e1f2983e57c4d6e34733f2ef7ea1cb454b79ef
Aaron van Geffen <aaron@aaronweb.net>
2018-06-16 12:17:20 +0200

    Finish implementing zooming to cursor.
    This implements a rough version of get_map_coordinates_from_pos.
    Will require expansion to meet all use cases.

--- src/openloco/window.cpp
+++ src/openloco/window.cpp
@@ -315,6 +315,20 @@ namespace openloco::ui
         return scrollIndex;
     }
 
+    // 0x00459E54
+    // TODO: needs expansion in terms of (output) parameters.
+    static void get_map_coordinates_from_pos(int32_t screenX, int32_t screenY, int32_t flags, int16_t* x, int16_t* y)
+    {
+        registers regs;
+        regs.ax = screenX;
+        regs.cx = screenY;
+        regs.edx = flags;
+        call(0x00459E54, regs);
+
+        *x = regs.ax;
+        *y = regs.cx;
+    }
+
     void window::viewport_get_map_coords_by_cursor(int16_t* map_x, int16_t* map_y, int16_t* offset_x, int16_t* offset_y)
     {
         // Get mouse position to offset against.
@@ -322,8 +336,7 @@ namespace openloco::ui
         ui::get_cursor_pos(mouse_x, mouse_y);
 
         // Compute map coordinate by mouse position.
-        // TODO
-        // get_map_coordinates_from_pos(mouse_x, mouse_y, VIEWPORT_INTERACTION_MASK_NONE, map_x, map_y, nullptr, nullptr, nullptr);
+        get_map_coordinates_from_pos(mouse_x, mouse_y, 0, map_x, map_y);
 
         // Get viewport coordinates centring around the tile.
         int32_t base_height = map::tile_element_height(*map_x, *map_y);
