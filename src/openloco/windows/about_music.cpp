bba20e31bb006b1444c7b142af86860dd825cf33
Aaron van Geffen <aaron@aaronweb.net>
2018-06-25 20:12:39 +0200

    Music window: iterate over pairs instead; use constants.

--- src/openloco/windows/about_music.cpp
+++ src/openloco/windows/about_music.cpp
@@ -13,6 +13,8 @@ namespace openloco::ui::about_music
     constexpr uint16_t ww = 500;
     constexpr uint16_t wh = 312;
 
+    constexpr uint16_t num_songs = 31;
+
     namespace widx
     {
         enum
@@ -82,7 +84,7 @@ namespace openloco::ui::about_music
 
     static void get_scroll_size(ui::window* window, uint32_t scrollIndex, uint16_t* scrollWidth, uint16_t* scrollHeight)
     {
-        *scrollHeight = 1054;
+        *scrollHeight = num_songs * (10 + 10 + 14);
     }
 
     static void tooltip(ui::window* window, widget_index widgetIndex)
@@ -99,89 +101,58 @@ namespace openloco::ui::about_music
 
     static void draw_scroll(ui::window* window, gfx::drawpixelinfo_t* dpi, uint32_t scrollIndex)
     {
-        static string_id strings_to_draw[] = {
-            string_ids::locomotion_title,
-            string_ids::locomotion_title_credit,
-            string_ids::long_dusty_road,
-            string_ids::long_dusty_road_credit,
-            string_ids::flying_high,
-            string_ids::flying_high_credit,
-            string_ids::gettin_on_the_gas,
-            string_ids::gettin_on_the_gas_credit,
-            string_ids::jumpin_the_rails,
-            string_ids::jumpin_the_rails_credit,
-            string_ids::smooth_running,
-            string_ids::smooth_running_credit,
-            string_ids::traffic_jam,
-            string_ids::traffic_jam_credit,
-            string_ids::never_stop_til_you_get_there,
-            string_ids::never_stop_til_you_get_there_credit,
-            string_ids::soaring_away,
-            string_ids::soaring_away_credit,
-            string_ids::techno_torture,
-            string_ids::techno_torture_credit,
-            string_ids::everlasting_high_rise,
-            string_ids::everlasting_high_rise_credit,
-            string_ids::solace,
-            string_ids::solace_credit,
-            string_ids::chrysanthemum,
-            string_ids::chrysanthemum_credit,
-            string_ids::eugenia,
-            string_ids::eugenia_credit,
-            string_ids::the_ragtime_dance,
-            string_ids::the_ragtime_dance_credit,
-            string_ids::easy_winners,
-            string_ids::easy_winners_credit,
-            string_ids::setting_off,
-            string_ids::setting_off_credit,
-            string_ids::a_travellers_seranade,
-            string_ids::a_travellers_seranade_credit,
-            string_ids::latino_trip,
-            string_ids::latino_trip_credit,
-            string_ids::a_good_head_of_steam,
-            string_ids::a_good_head_of_steam_credit,
-            string_ids::hop_to_the_bop,
-            string_ids::hop_to_the_bop_credit,
-            string_ids::the_city_lights,
-            string_ids::the_city_lights_credit,
-            string_ids::steamin_down_town,
-            string_ids::steamin_down_town_credit,
-            string_ids::bright_expectations,
-            string_ids::bright_expectations_credit,
-            string_ids::mo_station,
-            string_ids::mo_station_credit,
-            string_ids::far_out,
-            string_ids::far_out_credit,
-            string_ids::running_on_time,
-            string_ids::running_on_time_credit,
-            string_ids::get_me_to_gladstone_bay,
-            string_ids::get_me_to_gladstone_bay_credit,
-            string_ids::chuggin_along,
-            string_ids::chuggin_along_credit,
-            string_ids::dont_lose_your_rag,
-            string_ids::dont_lose_your_rag_credit,
-            string_ids::sandy_track_blues,
-            string_ids::sandy_track_blues_credit,
+        static std::pair<string_id, string_id> strings_to_draw[num_songs] = {
+            { string_ids::locomotion_title, string_ids::locomotion_title_credit },
+            { string_ids::long_dusty_road, string_ids::long_dusty_road_credit },
+            { string_ids::flying_high, string_ids::flying_high_credit },
+            { string_ids::gettin_on_the_gas, string_ids::gettin_on_the_gas_credit },
+            { string_ids::jumpin_the_rails, string_ids::jumpin_the_rails_credit },
+            { string_ids::smooth_running, string_ids::smooth_running_credit },
+            { string_ids::traffic_jam, string_ids::traffic_jam_credit },
+            { string_ids::never_stop_til_you_get_there, string_ids::never_stop_til_you_get_there_credit },
+            { string_ids::soaring_away, string_ids::soaring_away_credit },
+            { string_ids::techno_torture, string_ids::techno_torture_credit },
+            { string_ids::everlasting_high_rise, string_ids::everlasting_high_rise_credit },
+            { string_ids::solace, string_ids::solace_credit },
+            { string_ids::chrysanthemum, string_ids::chrysanthemum_credit },
+            { string_ids::eugenia, string_ids::eugenia_credit },
+            { string_ids::the_ragtime_dance, string_ids::the_ragtime_dance_credit },
+            { string_ids::easy_winners, string_ids::easy_winners_credit },
+            { string_ids::setting_off, string_ids::setting_off_credit },
+            { string_ids::a_travellers_seranade, string_ids::a_travellers_seranade_credit },
+            { string_ids::latino_trip, string_ids::latino_trip_credit },
+            { string_ids::a_good_head_of_steam, string_ids::a_good_head_of_steam_credit },
+            { string_ids::hop_to_the_bop, string_ids::hop_to_the_bop_credit },
+            { string_ids::the_city_lights, string_ids::the_city_lights_credit },
+            { string_ids::steamin_down_town, string_ids::steamin_down_town_credit },
+            { string_ids::bright_expectations, string_ids::bright_expectations_credit },
+            { string_ids::mo_station, string_ids::mo_station_credit },
+            { string_ids::far_out, string_ids::far_out_credit },
+            { string_ids::running_on_time, string_ids::running_on_time_credit },
+            { string_ids::get_me_to_gladstone_bay, string_ids::get_me_to_gladstone_bay_credit },
+            { string_ids::chuggin_along, string_ids::chuggin_along_credit },
+            { string_ids::dont_lose_your_rag, string_ids::dont_lose_your_rag_credit },
+            { string_ids::sandy_track_blues, string_ids::sandy_track_blues_credit },
         };
 
         int16_t x = 240;
         int16_t y = 2;
 
-        size_t i = 0;
-        for (string_id credit_string : strings_to_draw)
+        for (auto song_strings : strings_to_draw)
         {
             // TODO: optimisation: don't draw past fold.
 
-            draw_string_centred(*dpi, x, y, colour::black, credit_string, nullptr);
+            // Song name
+            draw_string_centred(*dpi, x, y, colour::black, song_strings.first, nullptr);
             y += 10;
-            i++;
+
+            // Credit line
+            draw_string_centred(*dpi, x, y, colour::black, song_strings.second, nullptr);
+            y += 10;
 
             // Show CS' copyright after every two lines.
-            if (i % 2 == 0)
-            {
-                draw_string_centred(*dpi, x, y, colour::black, string_ids::music_copyright, nullptr);
-                y += 14;
-            }
+            draw_string_centred(*dpi, x, y, colour::black, string_ids::music_copyright, nullptr);
+            y += 14;
         }
     }
 }
