fa1a69f1f9b52cbd81ef91b4d81a5409fad6ebbe
Aaron van Geffen <aaron@aaronweb.net>
2018-06-27 00:09:35 +0200

    Implement widgets and events for title_logo window.

--- src/openloco/windows/title_logo.cpp
+++ src/openloco/windows/title_logo.cpp
@@ -1,4 +1,5 @@
 #include "../graphics/colours.h"
+#include "../graphics/image_ids.h"
 #include "../interop/interop.hpp"
 #include "../openloco.h"
 #include "../windowmgr.h"
@@ -7,13 +8,28 @@ using namespace openloco::interop;
 
 namespace openloco::ui::windows
 {
-    static void paint(ui::window* window, gfx::drawpixelinfo_t* dpi);
+    namespace widx
+    {
+        enum
+        {
+            logo
+        };
+    }
 
-    static loco_global<window_event_list[1], 0x004F9FB0> _events;
+    static widget_t _widgets[] = {
+        make_widget( { 0, 0 }, { 298, 170 }, widget_type::wt_3, 0 ),
+        widget_end(),
+    };
+
+    static window_event_list _events;
+
+    static void on_mouse_up(window* window, widget_index widgetIndex);
+    static void draw(ui::window* window, gfx::drawpixelinfo_t* dpi);
 
     ui::window* open_title_logo()
     {
-        _events[0].draw = paint;
+        _events.on_mouse_up = on_mouse_up;
+        _events.draw = draw;
 
         auto window = openloco::ui::windowmgr::create_window(
             window_type::title_logo,
@@ -21,14 +37,14 @@ namespace openloco::ui::windows
             0,
             298,
             170,
-            1 << 1,
-            (window_event_list*)&_events[0]);
-        window->widgets = (ui::widget_t*)0x00509e6c;
-        window->enabled_widgets = (1 << 0);
+            window_flags::stick_to_front | window_flags::transparent,
+            &_events);
+
+        window->widgets = _widgets;
+        window->enabled_widgets = 1 << widx::logo;
 
         window->init_scroll_widgets();
 
-        window->flags |= 0x10;
         window->colours[0] = colour::translucent(colour::grey);
         window->colours[1] = colour::translucent(colour::grey);
 
@@ -36,17 +52,17 @@ namespace openloco::ui::windows
     }
 
     // 0x00439298
-    static void paint(ui::window* window, gfx::drawpixelinfo_t* dpi)
+    static void draw(ui::window* window, gfx::drawpixelinfo_t* dpi)
     {
-        gfx::draw_image(dpi, window->x, window->y, 3624);
+        gfx::draw_image(dpi, window->x, window->y, image_ids::locomotion_logo);
     }
 
     // 0x004392AD
-    static void on_mouse_up(window* window, uint16_t widget)
+    static void on_mouse_up(window* window, widget_index widgetIndex)
     {
-        switch (widget)
+        switch (widgetIndex)
         {
-            case 0:
+            case widx::logo:
                 about::open();
                 break;
         }
