6327004be3e7a1325316dda29dc328b473bcd1a4
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2018-07-06 00:16:41 +0200

    Call `windowmgr::close` instead of address

--- src/openloco/gui.cpp
+++ src/openloco/gui.cpp
@@ -220,9 +220,7 @@ namespace openloco::gui
         {
             if (tutorial::state() == tutorial::tutorial_state::none)
             {
-                registers regs;
-                regs.esi = (uint32_t)window;
-                call(0x004CC6EA, regs);
+                windowmgr::close(window);
             }
         }
     }
