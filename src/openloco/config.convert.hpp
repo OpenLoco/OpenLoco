a7052231cb60a1f0f991d603a57fd943be0b7622
Ted John <ted@brambles.org>
2018-09-05 20:50:23 +0100

    Create proxy header for yaml include

--- src/openloco/config.convert.hpp
+++ src/openloco/config.convert.hpp
@@ -1,19 +1,7 @@
 #pragma once
 
 #include "config.h"
-
-#ifdef _WIN32
-// Ignore warnings generated from yaml-cpp
-#pragma warning(push)
-#pragma warning(disable : 4127) // conditional expression is constant
-#pragma warning(disable : 4251) // 'identifier': 'object_type1' 'identifier1' needs to have dll-interface to be used by clients of 'object_type' 'identfier2'
-#pragma warning(disable : 4275) // non dll-interface 'classkey' 'identifier1' used as base for dll-interface 'classkey' 'identifier2'
-#pragma warning(disable : 4996) // declaration deprecated
-#include <yaml-cpp/yaml.h>
-#pragma warning(pop)
-#else
-#include <yaml-cpp/yaml.h>
-#endif
+#include "utility/yaml.hpp"
 
 #define enum_def(x, y) \
     {                  \
--- src/openloco/config.cpp
+++ src/openloco/config.cpp
@@ -7,19 +7,6 @@
 #include <fstream>
 
 #ifdef _WIN32
-// Ignore warnings generated from yaml-cpp
-#pragma warning(push)
-#pragma warning(disable : 4127) // conditional expression is constant
-#pragma warning(disable : 4251) // 'identifier': 'object_type1' 'identifier1' needs to have dll-interface to be used by clients of 'object_type' 'identfier2'
-#pragma warning(disable : 4275) // non dll-interface 'classkey' 'identifier1' used as base for dll-interface 'classkey' 'identifier2'
-#pragma warning(disable : 4996) // declaration deprecated
-#include <yaml-cpp/yaml.h>
-#pragma warning(pop)
-#else
-#include <yaml-cpp/yaml.h>
-#endif
-
-#ifdef _WIN32
 #include <shlobj.h>
 #include <windows.h>
 #endif
@@ -28,6 +15,7 @@
 #include "config.h"
 #include "environment.h"
 #include "interop/interop.hpp"
+#include "utility/yaml.hpp"
 
 using namespace openloco::interop;
 
