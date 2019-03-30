gitter-badger/wissel

Implement part of 0x004691FA

src/openloco/interop/hooks.cpp
@@ -14,6 +14,7 @@
 #include "../graphics/gfx.h"
 #include "../gui.h"
 #include "../input.h"
+#include "../map/tile.h"
 #include "../platform/platform.h"
 #include "../station.h"
 #include "../things/vehicle.h"
@@ -837,6 +838,22 @@ void openloco::interop::register_hooks()
     ui::vehicle::registerHooks();
     ui::build_vehicle::registerHooks();
     ui::WindowManager::registerHooks();
+
+    // Part of 0x004691FA
+    register_hook(
+        0x0046959C,
+        [](registers& regs) -> uint8_t {
+            registers backup = regs;
+            int16_t x = regs.eax;
+            int16_t i = regs.ebx / 6;
+            int16_t y = regs.ecx;
+            map::surface_element* surface = (map::surface_element*)regs.esi;
+
+            surface->loc_46959C(x, y, i);
+
+            regs = backup;
+            return 0;
+        });
 
     register_hook(
         0x004AB655,

src/openloco/map/tile.h
@@ -78,6 +78,7 @@ namespace openloco::map
     namespace element_flags
     {
         constexpr uint8_t flag_4 = 1 << 4;
+        constexpr uint8_t flag_6 = 1 << 6;
         constexpr uint8_t last = 1 << 7;
     }
 
@@ -105,6 +106,7 @@ namespace openloco::map
         uint8_t clear_z() const { return _clear_z; }
 
         bool is_flag_4() const { return _flags & element_flags::flag_4; }
+        void set_flag_6() { _flags |= element_flags::flag_6; }
         bool is_last() const;
     };
 
@@ -170,6 +172,7 @@ namespace openloco::map
         uint8_t water() const { return _water & 0x1F; }
         uint8_t terrain() const { return _terrain & 0x1F; }
         uint8_t industry_id() const { return _industry; }
+        void loc_46959C(int16_t x, int16_t y, int ebx);
     };
 
     struct station_element : public tile_element_base

OpenLoco/OpenLoco

src/openloco/interop/hooks.cpp
@@ -13,6 +13,7 @@
 #include "../graphics/gfx.h"
 #include "../gui.h"
 #include "../input.h"
+#include "../map/tile.h"
 #include "../platform/platform.h"
 #include "../station.h"
 #include "../things/vehicle.h"
@@ -838,6 +839,22 @@ void openloco::interop::register_hooks()
     ui::vehicle::registerHooks();
     ui::WindowManager::registerHooks();
     ui::viewportmgr::registerHooks();
+
+    // Part of 0x004691FA
+    register_hook(
+        0x0046959C,
+        [](registers& regs) -> uint8_t {
+            registers backup = regs;
+            int16_t x = regs.eax;
+            int16_t i = regs.ebx / 6;
+            int16_t y = regs.ecx;
+            map::surface_element* surface = (map::surface_element*)regs.esi;
+
+            surface->createWave(x, y, i);
+
+            regs = backup;
+            return 0;
+        });
 
     register_hook(
         0x004AB655,

src/openloco/map/tile.h
@@ -78,6 +78,7 @@ namespace openloco::map
     namespace element_flags
     {
         constexpr uint8_t flag_4 = 1 << 4;
+        constexpr uint8_t flag_6 = 1 << 6;
         constexpr uint8_t last = 1 << 7;
     }
 
@@ -105,6 +106,7 @@ namespace openloco::map
         uint8_t clear_z() const { return _clear_z; }
 
         bool is_flag_4() const { return _flags & element_flags::flag_4; }
+        void set_flag_6() { _flags |= element_flags::flag_6; }
         bool is_last() const;
     };
 
@@ -170,6 +172,7 @@ namespace openloco::map
         uint8_t water() const { return _water & 0x1F; }
         uint8_t terrain() const { return _terrain & 0x1F; }
         uint8_t industry_id() const { return _industry; }
+        void createWave(int16_t x, int16_t y, int animationIndex);
     };
 
     struct station_element : public tile_element_base