1ec40051c5f50cd2adc2a118610b4ea8bd6022f8
Aaron van Geffen <aaron@aaronweb.net>
2019-04-13 12:03:41 +0200

    Introduce widx in vehicle_list window.

--- src/openloco/windows/vehicle_list.cpp
+++ src/openloco/windows/vehicle_list.cpp
@@ -1,11 +1,32 @@
 #include "../interop/interop.hpp"
 #include "../openloco.h"
 #include "../ui/WindowManager.h"
+#include <stdexcept>
 
 using namespace openloco::interop;
 
 namespace openloco::ui::windows::vehicle_list
 {
+    enum widx
+    {
+        frame = 0,
+        caption = 1,
+        close_button = 2,
+        panel = 3,
+        tab_trains,
+        tab_buses,
+        tab_trucks,
+        tab_trams,
+        tab_aircraft,
+        tab_ships,
+        company_select,
+        sort_name,
+        sort_profit,
+        sort_age,
+        sort_reliability,
+        scrollview,
+    };
+
     // 0x004C19DC
     window* open(uint16_t companyId, uint8_t type)
     {
@@ -19,7 +40,22 @@ namespace openloco::ui::windows::vehicle_list
             vehicle_list = (window*)regs.esi;
         }
         else
-            vehicle_list->call_on_mouse_up(type + 4);
+        {
+            if (type > 5)
+                throw std::domain_error("Unexpected vehicle type");
+
+            static widx type_to_widx[] = {
+                tab_trains,
+                tab_buses,
+                tab_trucks,
+                tab_trams,
+                tab_aircraft,
+                tab_ships,
+            };
+
+            widx target = type_to_widx[type];
+            vehicle_list->call_on_mouse_up(target);
+        }
 
         return vehicle_list;
     }
