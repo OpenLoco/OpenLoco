419d1adc74c31e5d9a2399f49e4f8e1b33db7312
Aaron van Geffen <aaron@aaronweb.net>
2019-04-13 15:54:53 +0200

    Introduce widx in terraform window.

--- src/openloco/windows/terraform.cpp
+++ src/openloco/windows/terraform.cpp
@@ -5,6 +5,22 @@ using namespace openloco::interop;
 
 namespace openloco::ui::windows::terraform
 {
+    namespace common
+    {
+        enum widx
+        {
+            frame = 0,
+            caption = 1,
+            close_button = 2,
+            panel = 3,
+            tab_clear_area,
+            tab_adjust_land,
+            tab_adjust_water,
+            tab_plant_trees,
+            tab_build_walls,
+        };
+    }
+
     window* open()
     {
         registers regs;
@@ -16,34 +32,34 @@ namespace openloco::ui::windows::terraform
     void open_clear_area()
     {
         auto terraform_window = open();
-        terraform_window->call_on_mouse_up(4);
+        terraform_window->call_on_mouse_up(common::widx::tab_clear_area);
     }
 
     // 0x004BB546
     void open_adjust_land()
     {
         auto terraform_window = open();
-        terraform_window->call_on_mouse_up(5);
+        terraform_window->call_on_mouse_up(common::widx::tab_adjust_land);
     }
 
     // 0x004BB556
     void open_adjust_water()
     {
         auto terraform_window = open();
-        terraform_window->call_on_mouse_up(6);
+        terraform_window->call_on_mouse_up(common::widx::tab_adjust_water);
     }
 
     // 0x004BB4A3
     void open_plant_trees()
     {
         auto terraform_window = open();
-        terraform_window->call_on_mouse_up(7);
+        terraform_window->call_on_mouse_up(common::widx::tab_plant_trees);
     }
 
     // 0x004BB576
     void open_build_walls()
     {
         auto terraform_window = open();
-        terraform_window->call_on_mouse_up(8);
+        terraform_window->call_on_mouse_up(common::widx::tab_build_walls);
     }
 }
