3cdd6377cbc0ec512931f86c4d98b0b9144e1f9f
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2020-05-05 20:28:40 +0100

    Add steam offsets

--- src/openloco/objects/steam_object.h
+++ src/openloco/objects/steam_object.h
@@ -8,12 +8,18 @@ namespace openloco
     struct steam_object
     {
         string_id name; // 0x00 probably not confirmed
-        uint8_t pad_02[0x5 - 0x2];
+        uint8_t pad_02[0x4 - 0x2];
+        uint8_t var_04;
         uint8_t var_05;
         uint8_t var_06;
         uint8_t var_07;
-        uint16_t var_08;
-        uint8_t pad_0A[0x1E - 0xA];
+        uint16_t var_08; // flags
+        uint32_t var_0A;
+        uint32_t var_0E; // images?
+        uint16_t var_12;
+        uint16_t var_14;
+        uint32_t var_16;
+        uint32_t var_1A;
         uint8_t sound_effect; // 0x1E probably not confirmed
         uint8_t var_1F[9];    // size tbc
     };
