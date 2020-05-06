9c0da939bc7805d870562b5fb2a3e07a97a5332d
Svelbeard <j.bloomfield.90@gmail.com>
2020-05-06 20:18:10 +0100

    Fixes for CI

--- src/openloco/objects/industry_object.h
+++ src/openloco/objects/industry_object.h
@@ -9,8 +9,8 @@ namespace openloco
     {
         string_id name;
         uint8_t pad_02[0xDE - 0x02];
-        uint8_t produced_cargo[2];  // 0xDE
-        uint8_t received_cargo[3];  // 0xE0
+        uint8_t produced_cargo[2]; // 0xDE
+        uint8_t received_cargo[3]; // 0xE0
         uint32_t var_E4;
         uint8_t pad_E8[0xEA - 0xE8];
         uint8_t var_EA;
