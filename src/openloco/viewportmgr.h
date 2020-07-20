eb683a0256df4f6ccf9c546a1db7fc649488fd8e
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2020-07-20 21:48:53 +0100

    Fix #491: Station/city names disappear on save.
    Mistake made in viewport create function meant that an un-implemented function would not get a pointer to the newly created viewport but a pointer to the main windows viewport. When it set the flags for the new viewport it instead set the flags for the main viewport. Fixed by passing back the viewport to the correct register.

--- src/openloco/viewportmgr.h
+++ src/openloco/viewportmgr.h
@@ -14,8 +14,8 @@ namespace openloco::ui::viewportmgr
     void init();
     void registerHooks();
     void collectGarbage();
-    void create(window* window, int viewportIndex, gfx::point_t origin, gfx::ui_size_t size, ZoomLevel zoom, thing_id_t thing_id);
-    void create(window* window, int viewportIndex, gfx::point_t origin, gfx::ui_size_t size, ZoomLevel zoom, map::map_pos3 tile);
+    viewport* create(window* window, int viewportIndex, gfx::point_t origin, gfx::ui_size_t size, ZoomLevel zoom, thing_id_t thing_id);
+    viewport* create(window* window, int viewportIndex, gfx::point_t origin, gfx::ui_size_t size, ZoomLevel zoom, map::map_pos3 tile);
     void invalidate(station* station);
     void invalidate(Thing* t, ZoomLevel zoom);
     void invalidate(map::map_pos pos, coord_t zMin, coord_t zMax, ZoomLevel zoom = ZoomLevel::eighth, int radius = 32);
