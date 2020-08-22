02e8708fe3b82350c90f41c278b21e06f833a7e5
Peter Gaal <petergaal@users.noreply.github.com>
2020-08-22 13:21:54 +0100

    implement stateScrollRight and sub functions
    - implement stateScrollRight at 0x004C76A7
    - implement horizontalDragFollow at 0x004C8CFD
    - implement verticalDragFollow at 0x004C8E2E

--- src/openloco/input/mouse_input.cpp
+++ src/openloco/input/mouse_input.cpp
@@ -26,6 +26,9 @@ using namespace openloco::ui::viewport_interaction;
 namespace openloco::input
 {
     static void stateScrollLeft(mouse_button cx, widget_index edx, ui::window* window, ui::widget_t* widget, int16_t x, int16_t y);
+    static void stateScrollRight(const mouse_button button, const int16_t x, const int16_t y);
+    static void horizontalDragFollow(ui::window* const w, ui::widget_t* const widget, const widget_index dragWidgetIndex, const size_t dragScrollIndex, const int16_t deltaX);
+    static void verticalDragFollow(ui::window* const w, ui::widget_t* const widget, const widget_index dragWidgetIndex, const size_t dragScrollIndex, const int16_t deltaY);
     static void state_resizing(mouse_button button, int16_t x, int16_t y, ui::window* window, ui::widget_t* widget, ui::widget_index widgetIndex);
     static void state_widget_pressed(mouse_button button, int16_t x, int16_t y, ui::window* window, ui::widget_t* widget, ui::widget_index widgetIndex);
     static void state_normal(mouse_button state, int16_t x, int16_t y, ui::window* window, ui::widget_t* widget, ui::widget_index widgetIndex);
@@ -429,7 +432,7 @@ namespace openloco::input
                 break;
 
             case input_state::scroll_right:
-                call(0x004C76A7, regs);
+                stateScrollRight(button, x, y);
                 break;
         }
     }
@@ -920,6 +923,122 @@ namespace openloco::input
             {
                 input::state(input_state::reset);
                 scrollview::clearPressedButtons(_pressedWindowType, _pressedWindowNumber, _pressedWidgetIndex);
+                break;
+            }
+
+            default:
+                break;
+        }
+    }
+
+    // 0x004C8E2E
+    // regs.bp: deltaY
+    static void verticalDragFollow(ui::window* const w, ui::widget_t* const widget, const widget_index dragWidgetIndex, const size_t dragScrollIndex, const int16_t deltaY)
+    {
+        scroll_area_t& scrollArea = w->scroll_areas[dragScrollIndex];
+        if ((scrollArea.flags & scroll_flags::VSCROLLBAR_VISIBLE) == 0)
+        {
+            return;
+        }
+
+        uint16_t trackHeight = widget->height() - 2 - thumbSize - thumbSize;
+        if ((scrollArea.flags & scroll_flags::HSCROLLBAR_VISIBLE) != 0)
+        {
+            trackHeight -= barWidth;
+        }
+
+        auto contentDeltaY = deltaY * scrollArea.contentHeight / trackHeight;
+
+        int16_t newOffset = scrollArea.contentOffsetY + contentDeltaY;
+
+        int frameHeight = widget->height() - 2;
+        if ((scrollArea.flags & scroll_flags::HSCROLLBAR_VISIBLE) != 0)
+        {
+            frameHeight -= barWidth;
+        }
+
+        int16_t maxOffset = scrollArea.contentHeight - frameHeight;
+        maxOffset = std::max<int16_t>(maxOffset, 0);
+
+        scrollArea.contentOffsetY = std::clamp<int16_t>(newOffset, 0, maxOffset);
+
+        scrollview::update_thumbs(w, dragWidgetIndex);
+        WindowManager::invalidateWidget(w->type, w->number, dragWidgetIndex);
+    }
+
+    // 0x004C8CFD
+    // regs.bp: deltaX
+    static void horizontalDragFollow(ui::window* const w, ui::widget_t* const widget, const widget_index dragWidgetIndex, const size_t dragScrollIndex, const int16_t deltaX)
+    {
+        scroll_area_t& scrollArea = w->scroll_areas[dragScrollIndex];
+        if ((scrollArea.flags & scroll_flags::HSCROLLBAR_VISIBLE) == 0)
+        {
+            return;
+        }
+
+        uint16_t trackWidth = widget->width() - 2 - thumbSize - thumbSize;
+        if (scrollArea.flags & scroll_flags::VSCROLLBAR_VISIBLE)
+        {
+            trackWidth -= barWidth;
+        }
+
+        auto contentDeltaX = deltaX * scrollArea.contentWidth / trackWidth;
+
+        int16_t newOffset = scrollArea.contentOffsetX + contentDeltaX;
+
+        int frameWidth = widget->width() - 2;
+        if (scrollArea.flags & scroll_flags::VSCROLLBAR_VISIBLE)
+        {
+            frameWidth -= barWidth;
+        }
+
+        int16_t maxOffset = scrollArea.contentWidth - frameWidth;
+        maxOffset = std::max<int16_t>(maxOffset, 0);
+
+        scrollArea.contentOffsetX = std::clamp<int16_t>(newOffset, 0, maxOffset);
+
+        scrollview::update_thumbs(w, dragWidgetIndex);
+        WindowManager::invalidateWidget(w->type, w->number, dragWidgetIndex);
+    }
+
+    // 0x004C76A7
+    // regs.cx = (uint16_t)button;
+    // regs.ax = x;
+    // regs.bx = y;
+    static void stateScrollRight(const mouse_button button, const int16_t x, const int16_t y)
+    {
+        auto window = WindowManager::find(_dragWindowType, _dragWindowNumber);
+        if (window == nullptr)
+        {
+            input::state(input_state::reset);
+            return;
+        }
+
+        switch (button)
+        {
+            case mouse_button::released:
+            {
+                _ticksSinceDragStart += time_since_last_tick;
+                if (x != 0 || y != 0)
+                {
+                    _ticksSinceDragStart = 1000;
+                    ui::widget_t* widget = &window->widgets[_dragWidgetIndex];
+                    horizontalDragFollow(window, widget, _dragWidgetIndex, _dragScrollIndex, x);
+                    verticalDragFollow(window, widget, _dragWidgetIndex, _dragScrollIndex, y);
+                }
+
+                break;
+            }
+
+            case mouse_button::right_released:
+            {
+                input::state(input_state::reset);
+                if (_ticksSinceDragStart <= 500)
+                {
+                    // in the assembly code:
+                    // regs.ax = _dragLastX
+                    // regs.bx = _dragLastY
+                }
                 break;
             }
 
