c799799f13ed290fe2da8b11663ae0bb0eb974d8
Aaron van Geffen <aaron@aaronweb.net>
2020-09-22 15:12:13 +0200

    Identified preview image

--- src/OpenLoco/ScenarioManager.h
+++ src/OpenLoco/ScenarioManager.h
@@ -7,17 +7,18 @@ namespace OpenLoco::ScenarioManager
 {
     struct ScenarioIndexEntry
     {
-        char filename[0x100];            // 0x000
-        uint8_t category;                // 0x100
-        uint8_t pad_101[0x124 - 0x101];  // 0x101
-        char scenarioName[0x40];         // 0x124
-        char description[0x100];         // 0x164
-        uint32_t flags;                  // 0x264
-        char highscoreName[0x100];       // 0x268
-        char objective[0x100];           // 0x368
-        uint8_t pad_469[0x46C - 0x469];  // 0x469
-        char currencyObjectId[8];        // 0x46C
-        uint8_t pad_268[0x4478 - 0x474]; // 0x474
+        char filename[0x100];           // 0x000
+        uint8_t category;               // 0x100
+        uint8_t pad_101[0x124 - 0x101]; // 0x101
+        char scenarioName[0x40];        // 0x124
+        char description[0x100];        // 0x164
+        uint32_t flags;                 // 0x264
+        char highscoreName[0x100];      // 0x268
+        char objective[0x100];          // 0x368
+        uint8_t pad_469[0x46C - 0x469]; // 0x469
+        char currencyObjectId[8];       // 0x46C
+        uint8_t pad_268[0x478 - 0x474]; // 0x474
+        uint8_t preview[128][128];      // 0x478
     };
 
     static_assert(offsetof(ScenarioIndexEntry, category) == 0x100);
