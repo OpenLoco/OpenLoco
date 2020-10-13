cbd798278ea42a2f2c9427a5918d5c26b97f9004
Peter Gaal <petergaal@users.noreply.github.com>
2020-10-13 16:20:09 +0100

    implement sub_4384E9

--- src/OpenLoco/Windows/CompanyWindow.cpp
+++ src/OpenLoco/Windows/CompanyWindow.cpp
@@ -2233,9 +2233,18 @@ namespace OpenLoco::Ui::Windows::CompanyWindow
 
     namespace Challenge
     {
+        static loco_global<cargo_object* [32], 0x0050C700> _cargoObjects;
+        static loco_global<cargo_object*, 0x0050D15C> _50D15C;
         static loco_global<char[256], 0x00526114> scenarioDetails;
+        static loco_global<uint8_t, 0x00526230> objectiveType;
         static loco_global<uint8_t, 0x00526231> objectiveFlags;
+        static loco_global<uint32_t, 0x00526232> objectiveCompanyValue;
+        static loco_global<uint32_t, 0x00526236> objectiveMonthlyVehicleProfit;
+        static loco_global<uint8_t, 0x0052623A> objectivePerformanceIndex;
+        static loco_global<uint8_t, 0x0052623B> objectiveDeliveredCargoType;
+        static loco_global<uint32_t, 0x0052623C> objectiveDeliveredCargoAmount;
         static loco_global<uint8_t, 0x00526240> objectiveTimeLimitYears;
+        static loco_global<uint16_t, 0x00526241> _526241; // objective complete goal until year?
         static loco_global<uint16_t, 0x00526243> objectiveMonthsInChallenge;
         static loco_global<uint16_t, 0x00526245> objectiveCompletedChallengeInMonths;
 
@@ -2278,12 +2287,93 @@ namespace OpenLoco::Ui::Windows::CompanyWindow
             Common::repositionTabs(self);
         }
 
+        // 0x00437D60
+        // Probably converts performance index * 10 to corporate rating index
+        // for example 10.0% (performanceIndex=100) converts to rating "Engineer" (output=1)
+        // input:
+        // ax = performanceIndex
+        // output:
+        // ax
+        static int16_t sub_437D60(int16_t performanceIndex)
+        {
+            registers regs;
+            regs.ax = performanceIndex;
+            call(0x00437D60, regs);
+            return regs.ax;
+        }
+
         // this will prepare _commonFormatArgs array before drawing the StringIds::challenge_value
         // after that for example it will draw this string: Achieve a performance index of 10.0% ("Engineer")
         // 0x004384E9
-        static void sub_4384E9()
+        static FormatArguments sub_4384E9()
         {
-            call(0x004384E9);
+            FormatArguments args{};
+
+            // 004384ED
+            switch (objectiveType)
+            {
+                case 0: // 004384FB-0043850A
+                    args.push(StringIds::achieve_a_company_value_of);
+                    args.push(*objectiveCompanyValue);
+                    break;
+
+                case 1: // 0043850C-0043851B
+                    args.push(StringIds::achieve_a_monthly_profit_from_vehicles_of);
+                    args.push(*objectiveMonthlyVehicleProfit);
+                    break;
+
+                case 2: // 0043851D-00438543
+                {
+                    args.push(StringIds::achieve_a_performance_index_of);
+                    int16_t performanceIndex = objectivePerformanceIndex * 10;
+                    args.push(performanceIndex);
+                    string_id corporateRating = sub_437D60(performanceIndex) + StringIds::corporate_rating_platelayer;
+                    args.push(corporateRating);
+                    break;
+                }
+
+                case 3: //00438545-00438576
+                {
+                    args.push(StringIds::deliver);
+                    cargo_object* cargoObject = _50D15C;
+                    if (objectiveDeliveredCargoType != 0xFF)
+                    {
+                        cargoObject = _cargoObjects[objectiveDeliveredCargoType];
+                    }
+                    args.push(cargoObject->unit_name_plural);
+                    args.push(*objectiveDeliveredCargoAmount);
+                    break;
+                }
+            }
+
+            if ((objectiveFlags & Scenario::objective_flags::flag_0) != 0) // 0043857B
+            {
+                args.push(StringIds::and_be_the_top_performing_company);
+            }
+            if ((objectiveFlags & Scenario::objective_flags::be_top_company) != 0) // 0043858C
+            {
+                args.push(StringIds::and_be_one_of_the_top_3_performing_companies);
+            }
+            if ((objectiveFlags & Scenario::objective_flags::be_within_top_three_companies) != 0) // 0043859D
+            {
+                if (isTitleMode() || isEditorMode())
+                {
+                    // 004385C5-004385D6
+                    args.push(StringIds::within_years);
+                    args.push<uint16_t>(*objectiveTimeLimitYears);
+                }
+                else
+                {
+                    // 004385B1-004385C3
+                    args.push(StringIds::by_the_end_of);
+                    args.push(*_526241);
+                }
+            }
+
+            // 004385D9
+            args.push<uint16_t>(0);
+            args.push<uint16_t>(0);
+            return args;
         }
 
         // 0x00433DEB
@@ -2305,8 +2395,7 @@ namespace OpenLoco::Ui::Windows::CompanyWindow
             y += 10;
 
             {
-                FormatArguments args{};
-                sub_4384E9();
+                FormatArguments args = sub_4384E9();
                 y = Gfx::drawString_495224(*dpi, self->x + 5, y, self->width - 10, Colour::black, StringIds::challenge_value, &args);
                 y += 5;
             }
