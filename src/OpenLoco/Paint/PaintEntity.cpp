52a5e922f649a654c4eb6356f6d563d09dd91231
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2020-12-23 16:44:51 +0000

    Start looking at function

--- src/OpenLoco/Paint/PaintEntity.cpp
+++ src/OpenLoco/Paint/PaintEntity.cpp
@@ -1,6 +1,8 @@
 #include "PaintEntity.h"
+#include "../Config.h"
 #include "../Interop/Interop.hpp"
 #include "../Map/Tile.h"
+#include "../Things/ThingManager.h"
 #include "Paint.h"
 
 using namespace OpenLoco::Interop;
@@ -11,6 +13,16 @@ namespace OpenLoco::Paint
     // 0x0046FA88
     void paintEntities(PaintSession& session, const Map::map_pos& loc)
     {
+        if (Config::get().vehicles_min_scale < session.dpi()->zoom_level)
+        {
+            return;
+        }
+
+        if (loc.x >= 0x4000 || loc.y >= 0x4000)
+        {
+            return;
+        }
+
         registers regs{};
         regs.eax = loc.x;
         regs.ecx = loc.y;
