0bca1bd5e178353679cbcdf1aad844e1519253fb
Peter Gaal <petergaal@users.noreply.github.com>
2021-01-16 10:41:10 +0000

    paintMiscEntity moved to separate file paintMiscEntity.cpp/h

---
+++ src/OpenLoco/Paint/PaintMiscEntity.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "../Things/Misc.h"
+#include "Paint.h"
+
+namespace OpenLoco::Paint
+{
+
+    /*
+     * @param base->x @<ax>
+     * @param base->y @<cx>
+     * @param base->z @<dx>
+     * @param ((base->sprite_yaw + (session.getRotation() << 4)) & 0x3F) @<ebx>
+     * @param base @<esi>
+     */
+    void paintMiscEntity(PaintSession& session, MiscBase* base);
+}
