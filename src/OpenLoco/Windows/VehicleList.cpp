2ab6f1079b9a39a3d3750f837e6eb398c684f336
Reinaldo A. C. Rauch <reinaldorauch@gmail.com>
2021-01-30 18:13:50 -0300

    Implementing open function

--- src/OpenLoco/Windows/VehicleList.cpp
+++ src/OpenLoco/Windows/VehicleList.cpp
@@ -7,6 +7,12 @@ using namespace OpenLoco::Interop;
 
 namespace OpenLoco::Ui::Windows::VehicleList
 {
+    static const Gfx::ui_size_t window_size = { 559, 213 };
+    static const Gfx::ui_size_t max_dimensions = { 640, 1200 };
+    static const Gfx::ui_size_t min_dimensions = { 192, 100 };
+
+    static window_event_list _events;
+
     enum widx
     {
         frame = 0,
@@ -27,36 +33,75 @@ namespace OpenLoco::Ui::Windows::VehicleList
         scrollview,
     };
 
+    static const uint8_t row_heights[] = {
+        28,
+        28,
+        28,
+        28,
+        48,
+        36
+    };
+
+    widx getTabFromType(uint8_t type);
+
+    void sub_4C1D4F()
+    {
+        registers regs;
+
+        call(0x004C1D4F);
+    }
+
     // 0x004C19DC
     window* open(uint16_t companyId, uint8_t type)
     {
         window* vehicle_list = WindowManager::bringToFront(WindowType::vehicleList, companyId);
+
         if (vehicle_list == nullptr)
         {
-            registers regs;
-            regs.eax = (uint32_t)companyId;
-            regs.ebx = type;
-            call(0x004C1A05, regs);
-            vehicle_list = (window*)regs.esi;
+            // 0x004C1A05
+            vehicle_list = WindowManager::createWindow(
+                WindowType::vehicleList,
+                window_size,
+                0,
+                &_events);
+
+            vehicle_list->current_tab = VehicleList::getTabFromType(type);
+            vehicle_list->row_height = row_heights[type];
+            vehicle_list->sort_mode = 0;
+            vehicle_list->row_hover = -1;
+
+            VehicleList::sub_4C1D4F();
+
+            vehicle_list->invalidate();
+
+            vehicle_list->callOnResize();
+            vehicle_list->callPrepareDraw();
+            vehicle_list->initScrollWidgets();
         }
         else
         {
-            if (type > 5)
-                throw std::domain_error("Unexpected vehicle type");
-
-            static widx type_to_widx[] = {
-                tab_trains,
-                tab_buses,
-                tab_trucks,
-                tab_trams,
-                tab_aircraft,
-                tab_ships,
-            };
-
-            widx target = type_to_widx[type];
-            vehicle_list->callOnMouseUp(target);
+            vehicle_list->callOnMouseUp(VehicleList::getTabFromType(type));
         }
 
         return vehicle_list;
     }
+
+    widx getTabFromType(uint8_t type)
+    {
+        if (type > 5)
+        {
+            throw std::domain_error("Unexpected vehicle type");
+        }
+
+        static widx type_to_widx[] = {
+            tab_trains,
+            tab_buses,
+            tab_trucks,
+            tab_trams,
+            tab_aircraft,
+            tab_ships,
+        };
+        return type_to_widx[type];
+    }
+
 }
