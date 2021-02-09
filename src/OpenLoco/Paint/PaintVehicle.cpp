231122458d55ab520185a5d80b00624756e9e858
Aaron van Geffen <aaron@aaronweb.net>
2021-02-09 19:58:35 +0100

    Fix translucent vehicle ghosts when white selected

--- src/OpenLoco/Paint/PaintVehicle.cpp
+++ src/OpenLoco/Paint/PaintVehicle.cpp
@@ -37,7 +37,7 @@ namespace OpenLoco::Paint
     // 0x004FFAE8
     static uint32_t applyGhostToImage(uint32_t imageId)
     {
-        if (Config::get().construction_marker)
+        if (Config::get().construction_marker == 0)
         {
             return Gfx::recolour(imageId, PaletteIndex::index_2C);
         }
