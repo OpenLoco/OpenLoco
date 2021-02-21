4c26a93ce2f41685af9f7f2cc92724529e81c78b
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2021-02-21 08:00:42 +0000

    Implement Order Skip game command

---
+++ src/OpenLoco/Vehicles/OrderSkip.cpp
@@ -0,0 +1,51 @@
+#include "../GameCommands.h"
+#include "../Interop/Interop.hpp"
+#include "../Things/ThingManager.h"
+#include "../Ui/WindowManager.h"
+#include "Orders.h"
+#include "Vehicle.h"
+
+using namespace OpenLoco::Interop;
+
+namespace OpenLoco::Vehicles
+{
+    loco_global<uint16_t, 0x009C68E0> gameCommandMapX;
+    loco_global<uint16_t, 0x009C68E2> gameCommandMapY;
+    loco_global<uint16_t, 0x009C68E4> gameCommandMapZ;
+
+    static uint32_t orderSkip(uint16_t headId, uint8_t flags)
+    {
+        auto* head = ThingManager::get<Vehicles::VehicleHead>(headId);
+        if (head == nullptr)
+        {
+            return GameCommands::FAILURE;
+        }
+
+        gameCommandMapX = head->x;
+        gameCommandMapY = head->y;
+        gameCommandMapZ = head->z;
+        if (!(flags & GameCommands::apply))
+        {
+            return 0;
+        }
+
+        Ui::WindowManager::sub_4B93A5(head->id);
+
+        OrderTableView orders(head->orderTableOffset);
+        auto curOrder = orders.atOffset(head->currentOrder);
+        curOrder++;
+        if (curOrder->is<OrderEnd>())
+        {
+            head->currentOrder = 0;
+        }
+        else
+        {
+            head->currentOrder = curOrder->getOffset() - head->orderTableOffset;
+        }
+    }
+
+    void orderSkip(registers& regs)
+    {
+        regs.ebx = orderSkip(regs.di, regs.bl);
+    }
+}
