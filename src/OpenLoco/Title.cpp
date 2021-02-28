01a1ece5b4755d5c1077fb884150b39433df1abd
Aaron van Geffen <aaron@aaronweb.net>
2021-02-28 20:32:33 +0100

    Integrate title sequence; use std::variant and std::visit

--- src/OpenLoco/Title.cpp
+++ src/OpenLoco/Title.cpp
@@ -8,12 +8,82 @@
 #include "Scenario.h"
 #include "Ui/WindowManager.h"
 
+#include <variant>
+#include <vector>
+
 using namespace OpenLoco::Interop;
 
 namespace OpenLoco::Title
 {
-    static loco_global<uint16_t*, 0x009DA3CC> _currentTitleCommand;
-    static loco_global<uint16_t, 0x009DA3D0> _waitCounter;
+    struct WaitStep
+    {
+        uint16_t duration;
+    };
+
+    struct ReloadStep
+    {
+        uint16_t arg;
+    };
+
+    struct MoveStep
+    {
+        coord_t xPos;
+        coord_t yPos;
+    };
+
+    struct RotateStep
+    {
+    };
+
+    struct ResetStep
+    {
+    };
+
+    using TitleStep = std::variant<WaitStep, ReloadStep, MoveStep, RotateStep, ResetStep>;
+    using TitleSequence = std::vector<TitleStep>;
+
+    // Helper type for using std::visit on TitleStep
+    template<class... Ts>
+    struct overloaded : Ts...
+    {
+        using Ts::operator()...;
+    };
+
+    // Explicit deduction guide (not needed as of C++20)
+    template<class... Ts>
+    overloaded(Ts...)->overloaded<Ts...>;
+
+    static const TitleSequence _titleSequence = {
+        MoveStep{ 231, 160 },
+        WaitStep{ 368 },
+        MoveStep{ 93, 59 },
+        WaitStep{ 370 },
+        RotateStep{},
+        MoveStep{ 314, 230 },
+        WaitStep{ 370 },
+        MoveStep{ 83, 314 },
+        WaitStep{ 370 },
+        RotateStep{},
+        RotateStep{},
+        MoveStep{ 224, 253 },
+        WaitStep{ 375 },
+        MoveStep{ 210, 322 },
+        WaitStep{ 375 },
+        MoveStep{ 125, 338 },
+        WaitStep{ 375 },
+        MoveStep{ 72, 138 },
+        WaitStep{ 375 },
+        RotateStep{},
+        MoveStep{ 178, 158 },
+        WaitStep{ 375 },
+        MoveStep{ 203, 316 },
+        WaitStep{ 375 },
+        ReloadStep{ 0 },
+        ResetStep{},
+    };
+
+    static TitleSequence::const_iterator _sequenceIterator;
+    static uint16_t _waitCounter;
 
     static void sub_473A95(int32_t eax);
 
@@ -62,7 +132,7 @@ namespace OpenLoco::Title
     // 0x00444357
     static void reset()
     {
-        _currentTitleCommand = (uint16_t*)0x4FB1F3;
+        _sequenceIterator = _titleSequence.begin();
         _waitCounter = 0;
         loadTitle();
         resetScreenAge();
@@ -110,87 +180,55 @@ namespace OpenLoco::Title
 
         resetScreenAge();
 
-        if (_waitCounter != 0)
+        if (_waitCounter > 0)
         {
-            _waitCounter = _waitCounter - 1;
+            _waitCounter -= 1;
             return;
         }
 
         do
         {
-            uint16_t cmd = *(*_currentTitleCommand);
-            _currentTitleCommand++;
-
-            switch (cmd)
-            {
-                case 0: // wait(duration)
-                {
-                    uint16_t arg = *(*_currentTitleCommand);
-                    _currentTitleCommand++;
-                    _waitCounter = arg / 4;
-                    break;
-                }
-
-                case 1: // reload
-                {
-                    uint16_t arg;
-                    do
-                    {
-                        arg = *(*_currentTitleCommand);
-                        _currentTitleCommand++;
-                    } while (arg != 0);
-
-                    loadTitle();
-                    Gfx::invalidateScreen();
-                    resetScreenAge();
-                    addr<0x50C19A, uint16_t>() = 55000;
-                    break;
-                }
-
-                case 2: // move(x, y)
-                {
-                    uint16_t argA = *(*_currentTitleCommand);
-                    _currentTitleCommand++;
-                    uint16_t argB = *(*_currentTitleCommand);
-                    _currentTitleCommand++;
-
-                    if (addr<0x00525E28, uint32_t>() & 1)
-                    {
-                        auto pos = Map::map_pos(argA * 32 + 16, argB * 32 + 16);
-                        auto height = Map::TileManager::getHeight(pos);
-                        auto main = Ui::WindowManager::getMainWindow();
-                        if (main != nullptr)
-                        {
-                            auto pos3d = Map::map_pos3(pos, height.landHeight);
-                            main->viewportCentreOnTile(pos3d);
-                            main->flags &= ~Ui::WindowFlags::scrolling_to_location;
-                            main->viewportsUpdatePosition();
-                        }
-                    }
-
-                    break;
-                }
-
-                case 3: // rotate()
-                {
-                    if (addr<0x00525E28, uint32_t>() & 1)
-                    {
-                        auto main = Ui::WindowManager::getMainWindow();
-                        if (main != nullptr)
-                        {
-                            main->viewportRotateRight();
-                        }
-                    }
-
-                    break;
-                }
-
-                case 4: // reset
-                {
-                    _currentTitleCommand = (uint16_t*)0x4FB1F3;
-                    break;
-                }
-            }
+            auto& command = *_sequenceIterator++;
+            std::visit(overloaded{
+                           [](WaitStep step) {
+                               _waitCounter = step.duration / 4;
+                           },
+                           [](ReloadStep step) {
+                               loadTitle();
+                               Gfx::invalidateScreen();
+                               resetScreenAge();
+                               addr<0x50C19A, uint16_t>() = 55000;
+                           },
+                           [](MoveStep step) {
+                               if (addr<0x00525E28, uint32_t>() & 1)
+                               {
+                                   auto pos = Map::map_pos(step.xPos * 32 + 16, step.yPos * 32 + 16);
+                                   auto height = Map::TileManager::getHeight(pos);
+                                   auto main = Ui::WindowManager::getMainWindow();
+                                   if (main != nullptr)
+                                   {
+                                       auto pos3d = Map::map_pos3(pos, height.landHeight);
+                                       main->viewportCentreOnTile(pos3d);
+                                       main->flags &= ~Ui::WindowFlags::scrolling_to_location;
+                                       main->viewportsUpdatePosition();
+                                   }
+                               }
+                           },
+                           [](RotateStep step) {
+                               if (addr<0x00525E28, uint32_t>() & 1)
+                               {
+                                   auto main = Ui::WindowManager::getMainWindow();
+                                   if (main != nullptr)
+                                   {
+                                       main->viewportRotateRight();
+                                   }
+                               }
+                           },
+                           [](ResetStep step) {
+                               _sequenceIterator = _titleSequence.begin();
+                           },
+                       },
+                       command);
         } while (_waitCounter == 0);
     }
 
