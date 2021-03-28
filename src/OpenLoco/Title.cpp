920ffa942a908759863f285c62cf16e0b186dacc
Aaron van Geffen <aaron@aaronweb.net>
2021-03-28 13:14:41 +0200

    Implement title sequence (#805)
    * Implement title sequence update function
    
    * Integrate title sequence using std::variant and std::visit
    
    Co-authored-by: Marijn van der Werf <marijn.vanderwerf@gmail.com>
    Co-authored-by: ζeh Matt <m.moninger.h@gmail.com>

--- src/OpenLoco/Title.cpp
+++ src/OpenLoco/Title.cpp
@@ -3,14 +3,84 @@
 #include "CompanyManager.h"
 #include "Gui.h"
 #include "Interop/Interop.hpp"
+#include "Map/TileManager.h"
 #include "OpenLoco.h"
 #include "Scenario.h"
 #include "Ui/WindowManager.h"
+
+#include <variant>
+#include <vector>
 
 using namespace OpenLoco::Interop;
 
 namespace OpenLoco::Title
 {
+    struct WaitStep
+    {
+        uint16_t duration;
+    };
+
+    struct ReloadStep
+    {
+        uint16_t arg;
+    };
+
+    using MoveStep = Map::TilePos;
+
+    struct RotateStep
+    {
+    };
+
+    struct ResetStep
+    {
+    };
+
+    using TitleStep = std::variant<WaitStep, ReloadStep, MoveStep, RotateStep, ResetStep>;
+    using TitleSequence = std::vector<TitleStep>;
+
+    // Helper type for using std::visit on TitleStep
+    template<class... Ts>
+    struct overloaded : Ts...
+    {
+        using Ts::operator()...;
+    };
+
+    // Explicit deduction guide (not needed as of C++20)
+    template<class... Ts>
+    overloaded(Ts...)->overloaded<Ts...>;
+
+    static const TitleSequence _titleSequence = {
+        MoveStep{ 231, 160 },
+        WaitStep{ 368 },
+        MoveStep{ 93, 59 },
+        WaitStep{ 370 },
+        RotateStep{},
+        MoveStep{ 314, 230 },
+        WaitStep{ 370 },
+        MoveStep{ 83, 314 },
+        WaitStep{ 370 },
+        RotateStep{},
+        RotateStep{},
+        MoveStep{ 224, 253 },
+        WaitStep{ 375 },
+        MoveStep{ 210, 322 },
+        WaitStep{ 375 },
+        MoveStep{ 125, 338 },
+        WaitStep{ 375 },
+        MoveStep{ 72, 138 },
+        WaitStep{ 375 },
+        RotateStep{},
+        MoveStep{ 178, 158 },
+        WaitStep{ 375 },
+        MoveStep{ 203, 316 },
+        WaitStep{ 375 },
+        ReloadStep{ 0 },
+        ResetStep{},
+    };
+
+    static TitleSequence::const_iterator _sequenceIterator;
+    static uint16_t _waitCounter;
+
     static void sub_473A95(int32_t eax);
 
     void registerHooks()
@@ -50,10 +120,20 @@ namespace OpenLoco::Title
         call(0x004284C8);
     }
 
-    // 0x00444357
-    static void sub_444357()
+    static void loadTitle()
     {
-        call(0x00444357);
+        call(0x004442C4);
+    }
+
+    // 0x00444357
+    static void reset()
+    {
+        _sequenceIterator = _titleSequence.begin();
+        _waitCounter = 0;
+        loadTitle();
+        resetScreenAge();
+        addr<0x50C19A, uint16_t>() = 55000;
+        update();
     }
 
     // 0x0046AD7D
@@ -81,11 +161,75 @@ namespace OpenLoco::Title
         initialiseViewports();
         sub_4284C8();
         Gui::init();
-        sub_444357();
+        reset();
         Gfx::invalidateScreen();
         resetScreenAge();
 
         Audio::playTitleScreenMusic();
+    }
+
+    // 0x00444387
+    void update()
+    {
+        if (!isTitleMode())
+            return;
+
+        resetScreenAge();
+
+        if (_waitCounter > 0)
+        {
+            _waitCounter -= 1;
+            return;
+        }
+
+        do
+        {
+            if (_sequenceIterator >= _titleSequence.end())
+                return;
+
+            auto& command = *_sequenceIterator++;
+            std::visit(overloaded{
+                           [](WaitStep step) {
+                               // This loop slightly deviates from the original, subtract 1 tick to make up for it.
+                               _waitCounter = step.duration - 1;
+                           },
+                           [](ReloadStep step) {
+                               loadTitle();
+                               Gfx::invalidateScreen();
+                               resetScreenAge();
+                               addr<0x50C19A, uint16_t>() = 55000;
+                           },
+                           [](MoveStep step) {
+                               if (addr<0x00525E28, uint32_t>() & 1)
+                               {
+                                   auto pos = Map::map_pos(step) + Map::map_pos(16, 16);
+                                   auto height = Map::TileManager::getHeight(pos);
+                                   auto main = Ui::WindowManager::getMainWindow();
+                                   if (main != nullptr)
+                                   {
+                                       auto pos3d = Map::map_pos3(pos.x, pos.y, height.landHeight);
+                                       main->viewportCentreOnTile(pos3d);
+                                       main->flags &= ~Ui::WindowFlags::scrolling_to_location;
+                                       main->viewportsUpdatePosition();
+                                   }
+                               }
+                           },
+                           [](RotateStep step) {
+                               if (addr<0x00525E28, uint32_t>() & 1)
+                               {
+                                   auto main = Ui::WindowManager::getMainWindow();
+                                   if (main != nullptr)
+                                   {
+                                       main->viewportRotateRight();
+                                   }
+                               }
+                           },
+                           [](ResetStep step) {
+                               _sequenceIterator = _titleSequence.begin();
+                           },
+                       },
+                       command);
+        } while (_waitCounter == 0);
     }
 
     // 0x00473A95
