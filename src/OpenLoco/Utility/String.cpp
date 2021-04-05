200f29973314ea5d2718fee773d8cbb3dc7a9858
Aaron van Geffen <aaron@aaronweb.net>
2021-04-05 22:05:38 +0200

    Use logical string comparison for vehicle list order

--- src/OpenLoco/Utility/String.cpp
+++ src/OpenLoco/Utility/String.cpp
@@ -12,6 +12,40 @@
 
 namespace OpenLoco::Utility
 {
+    // Case-insensitive logical string comparison function
+    int32_t strlogicalcmp(const char* s1, const char* s2)
+    {
+        for (;;)
+        {
+            if (*s2 == '\0')
+                return *s1 != '\0';
+            else if (*s1 == '\0')
+                return -1;
+            else if (!(isdigit(*s1) && isdigit(*s2)))
+            {
+                if (toupper(*s1) != toupper(*s2))
+                    return toupper(*s1) - toupper(*s2);
+                else
+                {
+                    ++s1;
+                    ++s2;
+                }
+            }
+            else
+            {
+                char *lim1, *lim2;
+                unsigned long n1 = strtoul(s1, &lim1, 10);
+                unsigned long n2 = strtoul(s2, &lim2, 10);
+                if (n1 > n2)
+                    return 1;
+                else if (n1 < n2)
+                    return -1;
+                s1 = lim1;
+                s2 = lim2;
+            }
+        }
+    }
+
     std::string toUtf8(const std::wstring_view& src)
     {
 #ifdef _WIN32
