647f79a9f22e153aa0bd9cef2d333ad1c68eefff
Aaron van Geffen <aaron@aaronweb.net>
2021-04-29 18:50:00 +0200

    Move comment in ClimateObject / clang-format

--- src/OpenLoco/Objects/ClimateObject.h
+++ src/OpenLoco/Objects/ClimateObject.h
@@ -7,7 +7,7 @@ namespace OpenLoco
 #pragma pack(push, 1)
     struct ClimateObject
     {
-        string_id name; // 0x00
+        string_id name;          // 0x00
         uint8_t firstSeason;     // 0x02
         uint8_t seasonLength[4]; // 0x03
         uint8_t winterSnowLine;  // 0x07
