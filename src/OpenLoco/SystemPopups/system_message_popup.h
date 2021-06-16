69b457698177ce2f9ccc05d62c8767132c0af221
rdrdrdrd <richard.dumene@gmail.com>
2021-06-15 23:51:49 -0500

    works for windows

--- src/OpenLoco/SystemPopups/system_message_popup.h
+++ src/OpenLoco/SystemPopups/system_message_popup.h
@@ -1,10 +1,13 @@
 #pragma once
 #include <string>
+#include <sstream>
 
 namespace OpenLoco
 {
     namespace SystemUtils
     {
         bool system_message_popup(const std::string& title, const std::string& message_to_display);
+
+        bool system_message_popup(const std::string& title, std::stringstream& message_as_stream);
     }
 }
