155845d2998b516b6173cf25b6b8f6a613be7c5c
Michał Janiszewski <janisozaur+signed@gmail.com>
2021-06-19 23:29:57 +0200

    Obtain version info for dump by querying OpenLoco

--- src/OpenLoco/Platform/Crash.cpp
+++ src/OpenLoco/Platform/Crash.cpp
@@ -1,15 +1,10 @@
 #include "Crash.h"
 #if defined(USE_BREAKPAD)
+#include "../OpenLoco.h"
 #include "../Utility/String.hpp"
 #include "Platform.h"
 #include <ShlObj.h>
 #include <client/windows/handler/exception_handler.h>
-
-#if defined(OPENLOCO_COMMIT_SHA1_SHORT)
-const wchar_t* _wszCommitSha1Short = L"" OPENLOCO_COMMIT_SHA1_SHORT;
-#else
-const wchar_t* _wszCommitSha1Short = L"Unknown";
-#endif
 
 static bool OnCrash(
     const wchar_t* dumpPath, const wchar_t* miniDumpId, void* context, EXCEPTION_POINTERS* exinfo,
@@ -27,9 +22,11 @@ static bool OnCrash(
     wchar_t dumpFilePath[MAX_PATH];
     swprintf_s(dumpFilePath, std::size(dumpFilePath), L"%s\\%s.dmp", dumpPath, miniDumpId);
 
+    std::wstring version = OpenLoco::Utility::toUtf16(OpenLoco::getVersionInfo());
+
     wchar_t dumpFilePathNew[MAX_PATH];
     swprintf_s(
-        dumpFilePathNew, std::size(dumpFilePathNew), L"%s\\%s(%s).dmp", dumpPath, miniDumpId, _wszCommitSha1Short);
+        dumpFilePathNew, std::size(dumpFilePathNew), L"%s\\%s(%s).dmp", dumpPath, miniDumpId, version.c_str());
 
     // Try to rename the files
     if (_wrename(dumpFilePath, dumpFilePathNew) == 0)
@@ -41,13 +38,13 @@ static bool OnCrash(
     wprintf(L"Dump Path: %s\n", dumpPath);
     wprintf(L"Dump File Path: %s\n", dumpFilePath);
     wprintf(L"Dump Id: %s\n", miniDumpId);
-    wprintf(L"Commit: %s\n", _wszCommitSha1Short);
+    wprintf(L"Version: %s\n", version.c_str());
 
     constexpr const wchar_t* MessageFormat = L"A crash has occurred and a dump was created at\n%s.\n\nPlease file an issue "
                                              L"with OpenLoco on GitHub and provide "
-                                             L"the dump and saved game there.\n\n\nCommit: %s\n\n";
+                                             L"the dump and saved game there.\n\n\nVersion: %s\n\n";
     wchar_t message[MAX_PATH * 2];
-    swprintf_s(message, MessageFormat, dumpFilePath, _wszCommitSha1Short);
+    swprintf_s(message, MessageFormat, dumpFilePath, version.c_str());
     MessageBoxW(nullptr, message, L"OpenLoco", MB_OK | MB_ICONERROR);
 
     HRESULT coInitializeResult = CoInitialize(nullptr);
