06cb4c5fa65f4bd279e2a50039842174b83f7af3
Aaron van Geffen <aaron@aaronweb.net>
2021-08-10 20:24:09 +0200

    Formatting

--- src/OpenLoco/Company.h
+++ src/OpenLoco/Company.h
@@ -107,12 +107,12 @@ namespace OpenLoco
         currency48_t vehicleProfit;                  // 0x8B9E
         uint16_t transportTypeCount[6];              // 0x8BA4
         uint8_t var_8BB0[9];
-        ObservationStatus observationStatus;   // 0x8BB9;
-        TownId_t observationTownId; // 0x8BBA;
+        ObservationStatus observationStatus; // 0x8BB9;
+        TownId_t observationTownId;          // 0x8BBA;
         EntityId_t observation_thing; // 0x8BBC;
         int16_t observation_x;        // 0x8BBE;
         int16_t observation_y;        // 0x8BC0;
-        uint16_t observationObject;  // 0x8BC2;
+        uint16_t observationObject;   // 0x8BC2;
         uint8_t pad_8BC4[0x8BCE - 0x8BC4];
         uint32_t cargoDelivered[32]; // 0x8BCE;
         uint8_t challengeProgress;   // 0x8C4E - percent completed on challenge
