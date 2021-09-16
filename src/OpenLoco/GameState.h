507208dc13a316e4e253bf75102695a882a19b32
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2021-09-16 21:49:32 +0100

    Label further gamestate

--- src/OpenLoco/GameState.h
+++ src/OpenLoco/GameState.h
@@ -15,7 +15,8 @@ namespace OpenLoco
     struct GameState
     {
         Utility::prng rng;                                                     // 0x000000 (0x00525E18)
-        uint32_t var_0008[3];                                                  // 0x000008 (0x00525E20)
+        Utility::prng unkRng;                                                  // 0x000008 (0x00525E20)
+        uint32_t flags;                                                        // 0x000010 (0x00525E28)
         uint32_t currentDay;                                                   // 0x000014 (0x00525E2C)
         uint16_t dayCounter;                                                   // 0x000018 (0x00525E30)
         uint16_t currentYear;                                                  // 0x00001A (0x00525E32)
