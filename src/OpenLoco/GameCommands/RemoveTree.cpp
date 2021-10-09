3a5973db6aa4e77bc346fe73adff0c54ab8c9bc2
Peter Gaal <petergaal@users.noreply.github.com>
2021-10-09 07:14:46 +0100

    Add documentation with parameters for function removeTree (#1192)
    * Add documentation with parameters for function removeTree
    
    * Addressed review comments and added more details

--- src/OpenLoco/GameCommands/RemoveTree.cpp
+++ src/OpenLoco/GameCommands/RemoveTree.cpp
@@ -11,7 +11,20 @@ using namespace OpenLoco::Interop;
 
 namespace OpenLoco::GameCommands
 {
-    // 0x004BB392
+    /**
+     * 0x004BB392
+     * Remove tree
+     *
+     * This is called when you activate the Plant Trees from the construction (first) menu and you move the cursor over the terrain.
+     *
+     * @param pos_x @<ax>
+     * @param pos_y @<cx>
+     * @param pos_z @<dl> * 4
+     * @param type @<dh>
+     * @param elementType @<bh>
+     * @param flags @<bl>
+     * @return @<ebx> - returns the remove cost if successful; otherwise GameCommands::FAILURE (in the assembly code we never get into failure path)
+     */
     static uint32_t removeTree(const Map::Pos3& pos, const uint8_t type, const uint8_t elementType, const uint8_t flags)
     {
         GameCommands::setExpenditureType(ExpenditureType::Construction);
