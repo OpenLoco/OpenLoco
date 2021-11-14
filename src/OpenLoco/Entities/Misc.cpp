7fd1a852c7c37bcd9b65c5d8a051ebc8a961a23d
Duncan <duncans_pumpkin@hotmail.co.uk>
2021-11-14 21:22:28 +0000

    Misc Update functions (#1216)
    * Split out misc update into individual types
    
    * Implement simple misc updates

--- src/OpenLoco/Entities/Misc.cpp
+++ src/OpenLoco/Entities/Misc.cpp
@@ -4,6 +4,7 @@
 #include "../Map/TileManager.h"
 #include "../Objects/ObjectManager.h"
 #include "../Ui/WindowManager.h"
+#include "../ViewportManager.h"
 #include "EntityManager.h"
 
 using namespace OpenLoco::Interop;
@@ -13,14 +14,49 @@ namespace OpenLoco
     // 0x004405CD
     void MiscBase::update()
     {
-        registers regs;
-        regs.esi = X86Pointer(this);
-        call(0x004405CD, regs);
+        switch (getSubType())
+        {
+            case MiscEntityType::exhaust:
+                asExhaust()->update();
+                break;
+            case MiscEntityType::redGreenCurrency:
+                asRedGreenCurrency()->update();
+                break;
+            case MiscEntityType::windowCurrency:
+                asWindowCurrency()->update();
+                break;
+            case MiscEntityType::vehicleCrashParticle:
+                asVehicleCrashParticle()->update();
+                break;
+            case MiscEntityType::explosionCloud:
+                asExplosionCloud()->update();
+                break;
+            case MiscEntityType::splash:
+                asSplash()->update();
+                break;
+            case MiscEntityType::fireball:
+                asFireball()->update();
+                break;
+            case MiscEntityType::explosionSmoke:
+                asExplosionSmoke()->update();
+                break;
+            case MiscEntityType::smoke:
+                asSmoke()->update();
+                break;
+        }
     }
 
     SteamObject* Exhaust::object() const
     {
         return ObjectManager::get<SteamObject>(object_id & 0x7F);
+    }
+
+    // 0x004408C2
+    void Exhaust::update()
+    {
+        registers regs;
+        regs.esi = X86Pointer(this);
+        call(0x004408C2, regs);
     }
 
     // 0x0044080C
@@ -57,6 +93,20 @@ namespace OpenLoco
         return _exhaust;
     }
 
+    // 0x004407A1
+    void Smoke::update()
+    {
+        Ui::ViewportManager::invalidate(this, ZoomLevel::half);
+        moveTo(position + Map::Pos3(0, 0, 1));
+        Ui::ViewportManager::invalidate(this, ZoomLevel::half);
+
+        frame += 0x55;
+        if (frame >= 0xC00)
+        {
+            EntityManager::freeEntity(this);
+        }
+    }
+
     // 0x00440BEB
     Smoke* Smoke::create(Map::Pos3 loc)
     {
@@ -75,6 +125,21 @@ namespace OpenLoco
     }
 
     static loco_global<int32_t, 0x112C876> _currentFontSpriteBase;
+
+    // 0x0044063E
+    void MoneyEffect::update()
+    {
+        registers regs;
+        regs.esi = X86Pointer(this);
+        if (getSubType() == MiscEntityType::windowCurrency)
+        {
+            call(0x0044063E, regs);
+        }
+        else
+        {
+            call(0x004405D8, regs);
+        }
+    }
 
     // 0x00440A74
     // company : updatingCompanyId global
@@ -110,5 +175,57 @@ namespace OpenLoco
             m->wiggle = 0;
         }
         return m;
+    }
+
+    // 0x004406A0
+    void VehicleCrashParticle::update()
+    {
+        registers regs;
+        regs.esi = X86Pointer(this);
+        call(0x004406A0, regs);
+    }
+
+    // 0x004407CC
+    void ExplosionCloud::update()
+    {
+        invalidateSprite();
+        frame += 0x80;
+        if (frame >= 0x1200)
+        {
+            EntityManager::freeEntity(this);
+        }
+    }
+
+    // 0x004407E0
+    void Splash::update()
+    {
+        invalidateSprite();
+        frame += 0x55;
+        if (frame >= 0x1C00)
+        {
+            EntityManager::freeEntity(this);
+        }
+    }
+
+    // 0x004407F3
+    void Fireball::update()
+    {
+        invalidateSprite();
+        frame += 0x40;
+        if (frame >= 0x1F00)
+        {
+            EntityManager::freeEntity(this);
+        }
+    }
+
+    // 0x00440078D
+    void ExplosionSmoke::update()
+    {
+        invalidateSprite();
+        frame += 0x80;
+        if (frame >= 0xA00)
+        {
+            EntityManager::freeEntity(this);
+        }
     }
 }
