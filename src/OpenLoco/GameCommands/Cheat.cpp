673be5055736586c564ae15b3db020c0c2f96dfc
Ben <bigbadbennybrother@gmail.com>
2022-01-16 19:39:46 +1100

    formatting?

--- src/OpenLoco/GameCommands/Cheat.cpp
+++ src/OpenLoco/GameCommands/Cheat.cpp
@@ -18,163 +18,163 @@ using namespace OpenLoco::Interop;
 
 namespace OpenLoco::GameCommands
 {
-	static loco_global<CompanyId, 0x009C68EB> _updatingCompanyId;
+    static loco_global<CompanyId, 0x009C68EB> _updatingCompanyId;
 
-	namespace Cheats
-	{
-		static uint32_t acquireAssets(CompanyId targetCompanyId)
-		{
-			auto ourCompanyId = CompanyManager::updatingCompanyId();
+    namespace Cheats
+    {
+        static uint32_t acquireAssets(CompanyId targetCompanyId)
+        {
+            auto ourCompanyId = CompanyManager::updatingCompanyId();
 
-			// First phase: change ownership of all tile elements that currently belong to the target company.
-			for (auto& element : TileManager::getElements())
-			{
-				auto* roadElement = element.as<RoadElement>();
-				if (roadElement != nullptr)
-				{
-					roadElement->setOwner(ourCompanyId);
-					continue;
-				}
+            // First phase: change ownership of all tile elements that currently belong to the target company.
+            for (auto& element : TileManager::getElements())
+            {
+                auto* roadElement = element.as<RoadElement>();
+                if (roadElement != nullptr)
+                {
+                    roadElement->setOwner(ourCompanyId);
+                    continue;
+                }
 
-				auto* trackElement = element.as<TrackElement>();
-				if (trackElement != nullptr)
-				{
-					trackElement->setOwner(ourCompanyId);
-					continue;
-				}
-			}
+                auto* trackElement = element.as<TrackElement>();
+                if (trackElement != nullptr)
+                {
+                    trackElement->setOwner(ourCompanyId);
+                    continue;
+                }
+            }
 
-			// Second phase: change ownership of all stations that currently belong to the target company.
-			for (auto& station : StationManager::stations())
-			{
-				if (station.owner != targetCompanyId)
-					continue;
+            // Second phase: change ownership of all stations that currently belong to the target company.
+            for (auto& station : StationManager::stations())
+            {
+                if (station.owner != targetCompanyId)
+                    continue;
 
-				station.owner = ourCompanyId;
-			}
+                station.owner = ourCompanyId;
+            }
 
-			// Third phase: change ownership of all vehicles that currently belong to the target company.
-			for (auto vehicle : EntityManager::VehicleList())
-			{
-				if (vehicle->owner != targetCompanyId)
-					continue;
+            // Third phase: change ownership of all vehicles that currently belong to the target company.
+            for (auto vehicle : EntityManager::VehicleList())
+            {
+                if (vehicle->owner != targetCompanyId)
+                    continue;
 
-				Vehicles::Vehicle train(vehicle);
-				train.head->owner = ourCompanyId;
-				train.veh1->owner = ourCompanyId;
-				train.veh2->owner = ourCompanyId;
-				train.tail->owner = ourCompanyId;
+                Vehicles::Vehicle train(vehicle);
+                train.head->owner = ourCompanyId;
+                train.veh1->owner = ourCompanyId;
+                train.veh2->owner = ourCompanyId;
+                train.tail->owner = ourCompanyId;
 
-				for (auto& car : train.cars)
-				{
-					for (auto& component : car)
-					{
-						component.front->owner = ourCompanyId;
-						component.body->owner = ourCompanyId;
-						component.back->owner = ourCompanyId;
-					}
-				}
-			}
+                for (auto& car : train.cars)
+                {
+                    for (auto& component : car)
+                    {
+                        component.front->owner = ourCompanyId;
+                        component.body->owner = ourCompanyId;
+                        component.back->owner = ourCompanyId;
+                    }
+                }
+            }
 
-			return 0;
-		}
+            return 0;
+        }
 
-		static uint32_t addCash(currency32_t amount)
-		{
-			auto company = CompanyManager::getPlayerCompany();
-			company->cash = company->cash + amount;
-			return 0;
-		}
+        static uint32_t addCash(currency32_t amount)
+        {
+            auto company = CompanyManager::getPlayerCompany();
+            company->cash = company->cash + amount;
+            return 0;
+        }
 
-		static uint32_t clearLoan()
-		{
-			auto company = CompanyManager::getPlayerCompany();
-			company->current_loan = 0;
-			return 0;
-		}
+        static uint32_t clearLoan()
+        {
+            auto company = CompanyManager::getPlayerCompany();
+            company->current_loan = 0;
+            return 0;
+        }
 
-		static uint32_t companyRatings(bool absolute, int32_t value)
-		{
-			auto companyId = CompanyManager::getControllingId();
+        static uint32_t companyRatings(bool absolute, int32_t value)
+        {
+            auto companyId = CompanyManager::getControllingId();
 
-			for (auto& town : TownManager::towns())
-			{
-				// Does this town have a rating for our company?
-				if (!(town.companies_with_rating &= (1 << enumValue(companyId))))
-					continue;
+            for (auto& town : TownManager::towns())
+            {
+                // Does this town have a rating for our company?
+                if (!(town.companies_with_rating &= (1 << enumValue(companyId))))
+                    continue;
 
-				int16_t newRanking{};
-				if (absolute)
-				{
-					newRanking = value * max_company_rating;
-				}
-				else
-				{
-					newRanking = town.company_ratings[enumValue(companyId)] + max_company_rating;
-					newRanking *= 1.0f + (1.0f / value);
-					newRanking -= max_company_rating;
-				}
+                int16_t newRanking{};
+                if (absolute)
+                {
+                    newRanking = value * max_company_rating;
+                }
+                else
+                {
+                    newRanking = town.company_ratings[enumValue(companyId)] + max_company_rating;
+                    newRanking *= 1.0f + (1.0f / value);
+                    newRanking -= max_company_rating;
+                }
 
-				// Set the new rating.
-				town.company_ratings[enumValue(companyId)] = std::clamp<int16_t>(newRanking, min_company_rating, max_company_rating);
-			}
+                // Set the new rating.
+                town.company_ratings[enumValue(companyId)] = std::clamp<int16_t>(newRanking, min_company_rating, max_company_rating);
+            }
 
-			return 0;
-		}
+            return 0;
+        }
 
-		static uint32_t switchCompany(CompanyId targetCompanyId)
-		{
-			auto ourId = CompanyManager::getControllingId();
-			auto otherId = CompanyManager::getSecondaryPlayerId();
+        static uint32_t switchCompany(CompanyId targetCompanyId)
+        {
+            auto ourId = CompanyManager::getControllingId();
+            auto otherId = CompanyManager::getSecondaryPlayerId();
 
-			// Already controlling the target company?
-			if (targetCompanyId == ourId)
-				return 0;
+            // Already controlling the target company?
+            if (targetCompanyId == ourId)
+                return 0;
 
-			// Is the other player controlling the target company? Swap companies.
-			if (targetCompanyId == otherId)
-			{
-				CompanyManager::setSecondaryPlayerId(ourId);
-				CompanyManager::setControllingId(otherId);
-				return 0;
-			}
+            // Is the other player controlling the target company? Swap companies.
+            if (targetCompanyId == otherId)
+            {
+                CompanyManager::setSecondaryPlayerId(ourId);
+                CompanyManager::setControllingId(otherId);
+                return 0;
+            }
 
-			// Change control over to the other company.
-			CompanyManager::setControllingId(targetCompanyId);
-			return 0;
-		}
+            // Change control over to the other company.
+            CompanyManager::setControllingId(targetCompanyId);
+            return 0;
+        }
 
-		static uint32_t toggleBankruptcy(CompanyId targetCompanyId)
-		{
-			auto company = CompanyManager::get(targetCompanyId);
-			company->challenge_flags ^= CompanyFlags::bankrupt;
-			return 0;
-		}
+        static uint32_t toggleBankruptcy(CompanyId targetCompanyId)
+        {
+            auto company = CompanyManager::get(targetCompanyId);
+            company->challenge_flags ^= CompanyFlags::bankrupt;
+            return 0;
+        }
 
-		static uint32_t toggleJail(CompanyId targetCompanyId)
-		{
-			auto company = CompanyManager::get(targetCompanyId);
-			company->jail_status = 30;
-			return 0;
-		}
+        static uint32_t toggleJail(CompanyId targetCompanyId)
+        {
+            auto company = CompanyManager::get(targetCompanyId);
+            company->jail_status = 30;
+            return 0;
+        }
 
-		static uint32_t vehicleReliability(int32_t newReliablity)
-		{
-			auto ourCompanyId = CompanyManager::updatingCompanyId();
+        static uint32_t vehicleReliability(int32_t newReliablity)
+        {
+            auto ourCompanyId = CompanyManager::updatingCompanyId();
 
-			for (auto vehicle : EntityManager::VehicleList())
-			{
-				if (vehicle->owner != ourCompanyId)
-					continue;
+            for (auto vehicle : EntityManager::VehicleList())
+            {
+                if (vehicle->owner != ourCompanyId)
+                    continue;
 
-				Vehicles::Vehicle train(vehicle);
-				train.veh2->reliability = newReliablity;
+                Vehicles::Vehicle train(vehicle);
+                train.veh2->reliability = newReliablity;
 
-				// Set reliability for the first car's front bogie component.
-				train.cars.firstCar.front->reliability = newReliablity * 256;
-			}
-			return 0;
-		}
+                // Set reliability for the first car's front bogie component.
+                train.cars.firstCar.front->reliability = newReliablity * 256;
+            }
+            return 0;
+        }
 
         static uint32_t modifyDateCheat(int32_t year, int32_t month, int32_t day)
         {
@@ -184,83 +184,83 @@ namespace OpenLoco::GameCommands
         }
     }
 
-	static uint32_t cheat(CheatCommand command, int32_t param1, int32_t param2, int32_t param3)
-	{
-		switch (command)
-		{
-		case CheatCommand::acquireAssets:
-			return Cheats::acquireAssets(CompanyId(param1));
+    static uint32_t cheat(CheatCommand command, int32_t param1, int32_t param2, int32_t param3)
+    {
+        switch (command)
+        {
+            case CheatCommand::acquireAssets:
+                return Cheats::acquireAssets(CompanyId(param1));
 
-		case CheatCommand::addCash:
-			return Cheats::addCash(param1);
+            case CheatCommand::addCash:
+                return Cheats::addCash(param1);
 
-		case CheatCommand::clearLoan:
-			return Cheats::clearLoan();
+            case CheatCommand::clearLoan:
+                return Cheats::clearLoan();
 
-		case CheatCommand::companyRatings:
-			return Cheats::companyRatings(param1, param2);
+            case CheatCommand::companyRatings:
+                return Cheats::companyRatings(param1, param2);
 
-		case CheatCommand::switchCompany:
-			return Cheats::switchCompany(CompanyId(param1));
+            case CheatCommand::switchCompany:
+                return Cheats::switchCompany(CompanyId(param1));
 
-		case CheatCommand::toggleBankruptcy:
-			return Cheats::toggleBankruptcy(CompanyId(param1));
+            case CheatCommand::toggleBankruptcy:
+                return Cheats::toggleBankruptcy(CompanyId(param1));
 
-		case CheatCommand::toggleJail:
-			return Cheats::toggleJail(CompanyId(param1));
+            case CheatCommand::toggleJail:
+                return Cheats::toggleJail(CompanyId(param1));
 
-		case CheatCommand::vehicleReliability:
-			return Cheats::vehicleReliability(param1);
+            case CheatCommand::vehicleReliability:
+                return Cheats::vehicleReliability(param1);
 
-		case CheatCommand::modifyDate:
-			return Cheats::modifyDateCheat(param1, param2, param3);
+            case CheatCommand::modifyDate:
+                return Cheats::modifyDateCheat(param1, param2, param3);
 
-		default:
-			break;
-		}
+            default:
+                break;
+        }
 
-		return 0;
-	}
+        return 0;
+    }
 
-	void cheat(registers& regs)
-	{
-		regs.ebx = cheat(CheatCommand(regs.eax), regs.ebx, regs.ecx, regs.edx);
-	}
+    void cheat(registers& regs)
+    {
+        regs.ebx = cheat(CheatCommand(regs.eax), regs.ebx, regs.ecx, regs.edx);
+    }
 
-	// 0x004BAC53
-	static uint32_t vehicleShuntCheat(EntityId head, uint8_t flags)
-	{
-		if (flags & Flags::apply)
-		{
-			EntityManager::get<Vehicles::VehicleHead>(head)->var_0C |= Vehicles::Flags0C::shuntCheat;
-		}
-		return 0;
-	}
+    // 0x004BAC53
+    static uint32_t vehicleShuntCheat(EntityId head, uint8_t flags)
+    {
+        if (flags & Flags::apply)
+        {
+            EntityManager::get<Vehicles::VehicleHead>(head)->var_0C |= Vehicles::Flags0C::shuntCheat;
+        }
+        return 0;
+    }
 
-	void vehicleShuntCheat(registers& regs)
-	{
-		VehicleApplyShuntCheatArgs args(regs);
-		regs.ebx = vehicleShuntCheat(args.head, regs.bl);
-	}
+    void vehicleShuntCheat(registers& regs)
+    {
+        VehicleApplyShuntCheatArgs args(regs);
+        regs.ebx = vehicleShuntCheat(args.head, regs.bl);
+    }
 
-	// 0x00438A08
-	static uint32_t freeCashCheat(uint8_t flags)
-	{
-		if (flags & Flags::apply)
-		{
-			auto* company = CompanyManager::get(_updatingCompanyId);
-			company->jail_status = 30;
-			Ui::WindowManager::invalidate(Ui::WindowType::company, static_cast<Ui::WindowNumber_t>(*_updatingCompanyId));
-			Ui::WindowManager::invalidate(Ui::WindowType::news);
-			Ui::WindowManager::invalidate(static_cast<Ui::WindowType>(0x2E));
-			MessageManager::post(MessageType::companyCheated, CompanyId::null, static_cast<uint16_t>(*_updatingCompanyId), 0xFFFF);
-			StationManager::sub_437F29(_updatingCompanyId, 4);
-		}
-		return 0;
-	}
+    // 0x00438A08
+    static uint32_t freeCashCheat(uint8_t flags)
+    {
+        if (flags & Flags::apply)
+        {
+            auto* company = CompanyManager::get(_updatingCompanyId);
+            company->jail_status = 30;
+            Ui::WindowManager::invalidate(Ui::WindowType::company, static_cast<Ui::WindowNumber_t>(*_updatingCompanyId));
+            Ui::WindowManager::invalidate(Ui::WindowType::news);
+            Ui::WindowManager::invalidate(static_cast<Ui::WindowType>(0x2E));
+            MessageManager::post(MessageType::companyCheated, CompanyId::null, static_cast<uint16_t>(*_updatingCompanyId), 0xFFFF);
+            StationManager::sub_437F29(_updatingCompanyId, 4);
+        }
+        return 0;
+    }
 
-	void freeCashCheat(registers& regs)
-	{
-		regs.ebx = freeCashCheat(regs.bl);
-	}
+    void freeCashCheat(registers& regs)
+    {
+        regs.ebx = freeCashCheat(regs.bl);
+    }
 }
