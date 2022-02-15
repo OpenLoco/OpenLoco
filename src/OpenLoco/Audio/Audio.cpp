1be60dc49cdf7c420ddf2b46d189b540450ffd62
Benjamin Sutas <benjamin.sutas@gmail.com>
2022-02-16 00:27:34 +1100

    default first to 'currentEra' playlist if the playlist is initially empty

--- src/OpenLoco/Audio/Audio.cpp
+++ src/OpenLoco/Audio/Audio.cpp
@@ -1015,6 +1015,22 @@ namespace OpenLoco::Audio
         }
     }
 
+    static void addCurrentEraSongsToPlaylist(int32_t excludeTrack, std::vector<uint8_t>& playlist)
+    {
+        auto currentYear = getCurrentYear();
+        for (auto i = 0; i < kNumMusicTracks; i++)
+        {
+            const auto& mi = kMusicInfo[i];
+            if (currentYear >= mi.startYear && currentYear <= mi.endYear)
+            {
+                if (i != excludeTrack)
+                {
+                    playlist.push_back(i);
+                }
+            }
+        }
+    }
+
     static int32_t chooseNextMusicTrack(int32_t excludeTrack)
     {
         using MusicPlaylistType = Config::MusicPlaylistType;
@@ -1026,21 +1042,8 @@ namespace OpenLoco::Audio
         switch (cfg.music_playlist)
         {
             case MusicPlaylistType::currentEra:
-            {
-                auto currentYear = getCurrentYear();
-                for (auto i = 0; i < kNumMusicTracks; i++)
-                {
-                    const auto& mi = kMusicInfo[i];
-                    if (currentYear >= mi.startYear && currentYear <= mi.endYear)
-                    {
-                        if (i != excludeTrack)
-                        {
-                            playlist.push_back(i);
-                        }
-                    }
-                }
+                addCurrentEraSongsToPlaylist(excludeTrack, playlist);
                 break;
-            }
             case MusicPlaylistType::all:
                 addAllSongsToPlaylist(excludeTrack, playlist);
                 break;
@@ -1055,7 +1058,12 @@ namespace OpenLoco::Audio
                 break;
         }
 
-        if (playlist.size() == 0)
+        if (playlist.empty() && cfg.music_playlist != MusicPlaylistType::currentEra)
+        {
+            addCurrentEraSongsToPlaylist(excludeTrack, playlist);
+        }
+
+        if (playlist.empty())
         {
             addAllSongsToPlaylist(excludeTrack, playlist);
         }
