ab8f0bab36b47757f8002f1f30662494b945eca3
Benjamin Sutas <benjamin.sutas@gmail.com>
2022-02-19 11:01:41 +1100

    adds working unit test in msvc

---
+++ test/main.cpp
@@ -0,0 +1,11 @@
+#ifdef _MSC_VER
+
+#include <gtest/gtest.h>
+
+int main(int argc, char** argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+#endif
---
+++ test/test.cpp
@@ -0,0 +1,11 @@
+#include "gtest/gtest.h"
+
+namespace Testing
+{
+    TEST(TestCaseName, TestName)
+    {
+        EXPECT_EQ(1, 1);
+        EXPECT_TRUE(true);
+        ASSERT_TRUE(true);
+    }
+}
