be5d60aad8b5feee97c8e7e08bbb492c18f30822
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2022-03-01 12:40:29 +0000

    Split up road/rail

--- src/OpenLoco/Vehicles/Vehicle1.cpp
+++ src/OpenLoco/Vehicles/Vehicle1.cpp
@@ -8,9 +8,35 @@ namespace OpenLoco::Vehicles
     // 0x004A9788
     bool Vehicle1::update()
     {
+        switch (mode)
+        {
+            case TransportMode::air:
+            case TransportMode::water:
+                return true;
+            case TransportMode::road:
+                return updateRoad();
+            case TransportMode::rail:
+                return updateRail();
+            default:
+                return false;
+        }
+    }
+
+    // 0x004A9969
+    bool Vehicle1::updateRoad()
+    {
         registers regs;
         regs.esi = X86Pointer(this);
 
-        return !(call(0x004A9788, regs) & X86_FLAG_CARRY);
+        return !(call(0x004A9969, regs) & X86_FLAG_CARRY);
+    }
+
+    // 0x004A97A6
+    bool Vehicle1::updateRail()
+    {
+        registers regs;
+        regs.esi = X86Pointer(this);
+
+        return !(call(0x004A97A6, regs) & X86_FLAG_CARRY);
     }
 }
