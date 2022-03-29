292975017fcab180293cf93d7179e1333f50058b
Duncan <duncans_pumpkin@hotmail.co.uk>
2022-03-29 11:59:52 +0100

    Implement getVehicleSoundParameters (#1352)
    * Look at vehicle audio calcs
    
    * Finish implementing function
    
    * Refactor to remove duplication
    
    * Reduce duplication
    
    * Apply review comments

--- src/OpenLoco/Audio/VehicleChannel.cpp
+++ src/OpenLoco/Audio/VehicleChannel.cpp
@@ -1,18 +1,93 @@
 #include "VehicleChannel.h"
 #include "../Entities/EntityManager.h"
 #include "../Interop/Interop.hpp"
+#include "../Map/TileManager.h"
+#include "../Ui/WindowManager.h"
 #include "../Vehicles/Vehicle.h"
 
 using namespace OpenLoco::Interop;
 
 namespace OpenLoco::Audio
 {
-    static std::pair<SoundId, Channel::Attributes> sub_48A590(const Vehicles::Vehicle2or6* v)
+    // Vehicle Volume is multiplied by the modifier then divided by 8 to get in terms of hundredth decibels
+    // 255 represents full volume
+    // 0 represents no volume
+    constexpr int8_t kVolumeModifierZoomIncrement = -35; // 13.7% decrease in volume for each zoom increment (up to 2)
+    constexpr int8_t kVolumeModifierUnderground = -28;   // 11.0% decrease in volume when underground
+    constexpr uint8_t kVolumeModifierMax = 255;
+
+    constexpr int32_t kVolumeMin = -100'00; // hundredth decibels (-100dB)
+    // Calculated min is 255*255/8=8128 but original has done 256*256/8=8192
+    // We have kept original value but could be changed to correctly represent the full range of volume
+    constexpr int32_t kVehicleVolumeCalcMin = -81'91; // hundredth decibels (-81.91dB)
+
+    constexpr int32_t kPanFalloffStart = 2048;
+    constexpr int32_t kPanFalloffEnd = 3072;
+
+    static int8_t getZoomVolumeModifier(uint8_t zoom)
     {
-        registers regs;
-        regs.esi = X86Pointer(v);
-        call(0x0048A590, regs);
-        return { static_cast<SoundId>(regs.eax), { regs.ecx, regs.edx, regs.ebx } };
+        return std::min<uint8_t>(zoom, 2) * kVolumeModifierZoomIncrement;
+    }
+
+    static int8_t getUndergroundVolumeModifier(const Map::Pos3& pos)
+    {
+        if (pos.x != Location::null && Map::validCoords(pos))
+        {
+            return 0;
+        }
+        auto* surface = Map::TileManager::get(pos).surface();
+        if (surface->baseZ() * 4 > pos.z)
+        {
+            return kVolumeModifierUnderground;
+        }
+        return 0;
+    }
+
+    static uint8_t getFalloffModifier(int32_t pan)
+    {
+        const auto absPan = std::abs(pan);
+
+        uint8_t falloffModifier = kVolumeModifierMax;
+        // This in theory is the max viewport width/height (might not be valid for modern screens)
+        if (absPan > kPanFalloffStart)
+        {
+            if (absPan > kPanFalloffEnd)
+            {
+                falloffModifier = 0;
+            }
+            else
+            {
+                falloffModifier = static_cast<uint8_t>(std::min<uint16_t>((kPanFalloffEnd - absPan) / 4, kVolumeModifierMax));
+            }
+        }
+        return falloffModifier;
+    }
+
+    // 0x0048A590
+    static std::pair<SoundId, Channel::Attributes> getChannelAttributesFromVehicle(const Vehicles::Vehicle2or6* v)
+    {
+        auto* w = Ui::WindowManager::find(v->soundWindowType, v->soundWindowNumber);
+        auto* viewport = w->viewports[0];
+        const auto uiPoint = viewport->viewportToScreen({ v->sprite_left, v->sprite_top });
+
+        const auto zoomVolumeModifier = getZoomVolumeModifier(viewport->zoom);
+
+        const auto panX = calculatePan(uiPoint.x, Ui::width());
+        const auto panY = calculatePan(uiPoint.y, Ui::height());
+
+        const auto undergroundVolumeModifier = getUndergroundVolumeModifier(v->position);
+
+        const auto xFalloffModifier = getFalloffModifier(panX);
+        const auto yFalloffModifier = getFalloffModifier(panY);
+
+        const auto falloffVolumeModifier = std::min(xFalloffModifier, yFalloffModifier);
+
+        const auto overallVolumeModifier = std::max(falloffVolumeModifier + undergroundVolumeModifier + zoomVolumeModifier, 0);
+
+        // volume is in hundredth decibels max decrease in volume is -100dB.
+        const auto volume = std::min(((v->drivingSoundVolume * overallVolumeModifier) / 8) - kVehicleVolumeCalcMin, kVolumeMin);
+
+        return { makeObjectSoundId(v->drivingSoundId), { volume, panX, v->drivingSoundFrequency } };
     }
 
     void VehicleChannel::begin(EntityId vid)
@@ -28,7 +103,7 @@ namespace OpenLoco::Audio
             return;
         }
 
-        auto [sid, sa] = sub_48A590(veh26);
+        auto [sid, sa] = getChannelAttributesFromVehicle(veh26);
         auto loop = Audio::shouldSoundLoop(sid);
         auto sample = Audio::getSoundSample(sid);
         if (sample)
@@ -64,7 +139,7 @@ namespace OpenLoco::Audio
             return;
         }
 
-        auto [sid, sa] = sub_48A590(veh26);
+        auto [sid, sa] = getChannelAttributesFromVehicle(veh26);
         if (_soundId != sid)
         {
             stop();
