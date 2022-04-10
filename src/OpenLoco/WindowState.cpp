90a7d96f4987fc8bd5075a09656817f93069415c
Benjamin Sutas <benjamin.sutas@gmail.com>
2022-04-10 14:36:46 +1000

    start adding getter/setters

--- src/OpenLoco/WindowState.cpp
+++ src/OpenLoco/WindowState.cpp
@@ -1,10 +1,66 @@
 #include "./GameState.h"
 #include "./LastWindowState.h"
 
-namespace OpenLoco
+namespace OpenLoco::GameState::LastOption
 {
-    LastWindowState& getLastWindowState()
+    // 0x00525FAF
+    VehicleType getLastVehicleType()
     {
-        return getGameState().lastWindowState;
+        return getGameState().lastVehicleType;
+    }
+
+    void setLastVehicleType(VehicleType& last)
+    {
+        getGameState().lastVehicleType = last;
+    }
+
+    // 0x00525FC7
+    uint8_t getLastIndustryOption()
+    {
+        return getGameState().lastIndustryOption;
+    }
+    void setLastIndustryOption(uint8_t last)
+    {
+        getGameState().lastIndustryOption = last;
+    }
+
+    // 0x00525FC8
+    uint8_t getLastBuildingOption()
+    {
+        return getGameState().lastBuildingOption;
+    }
+    void setLastBuildingOption(uint8_t last)
+    {
+        getGameState().lastBuildingOption = last;
+    }
+
+    // 0x00525FC9
+    uint8_t getLastMiscBuildingOption()
+    {
+        return getGameState().lastMiscBuildingOption;
+    }
+    void setLastMiscBuildingOption(uint8_t last)
+    {
+        getGameState().lastMiscBuildingOption = last;
+    }
+
+    // 0x00525FCA
+    uint8_t getLastWallOption()
+    {
+        return getGameState().lastWallOption;
+    }
+    void setLastWallOption(uint8_t last)
+    {
+        getGameState().lastWallOption = last;
+    }
+
+    // 0x0052622C
+    uint8_t getLastBuildVehiclesOption()
+    {
+        return getGameState().lastBuildVehiclesOption;
+    }
+    void setLastBuildVehiclesOption(uint8_t last)
+    {
+        getGameState().lastBuildVehiclesOption = last;
     }
 }
