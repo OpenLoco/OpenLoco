f9017dbb4f7ee581ac00d3b6b9f5c4fe3708bd74
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2022-04-14 21:29:23 +0100

    Simplify colour map and use paletteIndexs

--- src/OpenLoco/Graphics/Colour.cpp
+++ src/OpenLoco/Graphics/Colour.cpp
@@ -53,23 +53,26 @@ namespace OpenLoco::Colours
 
         for (uint32_t i = 0; i < 31; i++)
         {
-            assert(i + 2170 < 2201);
-            auto image = Gfx::getG1Element(2170 + i);
-            _colour_map_a[i][0] = image->offset[9];
+            const auto c = static_cast<Colour>(i);
+            auto paletteMap = Gfx::getPaletteMapForColour(toExt(c));
+            if (!paletteMap)
+            {
+                continue;
+            }
+            auto& map = *paletteMap;
+            _colour_map_a[i][0] = map[PaletteIndex::primaryRemap2];
 
-            _colour_map_a[i][1] = image->offset[246];
-            _colour_map_a[i][2] = image->offset[247];
-            _colour_map_a[i][3] = image->offset[248];
+            _colour_map_a[i][1] = map[PaletteIndex::primaryRemap3];
+            _colour_map_a[i][2] = map[PaletteIndex::primaryRemap4];
+            _colour_map_a[i][3] = map[PaletteIndex::primaryRemap5];
 
-            _colour_map_a[i][4] = image->offset[249];
-            _colour_map_a[i][5] = image->offset[250];
-            _colour_map_a[i][6] = image->offset[251];
-            _colour_map_a[i][7] = image->offset[252];
+            _colour_map_a[i][4] = map[PaletteIndex::primaryRemap6];
+            _colour_map_a[i][5] = map[PaletteIndex::primaryRemap7];
+            _colour_map_a[i][6] = map[PaletteIndex::primaryRemap8];
+            _colour_map_a[i][7] = map[PaletteIndex::primaryRemap9];
 
-            _colour_map_b[i][8 - 8] = image->offset[253];
-            _colour_map_b[i][9 - 8] = image->offset[254];
-            _colour_map_b[i][10 - 8] = image->offset[255];
-            _colour_map_b[i][11 - 8] = image->offset[256];
+            _colour_map_b[i][8 - 8] = map[PaletteIndex::primaryRemapA];
+            _colour_map_b[i][9 - 8] = map[PaletteIndex::primaryRemapB];
         }
     }
 
