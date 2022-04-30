OpenLoco/OpenLoco

commit a41ba0ba6c4aa8b8716adb602593a3582a859a13

Port over OpenRCT2 ImageId class (#1468)
* Port OpenRCT2 ImageId class

* Add further constexpr

* Use class for functions

* Add further users of imageId class

* Get compiling

* Fix issue with withIndex

src/OpenLoco/Graphics/Gfx.cpp
@@ -46,6 +46,7 @@ namespace OpenLoco::Gfx
     static loco_global<int16_t, 0x112C876> _currentFontSpriteBase;
     static loco_global<uint8_t[224 * 4], 0x112C884> _characterWidths;
     static loco_global<AdvancedColour[4], 0x1136594> _windowColours;
+    loco_global<uint32_t, 0x00E04324> _E04324;
 
     static PaletteIndex_t _textColours[8] = { 0 };
 
@@ -694,20 +695,20 @@ namespace OpenLoco::Gfx
                 case ControlCodes::inline_sprite_str:
                 {
                     uint32_t image = ((uint32_t*)str)[0];
-                    uint32_t imageId = image & 0x7FFFF;
+                    ImageId imageId{ image & 0x7FFFF };
                     str += 4;
 
                     if ((_currentFontFlags & TextDrawFlags::inset) != 0)
                     {
-                        Gfx::drawImageSolid(context, pos.x, pos.y, imageId, _textColours[3]);
-                        Gfx::drawImageSolid(context, pos.x + 1, pos.y + 1, imageId, _textColours[1]);
+                        Gfx::drawImageSolid(*context, pos, imageId, _textColours[3]);
+                        Gfx::drawImageSolid(*context, pos + Ui::Point{ 1, 1 }, imageId, _textColours[1]);
                     }
                     else
                     {
                         Gfx::drawImage(context, pos.x, pos.y, image);
                     }
 
-                    pos.x += _g1Elements[imageId].width;
+                    pos.x += _g1Elements[imageId.getIndex()].width;
                     break;
                 }
 
@@ -773,7 +774,8 @@ namespace OpenLoco::Gfx
                         // When offscreen in the y dimension there is no requirement to keep pos.x correct
                         if (chr >= 32)
                         {
-                            Gfx::drawImagePaletteSet(context, pos.x, pos.y, 1116 + chr - 32 + _currentFontSpriteBase, _textColours);
+                            _E04324 = 0x20000000;
+                            Gfx::drawImagePaletteSet(*context, pos, ImageId(1116 + chr - 32 + _currentFontSpriteBase), PaletteMap{ _textColours });
                             pos.x += _characterWidths[chr - 32 + _currentFontSpriteBase];
                         }
                         else
@@ -1358,6 +1360,7 @@ namespace OpenLoco::Gfx
         redrawScreenRect(Rect::fromLTRB(left, top, right, bottom));
     }
 
+    // 0x00448C79
     void drawImage(Gfx::Context* context, int16_t x, int16_t y, uint32_t image)
     {
         registers regs;
@@ -1366,6 +1369,11 @@ namespace OpenLoco::Gfx
         regs.ebx = image;
         regs.edi = X86Pointer(context);
         call(0x00448C79, regs);
+    }
+
+    void drawImage(Gfx::Context& context, const Ui::Point& pos, const ImageId& image)
+    {
+        drawImage(&context, pos.x, pos.y, image.toUInt32());
     }
 
     uint32_t recolour(uint32_t image)
@@ -1398,27 +1406,31 @@ namespace OpenLoco::Gfx
     }
 
     loco_global<uint8_t*, 0x0050B860> _50B860;
-    loco_global<uint32_t, 0x00E04324> _E04324;
 
-    void drawImageSolid(Gfx::Context* context, int16_t x, int16_t y, uint32_t image, uint8_t palette_index)
+    void drawImageSolid(Gfx::Context& context, const Ui::Point& pos, const ImageId& image, PaletteIndex_t paletteIndex)
     {
         uint8_t palette[256];
-        memset(palette, palette_index, 256);
+        memset(palette, paletteIndex, 256);
         palette[0] = 0;
 
-        drawImagePaletteSet(context, x, y, image, palette);
+        drawImagePaletteSet(context, pos, image, PaletteMap{ palette });
     }
 
-    void drawImagePaletteSet(Gfx::Context* context, int16_t x, int16_t y, uint32_t image, uint8_t* palette)
+    // 0x00448D90
+    static void drawImagePaletteSet(Gfx::Context* context, int16_t x, int16_t y, uint32_t image, uint8_t* palette)
     {
         _50B860 = palette;
-        _E04324 = 0x20000000;
         registers regs;
         regs.cx = x;
         regs.dx = y;
         regs.ebx = image;
         regs.edi = X86Pointer(context);
         call(0x00448D90, regs);
+    }
+
+    void drawImagePaletteSet(Gfx::Context& context, const Ui::Point& pos, const ImageId& image, const PaletteMap& palette)
+    {
+        drawImagePaletteSet(&context, pos.x, pos.y, image.toUInt32(), palette.data());
     }
 
     // 0x004CEC50

src/OpenLoco/Graphics/Gfx.h
@@ -5,6 +5,7 @@
 #include "../Types.hpp"
 #include "../Ui/Rect.h"
 #include "../Ui/Types.hpp"
+#include "ImageId.h"
 #include <cstdint>
 
 namespace OpenLoco
@@ -240,8 +241,9 @@ namespace OpenLoco::Gfx
     void drawRectInset(Gfx::Context& context, int16_t x, int16_t y, uint16_t dx, uint16_t dy, uint32_t colour, uint8_t flags);
     void drawLine(Gfx::Context& context, int16_t left, int16_t top, int16_t right, int16_t bottom, uint32_t colour);
     void drawImage(Gfx::Context* context, int16_t x, int16_t y, uint32_t image);
-    void drawImageSolid(Gfx::Context* context, int16_t x, int16_t y, uint32_t image, uint8_t palette_index);
-    void drawImagePaletteSet(Gfx::Context* context, int16_t x, int16_t y, uint32_t image, uint8_t* palette);
+    void drawImage(Gfx::Context& context, const Ui::Point& pos, const ImageId& image);
+    void drawImageSolid(Gfx::Context& context, const Ui::Point& pos, const ImageId& image, PaletteIndex_t paletteIndex);
+    void drawImagePaletteSet(Gfx::Context& context, const Ui::Point& pos, const ImageId& image, const PaletteMap& palette);
     [[nodiscard]] uint32_t recolour(uint32_t image);
     [[nodiscard]] uint32_t recolour(uint32_t image, Colour colour);
     [[nodiscard]] uint32_t recolour(uint32_t image, ExtColour colour);

src/OpenLoco/Graphics/ImageId.h
@@ -1,0 +1,179 @@
+#include "Colour.h"
+
+namespace OpenLoco
+{
+
+    using ImageIndex = uint32_t;
+
+    /**
+     * Represents a specific image from a catalogue such as G1, object etc. with remap
+     * colours and flags.
+     *
+     * This is currently all stored as a single 32-bit integer, but will allow easy
+     * extension to 64-bits or higher so that more images can be used.
+     */
+    struct ImageId
+    {
+    private:
+        // clang-format off
+        static constexpr uint32_t kMaskIndex       = 0b00000000000001111111111111111111;
+        static constexpr uint32_t kMaskRemap       = 0b00000111111110000000000000000000;
+        static constexpr uint32_t kMaskPrimary     = 0b00000000111110000000000000000000;
+        static constexpr uint32_t kMaskSecondary   = 0b00011111000000000000000000000000;
+        static constexpr uint32_t kFlagPrimary     = 0b00100000000000000000000000000000;
+        static constexpr uint32_t kFlagBlend       = 0b01000000000000000000000000000000;
+        static constexpr uint32_t kFlagSecondary   = 0b10000000000000000000000000000000;
+        static constexpr uint32_t kShiftRemap      = 19;
+        static constexpr uint32_t kShiftPrimary    = 19;
+        static constexpr uint32_t kShiftSecondary  = 24;
+        static constexpr uint32_t kIndexUndefined  = 0b00000000000001111111111111111111;
+        static constexpr uint32_t kValueUndefined  = kIndexUndefined;
+        // clang-format on
+
+        // NONE = No remap
+        // BLENDED = No source copy, remap destination only (glass)
+        // PRIMARY | BLENDED = Destination is blended with source (water)
+        // PRIMARY = Remap with palette id (first 32 are colour palettes)
+        // PRIMARY | SECONDARY = Remap with primary and secondary colours
+
+        static constexpr ImageIndex kImageIndexUndefined = std::numeric_limits<ImageIndex>::max();
+        uint32_t _index = kImageIndexUndefined;
+
+    public:
+        static ImageId fromUInt32(uint32_t value)
+        {
+            ImageId result;
+            result._index = value;
+            return result;
+        }
+
+        ImageId() = default;
+
+        explicit constexpr ImageId(ImageIndex index)
+            : _index(index == kIndexUndefined ? kImageIndexUndefined : index)
+        {
+        }
+
+        constexpr ImageId(uint32_t index, ExtColour palette)
+            : ImageId(ImageId(index).withRemap(palette))
+        {
+        }
+
+        constexpr ImageId(uint32_t index, Colour primaryColour)
+            : ImageId(ImageId(index).withPrimary(primaryColour))
+        {
+        }
+
+        constexpr ImageId(uint32_t index, Colour primaryColour, Colour secondaryColour)
+            : ImageId(ImageId(index).withPrimary(primaryColour).withSecondary(secondaryColour))
+        {
+        }
+
+        constexpr ImageId(uint32_t index, ColourScheme scheme)
+            : ImageId(index, scheme.primary, scheme.secondary)
+        {
+        }
+
+        constexpr uint32_t toUInt32() const
+        {
+            return _index;
+        }
+
+        constexpr bool hasValue() const
+        {
+            return getIndex() != kImageIndexUndefined;
+        }
+
+        constexpr bool hasPrimary() const
+        {
+            return _index & kFlagPrimary;
+        }
+
+        constexpr bool hasSecondary() const
+        {
+            return _index & kFlagSecondary;
+        }
+
+        constexpr bool isRemap() const
+        {
+            return (_index & kFlagPrimary) && !(_index & kFlagSecondary);
+        }
+
+        constexpr bool isBlended() const
+        {
+            return _index & kFlagBlend;
+        }
+
+        constexpr ImageIndex getIndex() const
+        {
+            return _index & kMaskIndex;
+        }
+
+        constexpr ExtColour getRemap() const
+        {
+            return static_cast<ExtColour>((_index & kMaskRemap) >> kShiftRemap);
+        }
+
+        constexpr Colour getPrimary() const
+        {
+            return static_cast<Colour>((_index & kMaskPrimary) >> kShiftPrimary);
+        }
+
+        constexpr Colour getSecondary() const
+        {
+            return static_cast<Colour>((_index & kMaskSecondary) >> kShiftSecondary);
+        }
+
+        constexpr ImageId withIndex(ImageIndex index) const
+        {
+            ImageId result = *this;
+            result._index &= ~kMaskIndex;
+            result._index |= index;
+            return result;
+        }
+
+        constexpr ImageId withIndexOffset(ImageIndex offset) const
+        {
+            ImageId result = *this;
+            result._index += offset;
+            return result;
+        }
+
+        constexpr ImageId withRemap(ExtColour paletteId) const
+        {
+            ImageId result = *this;
+            result._index &= ~(kMaskPrimary | kMaskSecondary | kFlagSecondary | kFlagBlend);
+            result._index |= enumValue(paletteId) << kShiftRemap;
+            result._index |= kFlagPrimary;
+            return result;
+        }
+
+        constexpr ImageId withPrimary(Colour colour) const
+        {
+            ImageId result = *this;
+            result._index &= ~kMaskPrimary;
+            result._index |= enumValue(colour) << kShiftPrimary;
+            result._index |= kFlagPrimary;
+            return result;
+        }
+
+        constexpr ImageId withSecondary(Colour colour) const
+        {
+            ImageId result = *this;
+            result._index &= ~kMaskSecondary;
+            result._index |= enumValue(colour) << kShiftSecondary;
+            result._index |= kFlagSecondary;
+            return result;
+        }
+
+        constexpr ImageId withTranslucency(ExtColour colour) const
+        {
+            ImageId result = *this;
+            result._index &= ~(kMaskPrimary | kMaskSecondary | kFlagSecondary | kFlagPrimary);
+            result._index |= enumValue(colour) << kShiftRemap;
+            result._index |= kFlagBlend;
+            return result;
+        }
+    };
+    static_assert(sizeof(ImageId) == 4);
+}

src/OpenLoco/Widget.cpp
@@ -226,61 +226,60 @@ namespace OpenLoco::Ui
 
     void Widget::sub_4CADE8(Gfx::Context* context, const Window* window, AdvancedColour colour, bool enabled, bool disabled, bool activated)
     {
-        int16_t xPlaceForImage = left + window->x;
-        int16_t yPlaceForImage = top + window->y;
-        uint32_t imageId = image;
+        Ui::Point placeForImage(left + window->x, top + window->y);
+        const bool isColourSet = image & Widget::imageIdColourSet;
+        ImageId imageId = ImageId::fromUInt32(image & ~Widget::imageIdColourSet);
         if (type == WidgetType::wt_6 || type == WidgetType::toolbarTab || type == WidgetType::tab || type == WidgetType::wt_4)
         {
             if (activated)
             {
                 // TODO: remove image addition
-                imageId++;
+                imageId = imageId.withIndexOffset(1);
             }
         }
 
         if (disabled)
         {
-            if (imageId & (1 << 31))
+            // TODO: this is odd most likely this is another flag like Widget::imageIdColourSet
+            if (imageId.hasSecondary())
             {
                 return;
             }
 
-            imageId &= 0x7FFFF;
+            // No colour applied image
+            const auto pureImage = ImageId{ imageId.getIndex() };
             uint8_t c;
             if (colour.isTranslucent())
             {
                 c = Colours::getShade(colour.c(), 4);
-                Gfx::drawImageSolid(context, xPlaceForImage + 1, yPlaceForImage + 1, imageId, c);
+                Gfx::drawImageSolid(*context, placeForImage + Ui::Point{ 1, 1 }, pureImage, c);
                 c = Colours::getShade(colour.c(), 2);
-                Gfx::drawImageSolid(context, xPlaceForImage, yPlaceForImage, imageId, c);
+                Gfx::drawImageSolid(*context, placeForImage, pureImage, c);
             }
             else
             {
                 c = Colours::getShade(colour.c(), 6);
-                Gfx::drawImageSolid(context, xPlaceForImage + 1, yPlaceForImage + 1, imageId, c);
+                Gfx::drawImageSolid(*context, placeForImage + Ui::Point{ 1, 1 }, pureImage, c);
                 c = Colours::getShade(colour.c(), 4);
-                Gfx::drawImageSolid(context, xPlaceForImage, yPlaceForImage, imageId, c);
+                Gfx::drawImageSolid(*context, placeForImage, pureImage, c);
             }
 
             return;
         }
 
-        if (imageId & (1 << 31))
+        // TODO: this is odd most likely this is another flag like Widget::imageIdColourSet
+        if (imageId.hasSecondary())
         {
             // 0x4CAE5F
             assert(false);
         }
 
-        if ((imageId & Widget::imageIdColourSet) == 0)
+        if (!isColourSet && imageId.hasPrimary())
         {
-            imageId |= enumValue(colour.c()) << 19;
-        }
-        else
-        {
-            imageId &= ~Widget::imageIdColourSet;
+            imageId = imageId.withPrimary(colour.c());
         }
 
-        Gfx::drawImage(context, xPlaceForImage, yPlaceForImage, imageId);
+        Gfx::drawImage(*context, placeForImage, imageId);
     }
 
     // 0x004CAB58
@@ -390,19 +389,16 @@ namespace OpenLoco::Ui
             return;
         }
 
+        const bool isColourSet = image & Widget::imageIdColourSet;
         // TODO: Remove addedImage addition
-        uint32_t addedImage = image + 2;
+        ImageId imageId = ImageId::fromUInt32(image & ~Widget::imageIdColourSet).withIndexOffset(2);
 
-        if ((addedImage & Widget::imageIdColourSet) == 0)
+        if (!isColourSet && imageId.hasPrimary())
         {
-            addedImage |= enumValue(colour.c()) << 19;
-        }
-        else
-        {
-            addedImage &= ~Widget::imageIdColourSet;
+            imageId = imageId.withPrimary(colour.c());
         }
 
-        Gfx::drawImage(context, window->x + left, window->y + top, addedImage);
+        Gfx::drawImage(*context, Ui::Point(window->x + left, window->y + top), imageId);
     }
 
     // 0x004CACD4
@@ -452,34 +448,27 @@ namespace OpenLoco::Ui
             return;
         }
 
-        uint32_t addedImage = image;
+        const bool isColourSet = image & Widget::imageIdColourSet;
+        // TODO: Remove addition
+        ImageId imageId = ImageId::fromUInt32(image & ~Widget::imageIdColourSet);
 
-        if (enabled)
+        if (enabled && activated && hovered)
         {
-            // TODO: Remove addedImage addition
-            addedImage += 2;
-
-            if (!activated)
-            {
-                addedImage -= 1;
-
-                if (!hovered)
-                {
-                    addedImage -= 1;
-                }
-            }
+            // TODO: Remove addition
+            imageId = imageId.withIndexOffset(2);
+        }
+        else if (enabled && !activated && hovered)
+        {
+            // TODO: Remove addition
+            imageId = imageId.withIndexOffset(1);
         }
 
-        if ((addedImage & Widget::imageIdColourSet) == 0)
+        if (!isColourSet && imageId.hasPrimary())
         {
-            addedImage |= enumValue(colour.c()) << 19;
-        }
-        else
-        {
-            addedImage &= ~Widget::imageIdColourSet;
+            imageId = imageId.withPrimary(colour.c());
         }
 
-        Gfx::drawImage(context, window->x + left, window->y + top, addedImage);
+        Gfx::drawImage(*context, Ui::Point(window->x + left, window->y + top), imageId);
     }
 
     // 0x004CB164

src/OpenLoco/openloco.vcxproj
@@ -274,6 +274,7 @@
     <ClInclude Include="GameState.h" />
     <ClInclude Include="Graphics\Colour.h" />
     <ClInclude Include="Graphics\Gfx.h" />
+    <ClInclude Include="Graphics\ImageId.h" />
     <ClInclude Include="Graphics\ImageIds.h" />
     <ClInclude Include="Gui.h" />
     <ClInclude Include="Industry.h" />

commit 05cd240ab551b1340ce31053363dbac2fd53e03a

Fix #1490. drawImageSolid not setting global variables correctly
Regression from #1468

src/OpenLoco/Graphics/Gfx.cpp
@@ -1413,6 +1413,8 @@ namespace OpenLoco::Gfx
         memset(palette, paletteIndex, 256);
         palette[0] = 0;
 
+        // Set the image primary flag to tell drawImagePaletteSet to recolour with the palette. TODO: This should be refactored out when drawImagePaletteSet implemented
+        _E04324 = 0x20000000;
         drawImagePaletteSet(context, pos, image, PaletteMap{ palette });
     }
 