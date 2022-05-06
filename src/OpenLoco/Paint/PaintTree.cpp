79ece570d4234d0f91342bcc8d521f19aa2430ea
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2022-05-06 23:23:39 +0100

    Fix further tree issue

--- src/OpenLoco/Paint/PaintTree.cpp
+++ src/OpenLoco/Paint/PaintTree.cpp
@@ -13,7 +13,7 @@ using namespace OpenLoco::Ui::ViewportInteraction;
 namespace OpenLoco::Paint
 {
     constexpr std::array<uint8_t, 6> _50076A = { 3, 0, 1, 2, 1, 4 };
-    constexpr std::array<bool, 5> _500770 = { true, true, false, false, true };
+    constexpr std::array<bool, 6> _500770 = { true, true, false, false, true, true };
     constexpr std::array<Map::Pos2, 4> kTreeQuadrantOffset = {
         Map::Pos2{ 7, 7 },
         Map::Pos2{ 7, 23 },
@@ -44,7 +44,7 @@ namespace OpenLoco::Paint
 
             auto image2Season = elTree.season();
 
-            if (!_500770[season])
+            if (_500770[image2Season])
             {
                 image2Season = season;
                 season = elTree.season();
