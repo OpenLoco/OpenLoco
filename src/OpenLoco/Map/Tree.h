00b0f895d8edb0f804d127d6a55c4be3d1c26da6
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2022-05-25 17:05:22 +0100

    Start moving tree funcs

---
+++ src/OpenLoco/Map/Tree.h
@@ -0,0 +1,8 @@
+#include "Map.hpp"
+#include <optional>
+
+namespace OpenLoco::Map
+{
+    std::optional<uint8_t> getRandomTreeTypeFromSurface(const Map::TilePos2& loc, bool unk);
+    bool placeTreeCluster(const Map::TilePos2& centreLoc, const uint16_t range, const uint16_t density, std::optional<uint8_t> treeType);
+}
