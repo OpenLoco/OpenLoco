d63dba5b399a4d6151ca2930b15e74d78d0a73ae
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2022-11-28 22:05:06 +0000

    Start implementing scenarioIndex

--- src/OpenLoco/src/ScenarioManager.cpp
+++ src/OpenLoco/src/ScenarioManager.cpp
@@ -1,18 +1,53 @@
 #include "ScenarioManager.h"
 #include "CompanyManager.h"
+#include "EditorController.h"
+#include "Environment.h"
 #include "GameState.h"
 #include "Interop/Interop.hpp"
+#include "Localisation/StringIds.h"
+#include "Localisation/StringManager.h"
+#include "Objects/ObjectManager.h"
+#include "Objects/ScenarioTextObject.h"
+#include "OpenLoco.h"
+#include "S5/S5.h"
+#include "SceneManager.h"
+#include "Ui.h"
+#include <OpenLoco/Utility/Stream.hpp>
+#include <fstream>
 
 using namespace OpenLoco::Interop;
 
 namespace OpenLoco::ScenarioManager
 {
+#pragma pack(push, 1)
+    struct ScenarioFolderState
+    {
+        uint32_t numFiles = 0;
+        uint64_t totalFileSize = 0;
+        constexpr bool operator==(const ScenarioFolderState& rhs) const
+        {
+            return ((numFiles & 0xFFFFFF) == (rhs.numFiles & 0xFFFFFF)) && (totalFileSize == rhs.totalFileSize);
+        }
+        constexpr bool operator!=(const ScenarioFolderState& rhs) const
+        {
+            return !(*this == rhs);
+        }
+    };
+    static_assert(sizeof(ScenarioFolderState) == 0xC);
+    struct ScoreHeader
+    {
+        ScenarioFolderState state;
+        uint32_t numScenarios; // Note this is a subset of state.numFiles
+    };
+    static_assert(sizeof(ScoreHeader) == 0x10);
+#pragma pack(pop)
+
     loco_global<ScenarioIndexEntry*, 0x0050AE8C> _scenarioList;
-    loco_global<int32_t, 0x0050AEA0> _scenarioCount;
+    loco_global<ScoreHeader, 0x0050AE04> _scenarioHeader;
 
     bool hasScenariosForCategory(uint8_t category)
     {
-        for (auto i = 0; i < _scenarioCount; i++)
+        for (uint32_t i = 0; i < _scenarioHeader->numScenarios; i++)
         {
             ScenarioIndexEntry& entry = _scenarioList[i];
             if (!entry.hasFlag(ScenarioIndexFlags::flag_0))
@@ -27,7 +62,7 @@ namespace OpenLoco::ScenarioManager
 
     bool hasScenarioInCategory(uint8_t category, ScenarioIndexEntry* scenario)
     {
-        for (auto i = 0; i < _scenarioCount; i++)
+        for (uint32_t i = 0; i < _scenarioHeader->numScenarios; i++)
         {
             ScenarioIndexEntry* entry = &_scenarioList[i];
 
@@ -50,7 +85,7 @@ namespace OpenLoco::ScenarioManager
     uint16_t getScenarioCountByCategory(uint8_t category)
     {
         auto scenarioCountInCategory = 0;
-        for (auto i = 0; i < _scenarioCount; i++)
+        for (uint32_t i = 0; i < _scenarioHeader->numScenarios; i++)
         {
             ScenarioIndexEntry& entry = _scenarioList[i];
             if (entry.category != category || !entry.hasFlag(ScenarioIndexFlags::flag_0))
@@ -65,7 +100,7 @@ namespace OpenLoco::ScenarioManager
     ScenarioIndexEntry* getNthScenarioFromCategory(uint8_t category, uint8_t index)
     {
         uint8_t j = 0;
-        for (auto i = 0; i < _scenarioCount; i++)
+        for (uint32_t i = 0; i < _scenarioHeader->numScenarios; i++)
         {
             ScenarioIndexEntry& entry = _scenarioList[i];
             if (entry.category != category || !entry.hasFlag(ScenarioIndexFlags::flag_0))
@@ -80,12 +115,201 @@ namespace OpenLoco::ScenarioManager
         return nullptr;
     }
 
+    // 0x00444574
+    static ScenarioFolderState getCurrentScenarioFolderState()
+    {
+        ScenarioFolderState currentState;
+        const auto scenarioPath = Environment::getPathNoWarning(Environment::PathId::scenarios);
+        for (const auto& file : fs::directory_iterator(scenarioPath, fs::directory_options::skip_permission_denied))
+        {
+            if (!file.is_regular_file())
+            {
+                continue;
+            }
+            currentState.numFiles++;
+            currentState.totalFileSize += file.file_size();
+        }
+        return currentState;
+    }
+
+    // 0x00444611
+    static bool tryLoadIndex(const ScenarioFolderState& currentState)
+    {
+        const auto scorePath = Environment::getPath(Environment::PathId::scores);
+        if (!fs::exists(scorePath))
+        {
+            return false;
+        }
+        std::ifstream stream;
+        stream.open(scorePath, std::ios::in | std::ios::binary);
+        if (!stream.is_open())
+        {
+            return false;
+        }
+        // 0x00112A14C -> 160
+        _scenarioHeader = ScoreHeader{};
+        Utility::readData(stream, *_scenarioHeader);
+        if (stream.gcount() != sizeof(ScoreHeader))
+        {
+            return false;
+        }
+
+        const auto scenarioListSize = _scenarioHeader->numScenarios * sizeof(ScenarioIndexEntry);
+        _scenarioList = static_cast<ScenarioIndexEntry*>(malloc(scenarioListSize));
+        if (_scenarioList == nullptr)
+        {
+            exitWithError(StringIds::unable_to_allocate_enough_memory, StringIds::game_init_failure);
+            return false;
+        }
+        Utility::readData(stream, *_scenarioList, _scenarioHeader->numScenarios);
+        if (stream.gcount() != static_cast<int32_t>(scenarioListSize))
+        {
+            _scenarioHeader = ScoreHeader{};
+            free(_scenarioList);
+            return false;
+        }
+        if (currentState != _scenarioHeader->state)
+        {
+            return false;
+        }
+        // version?
+        if ((_scenarioHeader->state.numFiles >> 24) != 1)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    static bool loadScenarioDetails(ScenarioIndexEntry& entry, S5::Options& options)
+    {
+        if ((options.scenarioText.flags & 0xFF) == 0xFF)
+        {
+            // Details are not loaded they are prepopulated
+            return true;
+        }
+
+        if (!ObjectManager::loadTemporaryObject(options.scenarioText))
+        {
+            return false;
+        }
+
+        auto* stex = reinterpret_cast<ScenarioTextObject*>(ObjectManager::getTemporaryObject());
+        StringManager::formatString(entry.scenarioName, stex->name);
+        StringManager::formatString(entry.description, stex->details);
+        ObjectManager::freeTemporaryObject();
+        return true;
+    }
+
+    // 0x004447DF
+    static void createIndex(const ScenarioFolderState& currentState)
+    {
+        if (_scenarioList == reinterpret_cast<ScenarioIndexEntry*>(-1) || _scenarioList == nullptr)
+        {
+            _scenarioHeader->numScenarios = 0;
+            _scenarioList = static_cast<ScenarioIndexEntry*>(malloc(currentState.numFiles * sizeof(ScenarioIndexEntry)));
+            if (_scenarioList == nullptr)
+            {
+                exitWithError(StringIds::unable_to_allocate_enough_memory, StringIds::game_init_failure);
+                return;
+            }
+            std::fill(*_scenarioList, *_scenarioList + currentState.numFiles, ScenarioIndexEntry{});
+        }
+        _scenarioHeader->state = currentState;
+        _scenarioHeader->state.numFiles = (currentState.numFiles & 0xFFFFFF) | (1 << 24);
+
+        for (uint32_t i = 0; i < _scenarioHeader->numScenarios; i++)
+        {
+            ScenarioIndexEntry& entry = _scenarioList[i];
+            entry.flags &= ~ScenarioIndexFlags::flag_0;
+        }
+
+        const auto scenarioPath = Environment::getPathNoWarning(Environment::PathId::scenarios);
+        for (const auto& file : fs::directory_iterator(scenarioPath, fs::directory_options::skip_permission_denied))
+        {
+            if (!file.is_regular_file())
+            {
+                continue;
+            }
+            Ui::processMessagesMini();
+
+            const auto u8FileName = file.path().filename().u8string();
+            bool entryFound = false;
+            uint32_t foundId = 0;
+            for (; foundId < _scenarioHeader->numScenarios; foundId++)
+            {
+                ScenarioIndexEntry& entry = _scenarioList[foundId];
+
+                if (std::strcmp(entry.filename, u8FileName.c_str()) == 0)
+                {
+                    entryFound = true;
+                    break;
+                }
+            }
+
+            const auto options = S5::readScenarioOptions(file.path());
+            if (options == nullptr)
+            {
+                continue;
+            }
+            if (options->editorStep != EditorController::Step::null)
+            {
+                continue;
+            }
+            ScenarioIndexEntry& entry = _scenarioList[entryFound ? foundId : _scenarioHeader->numScenarios];
+
+            entry.flags |= ScenarioIndexFlags::flag_0;
+            entry.category = options->difficulty;
+            entry.flags &= ~hasPreviewImage;
+            if (options->scenarioFlags & Scenario::Flags::landscapeGenerationDone)
+            {
+                entry.flags |= hasPreviewImage;
+                std::copy(&options->preview[0][0], &options->preview[0][0] + sizeof(options->preview), &entry.preview[0][0]);
+            }
+            entry.startYear = options->scenarioStartYear;
+            entry.numCompetingCompanies = options->maxCompetingCompanies;
+            entry.competingCompanyDelay = options->competitorStartDelay;
+
+            // sub_444C4E get/set objective set currency
+            if (entryFound)
+            {
+            }
+            else
+            {
+                _scenarioHeader->numScenarios++;
+                std::strcpy(entry.filename, u8FileName.c_str());
+            }
+            loadScenarioDetails(entry, *options);
+
+            // sort.
+        }
+    }
+
     // 0x0044452F
     void loadIndex(uint8_t al)
     {
-        registers regs;
-        regs.al = al;
-        call(0x0044452F, regs);
+        const auto oldFlags = getScreenFlags();
+        setAllScreenFlags(ScreenFlags::title | oldFlags);
+
+        if (_scenarioList != reinterpret_cast<ScenarioIndexEntry*>(-1) && _scenarioList != nullptr)
+        {
+            free(_scenarioList);
+        }
+
+        const auto currentState = getCurrentScenarioFolderState();
+
+        if (!tryLoadIndex(currentState))
+        {
+            createIndex(currentState);
+        }
+        else
+        {
+            if (al != 0)
+            {
+                createIndex(currentState);
+            }
+        }
+        setAllScreenFlags(oldFlags);
     }
 
     // 0x00438959
