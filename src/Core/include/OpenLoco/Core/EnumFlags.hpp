7d19eec6448be2ca7059b7f7b134901a6ee40a9b
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2022-12-22 09:02:39 +0200

    Add requirement for none value

--- src/Core/include/OpenLoco/Core/EnumFlags.hpp
+++ src/Core/include/OpenLoco/Core/EnumFlags.hpp
@@ -8,6 +8,7 @@ namespace OpenLoco::Core
 
 #define OPENLOCO_ENABLE_ENUM_OPERATORS(ENM)                                                                                 \
     static_assert(std::is_unsigned_v<std::underlying_type_t<ENM>> == true, "Underlying enum type must be unsignd");         \
+    static_assert(static_cast<std::underlying_type_t<ENM>>(ENM::none) == 0U, "Enum must have a none value");                \
     inline constexpr ENM operator|(const ENM a, const ENM b) noexcept                                                       \
     {                                                                                                                       \
         return static_cast<ENM>(static_cast<std::underlying_type_t<ENM>>(a) | static_cast<std::underlying_type_t<ENM>>(b)); \
