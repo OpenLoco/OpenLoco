9acd02180bfd1650d2b52a552e177ef3de6cf19e
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2022-12-29 21:02:57 +0000

    Implement airport object load

--- src/OpenLoco/src/Objects/AirportObject.cpp
+++ src/OpenLoco/src/Objects/AirportObject.cpp
@@ -2,7 +2,9 @@
 #include "Graphics/Colour.h"
 #include "Graphics/Gfx.h"
 #include "Interop/Interop.hpp"
+#include "ObjectImageTable.h"
 #include "ObjectManager.h"
+#include "ObjectStringTable.h"
 
 namespace OpenLoco
 {
@@ -40,11 +42,50 @@ namespace OpenLoco
     // 0x00490CAF
     void AirportObject::load(const LoadedObjectHandle& handle, stdx::span<const std::byte> data, ObjectManager::DependentObjects*)
     {
-        Interop::registers regs;
-        regs.esi = Interop::X86Pointer(this);
-        regs.ebx = handle.id;
-        regs.ecx = enumValue(handle.type);
-        Interop::call(0x00490CAF, regs);
+        auto remainingData = data.subspan(sizeof(AirportObject));
+
+        {
+            auto strRes = ObjectManager::loadStringTable(remainingData, handle, 0);
+            name = strRes.str;
+            remainingData = remainingData.subspan(strRes.tableLength);
+        }
+
+        var_14 = reinterpret_cast<const uint8_t*>(remainingData.data());
+        remainingData = remainingData.subspan(numSpriteSets * sizeof(uint8_t));
+        var_18 = reinterpret_cast<const uint16_t*>(remainingData.data());
+        remainingData = remainingData.subspan(numSpriteSets * sizeof(uint16_t));
+
+        for (auto i = 0; i < numTiles; ++i)
+        {
+            var_1C[i] = reinterpret_cast<const uint8_t*>(remainingData.data());
+            auto* ptr = var_1C[i];
+            while (*ptr++ != 0xFF)
+                ;
+            remainingData = remainingData.subspan(ptr - var_1C[i]);
+        }
+
+        var_9C = reinterpret_cast<const uint32_t*>(remainingData.data());
+        auto* ptr = reinterpret_cast<const uint8_t*>(remainingData.data());
+        while (*ptr != 0xFF)
+            ptr += 4;
+        ptr++;
+        remainingData = remainingData.subspan(ptr - reinterpret_cast<const uint8_t*>(var_9C));
+
+        movementNodes = reinterpret_cast<const MovementNode*>(remainingData.data());
+        remainingData = remainingData.subspan(numMovementNodes * sizeof(MovementNode));
+        movementEdges = reinterpret_cast<const MovementEdge*>(remainingData.data());
+        remainingData = remainingData.subspan(numMovementEdges * sizeof(MovementEdge));
+
+        auto imgRes = ObjectManager::loadImageTable(remainingData);
+        image = imgRes.imageOffset;
+        assert(remainingData.size() == imgRes.tableLength);
+
+        auto nextImage = image + 1;
+        if (allowedPlaneTypes & (1 << 0))
+        {
+            nextImage += numTiles * 4;
+        }
+        var_0C = nextImage;
     }
 
     // 0x00490D66
@@ -54,9 +95,9 @@ namespace OpenLoco
         image = 0;
         var_0C = 0;
 
-        std::fill(std::begin(var_1C), std::end(var_1C), 0);
+        std::fill(std::begin(var_1C), std::end(var_1C), nullptr);
 
-        var_9C = 0;
+        var_9C = nullptr;
 
         movementNodes = nullptr;
         movementEdges = nullptr;
