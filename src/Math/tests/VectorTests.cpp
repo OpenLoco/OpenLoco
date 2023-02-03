420b81cf4aa4a308ea3eb7ae3827a64f4ad813fc
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2023-02-03 19:34:29 +0000

    Add some values that require computation

--- src/Math/tests/VectorTests.cpp
+++ src/Math/tests/VectorTests.cpp
@@ -6,4 +6,8 @@ using namespace OpenLoco::Math;
 TEST(VectorTest, fastSquareRoot)
 {
     ASSERT_EQ(Vector::fastSquareRoot(0), 0);
+    ASSERT_EQ(Vector::fastSquareRoot(256), 16);
+    ASSERT_EQ(Vector::fastSquareRoot(10000), 100);
+    ASSERT_EQ(Vector::fastSquareRoot(0xFFFFFFFF), 65519); // Note: Not very accurate...
+    ASSERT_EQ(Vector::fastSquareRoot(4095), 63);          // Note: Not very accurate...
 }
