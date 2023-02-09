9484c175fe82b0b1592a6dfe382ec779d2b2f1a9
David Schoemehl <davidschoemehl@gmail.com>
2023-02-09 08:09:33 -0600

    clang fix

--- src/OpenLoco/src/Objects/BuildingObject.h
+++ src/OpenLoco/src/Objects/BuildingObject.h
@@ -32,21 +32,21 @@ namespace OpenLoco
     {
         static constexpr auto kObjectType = ObjectType::building;
 
-        string_id name;                     // 0x0
-        uint32_t image;                     // 0x2
-        uint8_t var_06;                     // 0x6
-        uint8_t numVariations;              // 0x7
-        const uint8_t* varationHeights;     // 0x8
-        const uint16_t* var_0C;             // 0xC
-        const uint8_t* variationsArr10[32]; // 0x10
-        uint32_t colours;                   // 0x90
-        uint16_t designedYear;              // 0x94
-        uint16_t obsoleteYear;              // 0x96
-        BuildingObjectFlags flags;          // 0x98
-        uint8_t clearCostIndex;             // 0x99
-        uint16_t clearCostFactor;           // 0x9A
-        uint8_t scaffoldingSegmentFlagsType;     // 0x9C
-        Colour scaffoldingColour;           // 0x9D
+        string_id name;                      // 0x0
+        uint32_t image;                      // 0x2
+        uint8_t var_06;                      // 0x6
+        uint8_t numVariations;               // 0x7
+        const uint8_t* varationHeights;      // 0x8
+        const uint16_t* var_0C;              // 0xC
+        const uint8_t* variationsArr10[32];  // 0x10
+        uint32_t colours;                    // 0x90
+        uint16_t designedYear;               // 0x94
+        uint16_t obsoleteYear;               // 0x96
+        BuildingObjectFlags flags;           // 0x98
+        uint8_t clearCostIndex;              // 0x99
+        uint16_t clearCostFactor;            // 0x9A
+        uint8_t scaffoldingSegmentFlagsType; // 0x9C
+        Colour scaffoldingColour;            // 0x9D
         uint8_t pad_9E[0xA0 - 0x9E];
         uint8_t producedQuantity[2];     // 0xA0
         uint8_t producedCargoType[2];    // 0xA2
