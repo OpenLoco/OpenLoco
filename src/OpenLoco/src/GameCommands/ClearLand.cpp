3ac2866437c8a9fe937e611789cd18d58c8409a4
Aaron van Geffen <aaron@aaronweb.net>
2023-02-25 20:10:15 +0100

    WIP: start implementing sub_469D76

--- src/OpenLoco/src/GameCommands/ClearLand.cpp
+++ src/OpenLoco/src/GameCommands/ClearLand.cpp
@@ -1,7 +1,9 @@
 #include "Economy/Expenditures.h"
 #include "GameCommands.h"
+#include "Localisation/StringIds.h"
 #include "Map/TileLoop.hpp"
 #include "Map/TileManager.h"
+#include "SceneManager.h"
 
 using namespace OpenLoco::Interop;
 using namespace OpenLoco::World;
@@ -12,16 +14,51 @@ namespace OpenLoco::GameCommands
     static loco_global<uint32_t, 0x00F00140> _F00140;
     static loco_global<uint16_t, 0x00F00144> _F00144;
     static loco_global<uint16_t, 0x00F00170> _F00170;
+    static loco_global<uint32_t, 0x00F25308> _F25308;
 
-    // 0x00469D76
-    static uint32_t sub_469D76(World::Pos2 pos, const uint8_t flags)
+    // 0x004690FC
+    static void sub_4690FC(World::Pos2 pos)
     {
         registers regs;
         regs.ax = pos.x;
         regs.cx = pos.y;
+        call(0x004690FC, regs);
+    }
+
+    // 0x00469D76
+    static uint32_t sub_469D76(World::Pos2 pos, const uint8_t flags)
+    {
+        // This shoudn't happen due to using TilePosRangeView
+        if (pos.x > 0x2FFF || pos.y > 0x2FFF)
+        {
+            GameCommands::setErrorText(StringIds::off_edge_of_map);
+            return GameCommands::FAILURE;
+        }
+
+        _F25308 = 0;
+        if (flags & GameCommands::Flags::apply)
+        {
+            if (!isEditorMode())
+            {
+                sub_4690FC(pos);
+            }
+
+            auto tileHeight = World::TileManager::getHeight(pos);
+            TileManager::removeAllWallsOnTile(pos, tileHeight.landHeight);
+        }
+
+        // 0x00469DAE
+
+        /*
+        registers regs;
+        regs.ax = pos.x;
+        regs.cx = pos.y;
         regs.bl = flags;
-        call(0x00469D76, regs);
+        call(0x00469DAE, regs);
         return regs.ebx;
+        */
+
+        return 0;
     }
 
     // 0x00469CCB
