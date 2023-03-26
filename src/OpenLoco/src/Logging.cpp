821ab3ee9b91834796e07b2b887e9534e01e99d8
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2023-03-27 01:59:21 +0300

    Write log files into user data

--- src/OpenLoco/src/Logging.cpp
+++ src/OpenLoco/src/Logging.cpp
@@ -3,6 +3,8 @@
 #include <OpenLoco/Diagnostics/LogFile.h>
 #include <OpenLoco/Diagnostics/LogTerminal.h>
 #include <OpenLoco/Diagnostics/Logging.h>
+#include <OpenLoco/Platform/Platform.h>
+#include <fmt/chrono.h>
 
 namespace OpenLoco::Diagnostics::Logging
 {
@@ -11,8 +13,13 @@ namespace OpenLoco::Diagnostics::Logging
 
     static fs::path getLogFilePath()
     {
-        // TODO: Use the user's Documents folder.
-        return fs::path{ "logs/openloco.log" };
+        const auto userPath = Platform::getUserDirectory();
+        const auto logPath = userPath / "logs";
+
+        // Get a unique log file name
+        const auto logFileName = fmt::format("openloco_{:%Y-%m-%d_%H_%M_%S}.log", fmt::localtime(std::time(nullptr)));
+
+        return logPath / logFileName;
     }
 
     static uint32_t parseLogLevels(std::string_view logLevels)
