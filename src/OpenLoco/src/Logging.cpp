324220019bafe017b744e1e7060d3dab5252730a
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2023-03-27 02:11:36 +0300

    Limit the amount of log files in the logs folder and cleanup code

--- src/OpenLoco/src/Logging.cpp
+++ src/OpenLoco/src/Logging.cpp
@@ -11,15 +11,46 @@ namespace OpenLoco::Diagnostics::Logging
     static std::shared_ptr<LogTerminal> _terminalLogSink{};
     static std::shared_ptr<LogFile> _fileLogSink{};
 
-    static fs::path getLogFilePath()
+    // The maximum amount of log files to keep in the folder, will delete the oldest
+    // logs if the amount of file exceeds this number.
+    static constexpr auto kMaxLogFiles = 20;
+
+    static fs::path getLogsFolderPath()
     {
-        const auto userPath = Platform::getUserDirectory();
-        const auto logPath = userPath / "logs";
+        return Platform::getUserDirectory() / "logs";
+    }
 
-        // Get a unique log file name
-        const auto logFileName = fmt::format("openloco_{:%Y-%m-%d_%H_%M_%S}.log", fmt::localtime(std::time(nullptr)));
+    static std::string getLogFileName()
+    {
+        return fmt::format("openloco_{:%Y-%m-%d_%H_%M_%S}.log", fmt::localtime(std::time(nullptr)));
+    }
 
-        return logPath / logFileName;
+    static void cleanupLogFiles(const fs::path& logsFolderPath)
+    {
+        // Get all log files in the folder
+        std::vector<std::filesystem::path> logFiles;
+        for (const auto& file : std::filesystem::directory_iterator(logsFolderPath))
+        {
+            if (file.path().extension() == ".log")
+            {
+                logFiles.push_back(file.path());
+            }
+        }
+
+        // Sort log files by last write time in ascending order
+        std::sort(logFiles.begin(), logFiles.end(), [](const auto& a, const auto& b) {
+            return std::filesystem::last_write_time(a) < std::filesystem::last_write_time(b);
+        });
+
+        // Delete oldest log files
+        if (logFiles.size() > kMaxLogFiles)
+        {
+            int numFilesToDelete = logFiles.size() - kMaxLogFiles;
+            for (int i = 0; i < numFilesToDelete; i++)
+            {
+                std::filesystem::remove(logFiles[i]);
+            }
+        }
     }
 
     static uint32_t parseLogLevels(std::string_view logLevels)
@@ -62,6 +93,9 @@ namespace OpenLoco::Diagnostics::Logging
 
     void initialize(std::string_view logLevels)
     {
+        const auto logsFolder = getLogsFolderPath();
+        cleanupLogFiles(logsFolder);
+
         const auto logLevelMask = parseLogLevels(logLevels);
 
         // Setup sink for the terminal/console.
@@ -71,7 +105,8 @@ namespace OpenLoco::Diagnostics::Logging
         Logging::installSink(_terminalLogSink);
 
         // Setup log file sink.
-        _fileLogSink = std::make_shared<LogFile>(getLogFilePath());
+        const auto logFile = logsFolder / getLogFileName();
+        _fileLogSink = std::make_shared<LogFile>(logFile);
         _fileLogSink->setWriteTimestamps(true);
         _fileLogSink->setLevelMask(logLevelMask);
         Logging::installSink(_fileLogSink);
