30a92787ba3c08d4661c4a60373944c13994c9e7
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2023-03-27 03:09:26 +0300

    Cleanup format argument passing, now allows std::string/string_view

--- src/Diagnostics/include/OpenLoco/Diagnostics/LogSink.h
+++ src/Diagnostics/include/OpenLoco/Diagnostics/LogSink.h
@@ -13,26 +13,26 @@ namespace OpenLoco::Diagnostics::Logging
 
         virtual void print(Level level, std::string_view message) = 0;
 
-        template<typename... TArgs>
-        void info(const char* fmt, TArgs&&... args)
+        template<typename TFmt, typename... TArgs>
+        void info(const TFmt& fmt, TArgs&&... args)
         {
             printLevel(Level::info, fmt, std::forward<TArgs>(args)...);
         }
 
-        template<typename... TArgs>
-        void warn(const char* fmt, TArgs&&... args)
+        template<typename TFmt, typename... TArgs>
+        void warn(const TFmt& fmt, TArgs&&... args)
         {
             printLevel(Level::warning, fmt, std::forward<TArgs>(args)...);
         }
 
-        template<typename... TArgs>
-        void error(const char* fmt, TArgs&&... args)
+        template<typename TFmt, typename... TArgs>
+        void error(const TFmt& fmt, TArgs&&... args)
         {
             printLevel(Level::error, fmt, std::forward<TArgs>(args)...);
         }
 
-        template<typename... TArgs>
-        void verbose([[maybe_unused]] const char* fmt, [[maybe_unused]] TArgs&&... args)
+        template<typename TFmt, typename... TArgs>
+        void verbose(const TFmt& fmt, TArgs&&... args)
         {
             printLevel(Level::verbose, fmt, std::forward<TArgs>(args)...);
         }
@@ -53,8 +53,8 @@ namespace OpenLoco::Diagnostics::Logging
 
     private:
         // Helper function to avoid code duplication.
-        template<typename... TArgs>
-        void printLevel(Level level, const char* fmt, TArgs&&... args)
+        template<typename TFmt, typename... TArgs>
+        void printLevel(Level level, const TFmt& fmt, TArgs&&... args)
         {
             if (!passesLevelFilter(level))
                 return;
--- src/Diagnostics/include/OpenLoco/Diagnostics/Logging.h
+++ src/Diagnostics/include/OpenLoco/Diagnostics/Logging.h
@@ -20,8 +20,8 @@ namespace OpenLoco::Diagnostics::Logging
         bool passesLevelFilter(Level level);
 
         // Helper function to avoid code duplication.
-        template<typename... TArgs>
-        void printLevel(Level level, const char* fmt, TArgs&&... args)
+        template<typename TFmt, typename... TArgs>
+        void printLevel(Level level, const TFmt& fmt, TArgs&&... args)
         {
             if (!passesLevelFilter(level))
                 return;
@@ -31,26 +31,26 @@ namespace OpenLoco::Diagnostics::Logging
         }
     }
 
-    template<typename... TArgs>
-    void info(const char* fmt, TArgs&&... args)
+    template<typename TFmt, typename... TArgs>
+    void info(const TFmt& fmt, TArgs&&... args)
     {
         Detail::printLevel(Level::info, fmt, std::forward<TArgs>(args)...);
     }
 
-    template<typename... TArgs>
-    void warn(const char* fmt, TArgs&&... args)
+    template<typename TFmt, typename... TArgs>
+    void warn(const TFmt& fmt, TArgs&&... args)
     {
         Detail::printLevel(Level::warning, fmt, std::forward<TArgs>(args)...);
     }
 
-    template<typename... TArgs>
-    void error(const char* fmt, TArgs&&... args)
+    template<typename TFmt, typename... TArgs>
+    void error(const TFmt& fmt, TArgs&&... args)
     {
         Detail::printLevel(Level::error, fmt, std::forward<TArgs>(args)...);
     }
 
-    template<typename... TArgs>
-    void verbose([[maybe_unused]] const char* fmt, [[maybe_unused]] TArgs&&... args)
+    template<typename TFmt, typename... TArgs>
+    void verbose(const TFmt& fmt, TArgs&&... args)
     {
         Detail::printLevel(Level::verbose, fmt, std::forward<TArgs>(args)...);
     }
