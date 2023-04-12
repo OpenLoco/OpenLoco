0ae1a7ac0e33332a67dd0f9207ba9bcfdf18b1ee
Mark Ellis <memellis@gmail.com>
2023-04-13 00:38:32 +0100

    Fixed clang formatting in ScenarioConstruction.cpp

--- src/OpenLoco/src/ScenarioConstruction.cpp
+++ src/OpenLoco/src/ScenarioConstruction.cpp
@@ -8,7 +8,8 @@ namespace OpenLoco::Scenario
         return getGameState().scenarioConstruction;
     }
 
-    void resetConstruction() {
+    void resetConstruction()
+    {
         std::fill(std::begin(Scenario::getConstruction().signals), std::end(Scenario::getConstruction().signals), 0xFF);
         std::fill(std::begin(Scenario::getConstruction().bridges), std::end(Scenario::getConstruction().bridges), 0xFF);
         std::fill(std::begin(Scenario::getConstruction().trainStations), std::end(Scenario::getConstruction().trainStations), 0xFF);
