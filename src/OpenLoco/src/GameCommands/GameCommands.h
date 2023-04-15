bc5c714119e7fcd3e3fd9d2a95e3f3acca64868f
Reinaldo A. C. Rauch <reinaldorauch@gmail.com>
2023-04-15 03:39:21 -0300

    adding arg struct for rename station

--- src/OpenLoco/src/GameCommands/GameCommands.h
+++ src/OpenLoco/src/GameCommands/GameCommands.h
@@ -467,6 +467,42 @@ namespace OpenLoco::GameCommands
         doCommand(GameCommand::changeStationName, regs);
     }
 
+    struct RenameStationArgs
+    {
+        static constexpr auto command = GameCommand::changeStationName;
+
+        RenameStationArgs() = default;
+        explicit RenameStationArgs(const registers& regs)
+            : stationId(StationId(regs.cx))
+            , nameBufferIndex(regs.ax)
+            , buffer{}
+        {
+            std::memcpy(buffer, &regs.edx, 4);
+            std::memcpy(buffer + 4, &regs.ebp, 4);
+            std::memcpy(buffer + 8, &regs.edi, 4);
+        }
+
+        StationId stationId;
+        uint8_t nameBufferIndex;
+        char buffer[37];
+
+        explicit operator registers() const
+        {
+            registers regs;
+
+            regs.cx = enumValue(stationId);
+            regs.ax = nameBufferIndex;
+            constexpr std::array<uint8_t, 3> iToOffset = { 24, 0, 12 };
+            const auto offset = iToOffset[nameBufferIndex];
+
+            std::memcpy(&regs.edx, buffer + offset, 4);
+            std::memcpy(&regs.ebp, buffer + offset + 4, 4);
+            std::memcpy(&regs.edi, buffer + offset + 8, 4);
+
+            return regs;
+        }
+    };
+
     inline void do12(EntityId head, uint8_t bh)
     {
         registers regs;
