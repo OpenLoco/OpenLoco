105d03cfc4ac57581cdbda18bbf3c0a5274eadfb
Mark Ellis <memellis@gmail.com>
2023-04-16 14:28:32 +0100

    Added note on var_17A[8] as it is not used always filled with 0xFF

--- src/OpenLoco/src/ScenarioConstruction.h
+++ src/OpenLoco/src/ScenarioConstruction.h
@@ -11,7 +11,7 @@ namespace OpenLoco::Scenario
         uint8_t bridges[8];       // 0x000162 (0x00525F7A)
         uint8_t trainStations[8]; // 0x00016A (0x00525F82)
         uint8_t trackMods[8];     // 0x000172 (0x00525F8A)
-        uint8_t var_17A[8];       // 0x00017A (0x00525F92)
+        uint8_t var_17A[8];       // 0x00017A (0x00525F92) NOTE: not used always filled with 0xFF
         uint8_t roadStations[8];  // 0x000182 (0x00525F9A)
         uint8_t roadMods[8];      // 0x00018A (0x00525FA2)
     };
