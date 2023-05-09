f4b30e9521d1e0dfc96d57cff9a0d46aede84b94
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2023-05-09 21:16:38 +0100

    Add logging

--- src/OpenLoco/src/Main.cpp
+++ src/OpenLoco/src/Main.cpp
@@ -4,9 +4,13 @@
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
 #endif
+#include <OpenLoco/Diagnostics/Logging.h>
 #include <windows.h>
 
 #include <shellapi.h>
+
+using namespace OpenLoco::Diagnostics;
+
 /**
  * The function that is called directly from the host application (loco.exe)'s WinMain. This will be removed when OpenLoco can
  * be built as a stand alone application.
@@ -20,6 +24,12 @@ extern "C" __declspec(dllexport) int StartOpenLoco([[maybe_unused]] HINSTANCE hI
 
     int argc{};
     auto* argw = CommandLineToArgvW(cmdline, &argc);
+    if (argw == nullptr)
+    {
+        Logging::error("CommandLineToArgvW failed to convert commandline");
+        // We will continue but just ignore the command line args.
+        argc = 0;
+    }
 
     // This will hold a utf8 string of the command line
     std::vector<std::string> argvStrs;
@@ -28,9 +38,19 @@ extern "C" __declspec(dllexport) int StartOpenLoco([[maybe_unused]] HINSTANCE hI
     for (auto i = 0; i < argc; ++i)
     {
         int length = WideCharToMultiByte(CP_UTF8, 0, argw[i], -1, 0, 0, NULL, NULL);
+
+        if (length == 0)
+        {
+            // Sadly can't print what the argument is that is causing the issue as it needs to be in utf8...
+            Logging::error("Failed to get cmdline argument utf8 length.");
+            continue;
+        }
         argvStrs[i].resize(length);
 
-        WideCharToMultiByte(CP_UTF8, 0, argw[i], -1, argvStrs[i].data(), length, NULL, NULL);
+        if (WideCharToMultiByte(CP_UTF8, 0, argw[i], -1, argvStrs[i].data(), length, NULL, NULL) == 0)
+        {
+            Logging::error("Failed to convert cmdline argument to utf8.");
+        }
     }
     LocalFree(reinterpret_cast<HLOCAL>(argw));
     const auto res = OpenLoco::main(std::move(argvStrs));
