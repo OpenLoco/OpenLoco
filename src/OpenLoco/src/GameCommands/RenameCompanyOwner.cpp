fc55c766f8c5672aaee9a25f5b639930f0a25a46
Aaron van Geffen <aaron@aaronweb.net>
2023-05-17 22:16:56 +0200

    Use ChangeCompanyOwnerNameArgs in changeCompanyOwnerName

--- src/OpenLoco/src/GameCommands/RenameCompanyOwner.cpp
+++ src/OpenLoco/src/GameCommands/RenameCompanyOwner.cpp
@@ -22,42 +22,32 @@ namespace OpenLoco::GameCommands
      *
      * This command is called 3 times before the buffer is applied. Each time, 12 chars of the 36 char buffer are provided.
      * The resulting company owner name has a maximum length of 31 chars; the last bytes are not used.
-     *
-     * @param flags @<bl> - game command flags
-     * @param companyId @<cl> - company id
-     * @param index @<ax> - update index (in order of: 1, 2, 0)
-     * @param buffer0 @<edx> - First part (4 chars) of the 12 update buffer
-     * @param buffer1 @<dx> - Second part (4 chars) of the 12 update buffer
-     * @param buffer2 @<bp> - Third part (4 chars) of the 12 update buffer
-     * @return @<ebx> - returns 0 if rename is successful; otherwise GameCommands::FAILURE
      */
-    static uint32_t changeCompanyOwnerName(const uint8_t flags, CompanyId companyId, int16_t index, uint32_t buffer0, uint32_t buffer1, uint32_t buffer2)
+    static uint32_t changeCompanyOwnerName(const ChangeCompanyOwnerNameArgs& args, const uint8_t flags)
     {
         GameCommands::setExpenditureType(ExpenditureType::Miscellaneous);
 
         // Keep track of the company id over several calls.
         static CompanyId _companyId{};
-        if (index == 1)
-            _companyId = companyId;
+        if (args.bufferIndex == 1)
+            _companyId = args.companyId;
 
-        static uint32_t renameBuffer[9];
+        static char staticRenameBuffer[37]{};
 
         // Fill buffer over calls into the renameBuffer
         if ((flags & GameCommands::Flags::apply) != 0)
         {
             static const std::array<int, 3> transformTable = { 2, 0, 1 };
-            int arrayIndex = transformTable.at(index);
-            renameBuffer[arrayIndex * 3] = buffer0;
-            renameBuffer[arrayIndex * 3 + 1] = buffer1;
-            renameBuffer[arrayIndex * 3 + 2] = buffer2;
+            int arrayIndex = transformTable.at(args.bufferIndex);
+            std::memcpy(staticRenameBuffer + arrayIndex * 12, args.newName, 12);
         }
 
         // Applying the buffer?
-        if (index != 0)
+        if (args.bufferIndex != 0)
             return 0;
 
         char renameStringBuffer[37] = "";
-        memcpy(renameStringBuffer, renameBuffer, sizeof(renameBuffer));
+        memcpy(renameStringBuffer, staticRenameBuffer, sizeof(staticRenameBuffer));
         renameStringBuffer[36] = '\0';
 
         // Ensure the new name isn't empty.
@@ -112,7 +102,7 @@ namespace OpenLoco::GameCommands
 
     void changeCompanyOwnerName(registers& regs)
     {
-        regs.ebx = changeCompanyOwnerName(regs.bl, CompanyId(regs.cl), regs.ax, regs.edx, regs.ebp, regs.edi);
+        regs.ebx = changeCompanyOwnerName(ChangeCompanyOwnerNameArgs(regs), regs.bl);
     }
 
     // 0x00434BA1
