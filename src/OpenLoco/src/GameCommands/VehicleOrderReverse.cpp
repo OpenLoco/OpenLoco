f71e97be7c185c61ed9b771e89af1b77f830082f
Aaron van Geffen <aaron@aaronweb.net>
2023-06-03 22:34:02 +0200

    More printfs for debugging purposes

--- src/OpenLoco/src/GameCommands/VehicleOrderReverse.cpp
+++ src/OpenLoco/src/GameCommands/VehicleOrderReverse.cpp
@@ -29,6 +29,8 @@ namespace OpenLoco::GameCommands
         head->currentOrder = Vehicles::reverseVehicleOrderTable(head->orderTableOffset, head->currentOrder);
         printf("currentOrder after: %d\n", head->currentOrder);
 
+        printf("To be sure: 0x%x\n", 0x00987C5C + (uint32_t)head->orderTableOffset + head->currentOrder);
+
         return 0;
     }
 
