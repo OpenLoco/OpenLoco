2c0547caa3a91219793cf1467e5fb1eead085df4
Aaron van Geffen <aaron@aaronweb.net>
2023-06-04 22:50:37 +0200

    Move OrderManager out of VehicleManager (#1974)

---
+++ src/OpenLoco/src/Vehicles/OrderManager.cpp
@@ -0,0 +1,212 @@
+#include "OrderManager.h"
+#include "Drawing/SoftwareDrawingEngine.h"
+#include "GameState.h"
+#include "Graphics/ImageIds.h"
+#include "Input.h"
+#include "Localisation/Formatting.h"
+#include "Objects/CargoObject.h"
+#include "Objects/ObjectManager.h"
+#include "Ui/WindowManager.h"
+#include "Vehicle.h"
+#include "Vehicles/Orders.h"
+#include "Vehicles/VehicleManager.h"
+#include "World/StationManager.h"
+
+#include <sstream>
+
+namespace OpenLoco::Vehicles::OrderManager
+{
+    // TODO: Make this a fixed vector of size 63 (kMaxNumOrderPerVehicle) no need for it to be dynamic
+    std::vector<NumDisplayFrame> _displayFrames;
+    const std::vector<NumDisplayFrame>& displayFrames() { return _displayFrames; }
+
+    uint8_t* orders() { return getGameState().orders; }
+    uint32_t& numOrders() { return getGameState().numOrders; }
+
+    void sub_470795(const uint32_t removeOrderTableOffset, const int16_t sizeOfRemovedOrderTable)
+    {
+        for (auto head : VehicleManager::VehicleList())
+        {
+            if (head->orderTableOffset >= removeOrderTableOffset)
+            {
+                head->orderTableOffset += sizeOfRemovedOrderTable;
+            }
+        }
+    }
+
+    // 0x00470334
+    // Remove vehicle ?orders?
+    void freeOrders(VehicleHead* const head)
+    {
+        // Copy the offset as it will get modified during sub_470795
+        const auto offset = head->orderTableOffset;
+        const auto size = head->sizeOfOrderTable;
+
+        sub_470795(offset, -size);
+
+        // Fold orders table left to remove empty orders
+        std::rotate(&orders()[offset], &orders()[offset + size], &orders()[numOrders()]);
+
+        numOrders() -= head->sizeOfOrderTable;
+    }
+
+    // 0x00470B76
+    std::pair<World::Pos3, std::string> generateOrderUiStringAndLoc(uint32_t orderOffset, uint8_t orderNum)
+    {
+        std::stringstream ss;
+        ss << ControlCodes::inlineSpriteStr;
+
+        auto imageId = Gfx::recolour(ImageIds::getNumberCircle(orderNum), Colour::white);
+        ss.write(reinterpret_cast<const char*>(&imageId), 4);
+        OrderRingView orderRing(orderOffset);
+        auto order = orderRing.begin();
+        World::Pos3 pos{};
+
+        switch (order->getType())
+        {
+            case Vehicles::OrderType::StopAt:
+            {
+                auto* stopAt = order->as<Vehicles::OrderStopAt>();
+                // 0x00470B7D
+                auto* station = StationManager::get(stopAt->getStation());
+                pos = World::Pos3{ station->x, station->y, station->z } + World::Pos3{ 0, 0, 30 };
+                ss << ControlCodes::Colour::white;
+                for (auto nextOrder = order + 1; nextOrder != order; ++nextOrder)
+                {
+                    if (!nextOrder->hasFlags(Vehicles::OrderFlags::HasCargo))
+                    {
+                        break;
+                    }
+                    auto* unload = nextOrder->as<Vehicles::OrderUnloadAll>();
+                    auto* waitFor = nextOrder->as<Vehicles::OrderWaitFor>();
+                    const CargoObject* cargoObj = nullptr;
+                    if (unload != nullptr)
+                    {
+                        ss << " - ";
+                        cargoObj = ObjectManager::get<CargoObject>(unload->getCargo());
+                    }
+                    else if (waitFor != nullptr)
+                    {
+                        ss << " + ";
+                        cargoObj = ObjectManager::get<CargoObject>(waitFor->getCargo());
+                    }
+                    else
+                    {
+                        // Not possible
+                        break;
+                    }
+                    ss << ControlCodes::inlineSpriteStr;
+                    ss.write(reinterpret_cast<const char*>(&cargoObj->unitInlineSprite), 4);
+                }
+                break;
+            }
+            case Vehicles::OrderType::RouteThrough:
+            {
+
+                // 0x00470C25
+                auto* station = StationManager::get(order->as<Vehicles::OrderRouteThrough>()->getStation());
+                pos = World::Pos3{ station->x, station->y, station->z } + World::Pos3{ 0, 0, 30 };
+                break;
+            }
+            case Vehicles::OrderType::RouteWaypoint:
+                // 0x00470C6F
+                pos = order->as<Vehicles::OrderRouteWaypoint>()->getWaypoint() + World::Pos3{ 16, 16, 8 };
+                break;
+            case Vehicles::OrderType::End:
+            case Vehicles::OrderType::UnloadAll:
+            case Vehicles::OrderType::WaitFor:
+                return { {}, {} };
+        }
+        ss << std::endl;
+        return { pos, ss.str() };
+    }
+
+    // 0x00470824
+    void generateNumDisplayFrames(Vehicles::VehicleHead* head)
+    {
+        Input::setMapSelectionFlags(Input::MapSelectionFlags::unk_04);
+        Gfx::invalidateScreen();
+        _displayFrames.clear();
+        auto orders = Vehicles::OrderRingView(head->orderTableOffset);
+        uint8_t i = 0;
+        for (auto& order : orders)
+        {
+            if (!order.hasFlags(Vehicles::OrderFlags::HasNumber))
+            {
+                continue;
+            }
+            NumDisplayFrame newFrame;
+            newFrame.orderOffset = order.getOffset();
+
+            auto lineNumber = 0;
+            auto* stationOrder = order.as<Vehicles::OrderStation>();
+            auto* waypointOrder = order.as<Vehicles::OrderRouteWaypoint>();
+            if (stationOrder != nullptr)
+            {
+                lineNumber++; // station labels start on line 0 so add 1 for the number
+
+                const auto stationId = stationOrder->getStation();
+                for (auto innerOrder = orders.begin(); innerOrder->getOffset() != order.getOffset(); ++innerOrder)
+                {
+                    auto* innerStationOrder = innerOrder->as<Vehicles::OrderStation>();
+                    if (innerStationOrder == nullptr)
+                    {
+                        continue;
+                    }
+                    if (stationId == innerStationOrder->getStation())
+                    {
+                        lineNumber++;
+                    }
+                }
+            }
+            else if (waypointOrder != nullptr)
+            {
+                const auto rawOrder = order.getRaw();
+                for (auto innerOrder = orders.begin(); innerOrder->getOffset() != order.getOffset(); ++innerOrder)
+                {
+                    if (rawOrder == innerOrder->getRaw())
+                    {
+                        lineNumber++;
+                    }
+                }
+            }
+
+            // For some reason save only a byte when this could in theory be larger
+            newFrame.lineNumber = static_cast<uint8_t>(lineNumber);
+            _displayFrames.push_back(newFrame);
+            i++;
+        }
+
+        i = 1;
+        auto& drawingCtx = Gfx::getDrawingEngine().getDrawingContext();
+        for (auto& unk : _displayFrames)
+        {
+            auto order = Vehicles::OrderRingView(unk.orderOffset, 0).begin();
+            if (!order->hasFlags(Vehicles::OrderFlags::HasNumber))
+            {
+                continue;
+            }
+
+            auto [loc, str] = generateOrderUiStringAndLoc(order->getOffset(), i);
+            const auto pos = World::gameToScreen(loc, Ui::WindowManager::getCurrentRotation());
+            auto stringWidth = drawingCtx.getStringWidth(str.c_str());
+            for (auto zoom = 0; zoom < 4; ++zoom)
+            {
+                // The first line of the label will always be at the centre
+                // of the station/waypoint. This works out where the subsequent
+                // lines of the label will end up.
+                auto width = (stringWidth + 3) << zoom;
+                auto numberHeight = (unk.lineNumber * 10 /* lineHeight TODO make same as Windows::Vehicle.cpp lineHeight */) << zoom;
+                auto firstLineHeight = 11 << zoom;
+                auto midX = width / 2;
+                auto midFirstLineY = firstLineHeight / 2;
+
+                unk.frame.left[zoom] = (pos.x - midX) >> zoom;
+                unk.frame.right[zoom] = (pos.x + midX) >> zoom;
+                unk.frame.top[zoom] = (pos.y - midFirstLineY + numberHeight) >> zoom;
+                unk.frame.bottom[zoom] = (pos.y + midFirstLineY + numberHeight) >> zoom;
+            }
+            i++;
+        }
+    }
+}
