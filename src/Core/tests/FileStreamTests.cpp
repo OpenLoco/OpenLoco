5d5f9957dc0980aa981119c4ccc43b0f9337c02a
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2023-06-06 23:10:14 +0300

    Refactor FileStream tests a bit

--- src/Core/tests/FileStreamTests.cpp
+++ src/Core/tests/FileStreamTests.cpp
@@ -3,6 +3,7 @@
 #include <cstdio>
 #include <filesystem>
 #include <gtest/gtest.h>
+#include <numeric>
 
 using namespace OpenLoco;
 
@@ -20,30 +21,68 @@ static std::filesystem::path getTempFilePath()
     return tempFile;
 }
 
-TEST(FileStreamTest, testWriteRead)
-{
-    const auto filePath = getTempFilePath();
+using DataBuffer = std::vector<uint8_t>;
 
+static auto generateData(size_t dataLength)
+{
+    DataBuffer data(dataLength);
+    for (size_t i = 0; i < dataLength; i++)
+    {
+        data[i] = static_cast<uint8_t>(i % 256);
+    }
+    return data;
+}
+
+static void generateFile(const std::filesystem::path& filePath, const DataBuffer& inputBytes)
+{
     FileStream streamOut(filePath, StreamMode::write);
     ASSERT_EQ(streamOut.getLength(), 0);
     ASSERT_EQ(streamOut.getPosition(), 0);
 
-    const std::array<uint8_t, 4> writeBuffer{ 0x01, 0x02, 0x03, 0x04 };
-    streamOut.write(writeBuffer.data(), writeBuffer.size());
+    streamOut.write(inputBytes.data(), inputBytes.size());
 
-    ASSERT_EQ(streamOut.getLength(), sizeof(writeBuffer));
-    ASSERT_EQ(streamOut.getPosition(), sizeof(writeBuffer));
+    ASSERT_EQ(streamOut.getLength(), inputBytes.size());
+    ASSERT_EQ(streamOut.getPosition(), inputBytes.size());
+}
 
-    streamOut.close();
+TEST(FileStreamTest, testWriteRead)
+{
+    const auto testDataSize = 4;
+    const auto testData = generateData(testDataSize);
+    const auto filePath = getTempFilePath();
+
+    generateFile(filePath, testData);
 
     FileStream streamIn(filePath, StreamMode::read);
-    ASSERT_EQ(streamIn.getLength(), sizeof(writeBuffer));
+    ASSERT_EQ(streamIn.getLength(), testData.size());
     ASSERT_EQ(streamIn.getPosition(), 0);
 
-    std::array<uint8_t, 4> readBuffer{};
+    auto readBuffer = DataBuffer(testDataSize);
     streamIn.read(readBuffer.data(), readBuffer.size());
 
-    ASSERT_EQ(readBuffer, writeBuffer);
+    ASSERT_EQ(readBuffer, testData);
+
+    streamIn.close();
+    std::filesystem::remove(filePath);
+}
+
+TEST(FileStreamTest, testBadRead)
+{
+    const auto testDataSize = 4;
+    const auto testData = generateData(testDataSize);
+    const auto filePath = getTempFilePath();
+
+    generateFile(filePath, testData);
+
+    FileStream streamIn(filePath, StreamMode::read);
+    ASSERT_EQ(streamIn.getLength(), testData.size());
+    ASSERT_EQ(streamIn.getPosition(), 0);
+
+    streamIn.setPosition(2);
+    ASSERT_EQ(streamIn.getPosition(), 2);
+
+    std::array<uint8_t, 4> readBuffer{};
+    EXPECT_THROW(streamIn.read(readBuffer.data(), readBuffer.size()), std::runtime_error);
 
     streamIn.close();
     std::filesystem::remove(filePath);
@@ -51,17 +90,18 @@ TEST(FileStreamTest, testWriteRead)
 
 TEST(FileStreamTest, testPosition)
 {
+    const auto testDataSize = 4;
+    const auto testData = generateData(testDataSize);
     const auto filePath = getTempFilePath();
 
     FileStream streamOut(filePath, StreamMode::write);
     ASSERT_EQ(streamOut.getLength(), 0);
     ASSERT_EQ(streamOut.getPosition(), 0);
 
-    const std::array<uint8_t, 4> writeBuffer{ 0x01, 0x02, 0x03, 0x04 };
-    streamOut.write(writeBuffer.data(), writeBuffer.size());
+    streamOut.write(testData.data(), testData.size());
 
-    ASSERT_EQ(streamOut.getLength(), sizeof(writeBuffer));
-    ASSERT_EQ(streamOut.getPosition(), sizeof(writeBuffer));
+    ASSERT_EQ(streamOut.getLength(), testData.size());
+    ASSERT_EQ(streamOut.getPosition(), testData.size());
 
     streamOut.setPosition(0);
     ASSERT_EQ(streamOut.getPosition(), 0);
