44a0ea5a5bdcfb8dd8a0339cfd1a22f25ccc9b98
Aaron van Geffen <aaron@aaronweb.net>
2023-07-03 22:34:41 +0200

    Allow scrolling up and down on steppers to change their values
    Co-authored-by: Hielke Morsink <Broxzier@users.noreply.github.com>

--- src/OpenLoco/src/Ui/WindowManager.cpp
+++ src/OpenLoco/src/Ui/WindowManager.cpp
@@ -7,6 +7,7 @@
 #include "GameState.h"
 #include "Graphics/Colour.h"
 #include "Graphics/Gfx.h"
+#include "Graphics/ImageIds.h"
 #include "Input.h"
 #include "Intro.h"
 #include "Logging.h"
@@ -1466,6 +1467,103 @@ namespace OpenLoco::Ui::WindowManager
         invalidateWidget(window->type, window->number, widgetIndex);
     }
 
+    // Allow mouse wheel scrolling to manipulate stepper widgets and tool sizes
+    static bool stepperWheelInput(Window& w, WidgetIndex_t widgetIndex, int32_t wheel)
+    {
+        auto widgetType = w.widgets[widgetIndex].type;
+
+        // If we aren't scrolling on a tool preview or stepper textbox, lower the widget index
+        // once or twice to try to find the parent widget. This allows scrolling on the
+        // increase/decrease buttons themselves, too.
+        int32_t attempts = 0;
+        while (widgetType != WidgetType::wt_3 && widgetType != WidgetType::textbox && widgetIndex > 0)
+        {
+            switch (widgetType)
+            {
+                case WidgetType::toolbarTab: // + and - for tool widget
+                case WidgetType::button:     // + and - for stepper widget
+                {
+                    if (attempts > 0)
+                    {
+                        // Verify that the previous button was of the same type
+                        auto previousType = w.widgets[widgetIndex + 1].type;
+                        if (previousType != widgetType)
+                        {
+                            return false;
+                        }
+                    }
+                    break;
+                }
+                default:
+                    // The widget type is not an increment or decrement button
+                    return false;
+            }
+
+            attempts++;
+            if (attempts > 2)
+            {
+                // We've looked two widgets up, and no tool preview or stepper widget was found
+                return false;
+            }
+
+            widgetIndex--;
+            widgetType = w.widgets[widgetIndex].type;
+        }
+
+        WidgetIndex_t buttonWidgetIndex;
+        WidgetType expectedType;
+
+        switch (widgetType)
+        {
+            case WidgetType::wt_3:
+            {
+                auto expectedContent1 = Gfx::recolour(ImageIds::decrease_tool_area, Colour::white);
+                auto expectedContent2 = Gfx::recolour(ImageIds::increase_tool_area, Colour::white);
+
+                auto button1Image = w.widgets[widgetIndex + 1].image;
+                auto button2Image = w.widgets[widgetIndex + 2].image;
+                if (button1Image != expectedContent1 || button2Image != expectedContent2)
+                {
+                    return false;
+                }
+
+                buttonWidgetIndex = wheel < 0 ? widgetIndex + 2 : widgetIndex + 1;
+                expectedType = WidgetType::toolbarTab;
+                break;
+            }
+            case WidgetType::textbox:
+            {
+                auto button1StringId = w.widgets[widgetIndex + 1].text;
+                auto button2StringId = w.widgets[widgetIndex + 2].text;
+                if (button1StringId != StringIds::stepper_minus || button2StringId != StringIds::stepper_plus)
+                {
+                    return false;
+                }
+
+                buttonWidgetIndex = wheel < 0 ? widgetIndex + 2 : widgetIndex + 1;
+                expectedType = WidgetType::button;
+                break;
+            }
+            default:
+                return false;
+        }
+
+        if (w.isDisabled(buttonWidgetIndex))
+        {
+            return false;
+        }
+
+        auto button1Type = w.widgets[widgetIndex + 1].type;
+        auto button2Type = w.widgets[widgetIndex + 2].type;
+        if (button1Type != expectedType || button2Type != expectedType)
+        {
+            return false;
+        }
+
+        w.callOnMouseDown(buttonWidgetIndex);
+        return true;
+    }
+
     // 0x004C628E
     static bool windowWheelInput(Window* window, int wheel)
     {
@@ -1548,6 +1646,11 @@ namespace OpenLoco::Ui::WindowManager
             return;
         }
 
+        if (wheel == 0)
+        {
+            return;
+        }
+
         const Ui::Point cursorPosition = Input::getMouseLocation();
         auto window = findAt(cursorPosition);
 
@@ -1585,6 +1688,11 @@ namespace OpenLoco::Ui::WindowManager
                             windowScrollWheelInput(window, widgetIndex, wheel);
                             return;
                         }
+                    }
+
+                    if (stepperWheelInput(*window, widgetIndex, wheel))
+                    {
+                        return;
                     }
 
                     if (windowWheelInput(window, wheel))
