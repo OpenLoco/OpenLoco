03bd523ac4cfe6dc356a85a5eed092b46eb09f45
Aaron van Geffen <aaron@aaronweb.net>
2023-07-07 21:03:48 +0200

    Implement the rest and deduplicate

--- src/OpenLoco/src/GameCommands/CreateTown.cpp
+++ src/OpenLoco/src/GameCommands/CreateTown.cpp
@@ -48,62 +48,108 @@ namespace OpenLoco::GameCommands
         return regs.dx;
     }
 
+    static bool checkSurroundings(Pos2 pos)
+    {
+        auto tile = TileManager::get(pos);
+        auto* surfaceEl = tile.surface();
+        if (surfaceEl->slope() != 0 || surfaceEl->water() != 0)
+        {
+            return false;
+        }
+
+        auto* landObj = ObjectManager::get<LandObject>(surfaceEl->terrain());
+        if (landObj->hasFlags(LandObjectFlags::isDesert | LandObjectFlags::noTrees))
+        {
+            auto dx = sub_4C5604(pos);
+            if (dx < 10 && getGameState().rng.randNext() & 0xFF)
+            {
+                return false;
+            }
+        }
+
+        for (auto& town : TownManager::towns())
+        {
+            auto manhattanDistance = Math::Vector::manhattanDistance(pos, Pos2(town.x, town.y));
+            if (manhattanDistance < 768)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     // 0x00496C22
     static uint32_t createTown(const TownPlacementArgs& args, const uint8_t flags)
     {
         auto& gameState = getGameState();
+        Pos2 pos = args.pos;
         Town* newTown = nullptr;
 
         if (args.pos.x == -1)
         {
+            bool foundPos = false;
             for (auto attempts = 200; attempts > 0; attempts--)
             {
                 uint32_t rand = gameState.rng.randNext();
-                auto pos = Pos2(((rand & 0xFFFF) * kMapRows) >> 5, ((rand >> 16) * kMapColumns) >> 5);
+                pos = Pos2(((rand & 0xFFFF) * kMapRows) >> 5, ((rand >> 16) * kMapColumns) >> 5);
 
                 if (pos.x < 384 || pos.y < 384 || pos.x > 11904 || pos.y > 11904)
                 {
                     continue;
                 }
 
-                auto tile = TileManager::get(pos);
-                auto* surfaceEl = tile.surface();
-                if (surfaceEl->slope() != 0 || surfaceEl->water() != 0)
+                if (checkSurroundings(pos))
                 {
-                    continue;
+                    foundPos = true;
+                    break;
                 }
+            }
 
-                auto* landObj = ObjectManager::get<LandObject>(surfaceEl->terrain());
-                if (landObj->hasFlags(LandObjectFlags::isDesert | LandObjectFlags::noTrees))
-                {
-                    auto dx = sub_4C5604(pos);
-                    if (dx < 10 && gameState.rng.randNext() & 0xFF)
-                    {
-                        continue;
-                    }
-                }
-
-                // loc_496E89
-                for (auto& town : TownManager::towns())
-                {
-                    auto manhattanDistance = Math::Vector::manhattanDistance(pos, Pos2(town.x, town.y));
-                    if (manhattanDistance < 768)
-                    {
-                        break;
-                    }
-                }
-
-                newTown = initialiseTown(pos);
-                if (!newTown)
-                {
-                    setErrorText(StringIds::too_many_towns);
-                    return FAILURE;
-                }
+            if (!foundPos)
+            {
+                return FAILURE;
             }
         }
         else
         {
-            // TODO: loc_496C32 onwards
+            bool foundPos = false;
+            for (int attempts = 0; attempts < 40; attempts++)
+            {
+                if (attempts == 0)
+                {
+                    uint32_t rand = gameState.rng.randNext();
+                    pos += Pos2((rand & 0xE0) - 0x60, ((rand >> 4) & 0xE0) - 0x60);
+                }
+                else if (attempts <= 10)
+                {
+                    uint32_t rand = gameState.rng.randNext();
+                    pos += Pos2((rand & 0x60) - kTileSize, ((rand >> 4) & 0x60) - kTileSize);
+                }
+
+                if (pos.x < 160 || pos.y < 160 || pos.x > 12128 || pos.y > 12128)
+                {
+                    continue;
+                }
+
+                if (checkSurroundings(pos))
+                {
+                    foundPos = true;
+                    break;
+                }
+            }
+
+            if (!foundPos)
+            {
+                return FAILURE;
+            }
+        }
+
+        newTown = initialiseTown(pos);
+        if (!newTown)
+        {
+            setErrorText(StringIds::too_many_towns);
+            return FAILURE;
         }
 
         if (!(flags & Flags::apply))
