3e4e3f88cfa4ff3892e20b1bb10f1725b58899f2
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2023-07-12 20:01:20 +0100

    Fix two divergence issues

--- src/OpenLoco/src/GameCommands/CreateIndustry.cpp
+++ src/OpenLoco/src/GameCommands/CreateIndustry.cpp
@@ -548,7 +548,7 @@ namespace OpenLoco::GameCommands
                 }();
 
                 const auto randTileX = static_cast<tile_coord_t>((distanceRange * ((randVal2 >> 2) & 0xFF)) / 256) + minDistance;
-                const auto randTileY = static_cast<tile_coord_t>((distanceRange * ((randVal2 >> 10) & 0xFF)) / 256) + minDistance;
+                const auto randTileY = static_cast<tile_coord_t>((distanceRange * ((randVal2 >> 18) & 0xFF)) / 256) + minDistance;
 
                 const auto randPos = lastPlacedBuildingPos + World::toWorldSpace(World::TilePos2(randTileX, randTileY));
                 const auto minRandPos = randPos;
@@ -639,8 +639,8 @@ namespace OpenLoco::GameCommands
                     totalY += 16;
                 }
             }
-            newIndustry->x = World::tileFloor((totalX / newIndustry->numTiles) + 16);
-            newIndustry->y = World::tileFloor((totalY / newIndustry->numTiles) + 16);
+            newIndustry->x = 0xFFFFFFE0 & ((totalX / newIndustry->numTiles) + 16);
+            newIndustry->y = 0xFFFFFFE0 & ((totalY / newIndustry->numTiles) + 16);
         }
 
         // 0x00454745
