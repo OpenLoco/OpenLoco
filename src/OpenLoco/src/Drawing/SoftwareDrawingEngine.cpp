1459a0d53888d989a0bb53bd22f10ba0231a892b
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2023-07-14 05:21:55 +0300

    Bring UI style closer to the builtin one

--- src/OpenLoco/src/Drawing/SoftwareDrawingEngine.cpp
+++ src/OpenLoco/src/Drawing/SoftwareDrawingEngine.cpp
@@ -58,6 +58,70 @@ namespace OpenLoco::Drawing
         ImGui::DestroyContext();
     }
 
+    static void applyImGuiStyle()
+    {
+        auto& style = ImGui::GetStyle();
+
+        ImVec4* colors = style.Colors;
+        colors[ImGuiCol_Text] = ImVec4(1.00f, 1.00f, 1.00f, 1.00f);
+        colors[ImGuiCol_TextDisabled] = ImVec4(0.50f, 0.50f, 0.50f, 1.00f);
+        colors[ImGuiCol_WindowBg] = ImVec4(0.51f, 0.59f, 0.59f, 1.00f);
+        colors[ImGuiCol_ChildBg] = ImVec4(0.51f, 0.59f, 0.59f, 1.00f);
+        colors[ImGuiCol_PopupBg] = ImVec4(0.51f, 0.59f, 0.59f, 1.00f);
+        colors[ImGuiCol_Border] = ImVec4(0.00f, 0.00f, 0.00f, 0.14f);
+        colors[ImGuiCol_BorderShadow] = ImVec4(1.00f, 1.00f, 1.00f, 0.29f);
+        colors[ImGuiCol_FrameBg] = ImVec4(0.51f, 0.59f, 0.59f, 1.00f);
+        colors[ImGuiCol_FrameBgHovered] = ImVec4(0.51f, 0.59f, 0.63f, 1.00f);
+        colors[ImGuiCol_FrameBgActive] = ImVec4(0.36f, 0.45f, 0.45f, 1.00f);
+        colors[ImGuiCol_TitleBg] = ImVec4(0.36f, 0.45f, 0.45f, 1.00f);
+        colors[ImGuiCol_TitleBgActive] = ImVec4(0.29f, 0.39f, 0.39f, 1.00f);
+        colors[ImGuiCol_TitleBgCollapsed] = ImVec4(0.00f, 0.00f, 0.00f, 0.51f);
+        colors[ImGuiCol_MenuBarBg] = ImVec4(0.14f, 0.14f, 0.14f, 1.00f);
+        colors[ImGuiCol_ScrollbarBg] = ImVec4(0.46f, 0.53f, 0.53f, 1.00f);
+        colors[ImGuiCol_ScrollbarGrab] = ImVec4(0.36f, 0.45f, 0.45f, 1.00f);
+        colors[ImGuiCol_ScrollbarGrabHovered] = ImVec4(0.36f, 0.45f, 0.45f, 1.00f);
+        colors[ImGuiCol_ScrollbarGrabActive] = ImVec4(0.32f, 0.40f, 0.40f, 1.00f);
+        colors[ImGuiCol_CheckMark] = ImVec4(1.00f, 1.00f, 1.00f, 1.00f);
+        colors[ImGuiCol_SliderGrab] = ImVec4(0.36f, 0.45f, 0.45f, 1.00f);
+        colors[ImGuiCol_SliderGrabActive] = ImVec4(0.51f, 0.59f, 0.59f, 1.00f);
+        colors[ImGuiCol_Button] = ImVec4(0.51f, 0.59f, 0.59f, 1.00f);
+        colors[ImGuiCol_ButtonHovered] = ImVec4(0.49f, 0.55f, 0.55f, 1.00f);
+        colors[ImGuiCol_ButtonActive] = ImVec4(0.46f, 0.53f, 0.53f, 1.00f);
+        colors[ImGuiCol_Header] = ImVec4(0.51f, 0.59f, 0.59f, 1.00f);
+        colors[ImGuiCol_HeaderHovered] = ImVec4(0.00f, 0.00f, 0.00f, 0.80f);
+        colors[ImGuiCol_HeaderActive] = ImVec4(0.00f, 0.00f, 0.00f, 1.00f);
+        colors[ImGuiCol_Separator] = ImVec4(0.00f, 0.00f, 0.00f, 0.50f);
+        colors[ImGuiCol_SeparatorHovered] = ImVec4(0.00f, 0.00f, 0.00f, 0.78f);
+        colors[ImGuiCol_SeparatorActive] = ImVec4(0.00f, 0.00f, 0.00f, 1.00f);
+        colors[ImGuiCol_ResizeGrip] = ImVec4(0.62f, 0.62f, 0.62f, 0.20f);
+        colors[ImGuiCol_ResizeGripHovered] = ImVec4(0.39f, 0.39f, 0.39f, 0.67f);
+        colors[ImGuiCol_ResizeGripActive] = ImVec4(0.36f, 0.36f, 0.36f, 1.00f);
+        colors[ImGuiCol_Tab] = ImVec4(0.51f, 0.59f, 0.59f, 1.00f);
+        colors[ImGuiCol_TabHovered] = ImVec4(0.36f, 0.45f, 0.45f, 1.00f);
+        colors[ImGuiCol_TabActive] = ImVec4(0.36f, 0.45f, 0.45f, 1.00f);
+        colors[ImGuiCol_TabUnfocused] = ImVec4(0.07f, 0.10f, 0.15f, 0.97f);
+        colors[ImGuiCol_TabUnfocusedActive] = ImVec4(0.14f, 0.26f, 0.42f, 1.00f);
+        colors[ImGuiCol_PlotLines] = ImVec4(0.61f, 0.61f, 0.61f, 1.00f);
+        colors[ImGuiCol_PlotLinesHovered] = ImVec4(1.00f, 0.43f, 0.35f, 1.00f);
+        colors[ImGuiCol_PlotHistogram] = ImVec4(0.90f, 0.70f, 0.00f, 1.00f);
+        colors[ImGuiCol_PlotHistogramHovered] = ImVec4(1.00f, 0.60f, 0.00f, 1.00f);
+        colors[ImGuiCol_TableHeaderBg] = ImVec4(0.19f, 0.19f, 0.20f, 1.00f);
+        colors[ImGuiCol_TableBorderStrong] = ImVec4(0.31f, 0.31f, 0.35f, 1.00f);
+        colors[ImGuiCol_TableBorderLight] = ImVec4(0.23f, 0.23f, 0.25f, 1.00f);
+        colors[ImGuiCol_TableRowBg] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+        colors[ImGuiCol_TableRowBgAlt] = ImVec4(1.00f, 1.00f, 1.00f, 0.06f);
+        colors[ImGuiCol_TextSelectedBg] = ImVec4(0.26f, 0.59f, 0.98f, 0.35f);
+        colors[ImGuiCol_DragDropTarget] = ImVec4(1.00f, 1.00f, 0.00f, 0.90f);
+        colors[ImGuiCol_NavHighlight] = ImVec4(0.26f, 0.59f, 0.98f, 1.00f);
+        colors[ImGuiCol_NavWindowingHighlight] = ImVec4(1.00f, 1.00f, 1.00f, 0.70f);
+        colors[ImGuiCol_NavWindowingDimBg] = ImVec4(0.80f, 0.80f, 0.80f, 0.20f);
+        colors[ImGuiCol_ModalWindowDimBg] = ImVec4(0.80f, 0.80f, 0.80f, 0.35f);
+
+        style.FrameBorderSize = 1.0f;
+        style.TabRounding = 0.0f;
+        style.FramePadding = ImVec2(4.0f, 2.0f);
+    }
+
     void SoftwareDrawingEngine::initialize(SDL_Window* window)
     {
         SDL_SetHint(SDL_HINT_RENDER_DRIVER, "opengl");
@@ -88,6 +152,8 @@ namespace OpenLoco::Drawing
 
         ImGui_ImplSDL2_InitForSDLRenderer(window, _renderer);
         ImGui_ImplSDLRenderer2_Init(_renderer);
+
+        applyImGuiStyle();
     }
 
     // T[m][n]
