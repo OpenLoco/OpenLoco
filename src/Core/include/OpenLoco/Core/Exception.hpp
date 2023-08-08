51b28b85a88a2ebb0b5c2b91259ffabc34041611
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2023-08-08 16:07:41 +0300

    Use path relative to the project source directory for SourceLocation

--- src/Core/include/OpenLoco/Core/Exception.hpp
+++ src/Core/include/OpenLoco/Core/Exception.hpp
@@ -3,9 +3,25 @@
 #include <exception>
 #include <fmt/format.h>
 #include <string>
+#include <string_view>
 
 namespace OpenLoco::Exception
 {
+    namespace Detail
+    {
+        // TODO: Make this consteval for C++20
+        constexpr std::string_view sanitizePath(std::string_view path)
+        {
+#if defined(OPENLOCO_PROJECT_PATH)
+            constexpr std::string_view projectPath = OPENLOCO_PROJECT_PATH;
+            // Removes also the first slash.
+            return path.substr(projectPath.size() + 1);
+#else
+            return path;
+#endif
+        }
+    }
+
     // Something like std::source_location except this works prior to C++20.
     class SourceLocation
     {
@@ -14,7 +30,7 @@ namespace OpenLoco::Exception
         int _line;
 
     public:
-        explicit SourceLocation(const char* func = __builtin_FUNCTION(), const char* file = __builtin_FILE(), int line = __builtin_LINE())
+        explicit SourceLocation(std::string_view func = __builtin_FUNCTION(), std::string_view file = Detail::sanitizePath(__builtin_FILE()), int line = __builtin_LINE())
             : _function(func)
             , _file(file)
             , _line(line)
