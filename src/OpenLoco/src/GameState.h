52b0bb757a3d40593e898f2b1f16f294fd63771e
Mark Ellis <memellis@gmail.com>
2023-10-02 15:49:53 +0100

    Updatex_525FBC_global_to_use_gamestate_structure rebase master

--- src/OpenLoco/src/GameState.h
+++ src/OpenLoco/src/GameState.h
@@ -61,7 +61,7 @@ namespace OpenLoco
         uint8_t lastLandOption;                                                  // 0x00019E (0x00525FB6)
         uint8_t maxCompetingCompanies;                                           // 0x00019F (0x00525FB7)
         uint32_t orderTableLength;                                               // 0x0001A0 (0x00525FB8)
-        uint32_t var_1A4;                                                        // 0x0001A4 (0x00525FBC)
+        uint32_t roadObjectIdIsTram;                                             // 0x0001A4 (0x00525FBC)
         uint32_t var_1A8;                                                        // 0x0001A8 (0x00525FC0)
         uint8_t var_1AC;                                                         // 0x0001AC (0x00525FC4)
         uint8_t lastTrackTypeOption;                                             // 0x0001AD (0x00525FC5)
