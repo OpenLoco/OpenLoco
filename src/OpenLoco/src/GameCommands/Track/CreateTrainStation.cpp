d223d70e69ad20924ad51d42b50c269a1483e054
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2023-12-01 08:54:24 +0000

    Fix subtle difference

--- src/OpenLoco/src/GameCommands/Track/CreateTrainStation.cpp
+++ src/OpenLoco/src/GameCommands/Track/CreateTrainStation.cpp
@@ -357,7 +357,7 @@ namespace OpenLoco::GameCommands
                 if (piece.index == 0)
                 {
                     auto placementCostBase = Economy::getInflationAdjustedCost(stationObj->buildCostFactor, stationObj->costIndex, 8);
-                    const auto cost = (placementCostBase * World::TrackData::getTrackCostFactor(elTrack->trackId())) / 256;
+                    const auto cost = (placementCostBase * World::TrackData::getTrackCostFactor(args.trackId)) / 256;
                     totalCost += cost;
                 }
 
@@ -443,14 +443,14 @@ namespace OpenLoco::GameCommands
                         {
                             auto* oldStationObj = ObjectManager::get<TrainStationObject>(elStation->objectId());
                             auto removeCostBase = Economy::getInflationAdjustedCost(oldStationObj->sellCostFactor, oldStationObj->costIndex, 8);
-                            const auto cost = (removeCostBase * World::TrackData::getTrackCostFactor(elTrack->trackId())) / 256;
+                            const auto cost = (removeCostBase * World::TrackData::getTrackCostFactor(args.trackId)) / 256;
                             totalCost += cost;
                         }
                     }
                     if (calculateCost)
                     {
                         auto placementCostBase = Economy::getInflationAdjustedCost(stationObj->buildCostFactor, stationObj->costIndex, 8);
-                        const auto cost = (placementCostBase * World::TrackData::getTrackCostFactor(elTrack->trackId())) / 256;
+                        const auto cost = (placementCostBase * World::TrackData::getTrackCostFactor(args.trackId)) / 256;
                         totalCost += cost;
                     }
                 }
