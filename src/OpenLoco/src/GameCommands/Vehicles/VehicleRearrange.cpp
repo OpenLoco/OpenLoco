045d26daad91e4fd71ad6467d3bf2d4713ff856f
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2023-12-04 08:27:40 +0000

    Implement query side of command

--- src/OpenLoco/src/GameCommands/Vehicles/VehicleRearrange.cpp
+++ src/OpenLoco/src/GameCommands/Vehicles/VehicleRearrange.cpp
@@ -1,11 +1,64 @@
 #include "VehicleRearrange.h"
+#include "Economy/Expenditures.h"
+#include "Entities/EntityManager.h"
+#include "Vehicles/Vehicle.h"
 
 namespace OpenLoco::GameCommands
 {
     currency32_t vehicleRearrange(const VehicleRearrangeArgs& args, uint8_t flags)
     {
-        args.dest;
-        flags;
+        setExpenditureType(ExpenditureType::Miscellaneous);
+
+        auto* sourceVehicle = EntityManager::get<Vehicles::VehicleBase>(args.source);
+        auto* destVehicle = EntityManager::get<Vehicles::VehicleBase>(args.dest);
+
+        if (!(flags & Flags::apply))
+        {
+            if (!sub_431E6A(sourceVehicle->owner))
+            {
+                return FAILURE;
+            }
+            if (!sub_431E6A(destVehicle->owner))
+            {
+                return FAILURE;
+            }
+
+            auto* sourceHead = EntityManager::get<Vehicles::VehicleHead>(sourceVehicle->getHead());
+            auto* destHead = EntityManager::get<Vehicles::VehicleHead>(destVehicle->getHead());
+            if (!sourceHead->canBeModified())
+            {
+                return FAILURE;
+            }
+            if (!destHead->canBeModified())
+            {
+                return FAILURE;
+            }
+
+            if (sourceVehicle->getTrackType() != destVehicle->getTrackType())
+            {
+                setErrorText(StringIds::incompatible_vehicle);
+                return FAILURE;
+            }
+
+            // Pretty sure this needs to be true but not 100% (openloco addition)
+            auto* sourceBogie = sourceVehicle->asVehicleBogie();
+            if (sourceBogie == nullptr)
+            {
+                return FAILURE;
+            }
+
+            if (!destHead->isVehicleTypeCompatible(sourceBogie->objectId))
+            {
+                return FAILURE;
+            }
+
+            setPosition(destVehicle->position);
+            return 0;
+        }
+        else
+        {
+            // 0x004AF2A1
+        }
         return FAILURE;
     }
 
