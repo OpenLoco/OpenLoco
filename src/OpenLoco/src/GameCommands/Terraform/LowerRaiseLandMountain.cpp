8ac12d9c47e39d10101a21d3ca1c3f25842d8513
Aaron van Geffen <aaron@aaronweb.net>
2023-12-10 22:08:29 +0100

    More-or-less finish sub_4633F6

--- src/OpenLoco/src/GameCommands/Terraform/LowerRaiseLandMountain.cpp
+++ src/OpenLoco/src/GameCommands/Terraform/LowerRaiseLandMountain.cpp
@@ -12,14 +12,21 @@ using namespace OpenLoco::World;
 
 namespace OpenLoco::GameCommands
 {
+    static loco_global<uint32_t, 0x00F0014E> mtnToolCost;
+    static loco_global<uint8_t, 0x00F00154> mtnToolGCFlags;
+    static loco_global<uint8_t, 0x00F00155> _F00155;
+
     // TODO: remove after implementing the actual game command
     void registerMountainHooks()
     {
         registerHook(
             0x004633F6,
             [](registers& regs) FORCE_ALIGN_ARG_POINTER -> uint8_t {
+                // TODO: hacky workaround
+                std::set<World::Pos3, LessThanPos3> removedBuildings{};
+
                 registers backup = regs;
-                sub_4633F6({ regs.ax, regs.cx }, regs.dl);
+                sub_4633F6({ regs.ax, regs.cx }, regs.dl, removedBuildings);
                 regs = backup;
                 return 0;
             });
@@ -27,8 +34,11 @@ namespace OpenLoco::GameCommands
         registerHook(
             0x004634B9,
             [](registers& regs) FORCE_ALIGN_ARG_POINTER -> uint8_t {
+                // TODO: hacky workaround
+                std::set<World::Pos3, LessThanPos3> removedBuildings{};
+
                 registers backup = regs;
-                sub_4634B9({ regs.ax, regs.cx }, regs.dl);
+                sub_4634B9({ regs.ax, regs.cx }, regs.dl, removedBuildings);
                 regs = backup;
                 return 0;
             });
@@ -36,8 +46,11 @@ namespace OpenLoco::GameCommands
         registerHook(
             0x0046357C,
             [](registers& regs) FORCE_ALIGN_ARG_POINTER -> uint8_t {
+                // TODO: hacky workaround
+                std::set<World::Pos3, LessThanPos3> removedBuildings{};
+
                 registers backup = regs;
-                sub_46357C({ regs.ax, regs.cx }, regs.dl);
+                sub_46357C({ regs.ax, regs.cx }, regs.dl, removedBuildings);
                 regs = backup;
                 return 0;
             });
@@ -45,103 +58,150 @@ namespace OpenLoco::GameCommands
         registerHook(
             0x0046363F,
             [](registers& regs) FORCE_ALIGN_ARG_POINTER -> uint8_t {
+                // TODO: hacky workaround
+                std::set<World::Pos3, LessThanPos3> removedBuildings{};
+
                 registers backup = regs;
-                sub_46363F({ regs.ax, regs.cx }, regs.dl);
+                sub_46363F({ regs.ax, regs.cx }, regs.dl, removedBuildings);
                 regs = backup;
                 return 0;
             });
     }
 
     // 0x004633F6
-    void sub_4633F6(Pos2 pos, int8_t dl)
+    void sub_4633F6(Pos2 pos, int8_t targetBaseZ, std::set<Pos3, LessThanPos3>& removedBuildings)
     {
         if (!validCoords(pos))
         {
             return;
-        }
-
-        auto tile = TileManager::get(*pos);
-        const auto* surface = tile.surface();
-        auto baseZ = TileManager::getSurfaceCornerHeight(*surface);
-
-        // 0x00463513
-        if (baseZ < dl)
-        {
-            continue;
-        }
-        else
-        {
         }
 
         // Logging::info("sub_4633F6 with x={}, y={}", pos.x, pos.y);
+
+        auto tile = TileManager::get(pos);
+        const auto* surface = tile.surface();
+        auto cornerBaseZ = TileManager::getSurfaceCornerHeight(*surface);
+        auto baseZDiff = cornerBaseZ - targetBaseZ;
+        uint8_t slopeFlags;
+
+        // 0x00463450
+        if (baseZDiff == 0)
+        {
+            return;
+        }
+        else if (baseZDiff > 0)
+        {
+            if (baseZDiff <= _F00155)
+            {
+                return;
+            }
+
+            // TODO: only user; integrate
+            static loco_global<uint8_t[32], 0x004FD41C> _4FD41C;
+            slopeFlags = _4FD41C[surface->slope()];
+
+            targetBaseZ = surface->baseZ();
+            if (slopeFlags & SurfaceSlope::requiresHeightAdjustment)
+            {
+                targetBaseZ -= kSmallZStep;
+                slopeFlags &= ~SurfaceSlope::requiresHeightAdjustment;
+            }
+        }
+        else if (baseZDiff < 0)
+        {
+            if (-baseZDiff <= _F00155)
+            {
+                return;
+            }
+
+            // TODO: only user; integrate
+            static loco_global<uint8_t[32], 0x004FD37C> _4FD37C;
+            slopeFlags = _4FD37C[surface->slope()];
+
+            targetBaseZ = surface->baseZ();
+            if (slopeFlags & SurfaceSlope::requiresHeightAdjustment)
+            {
+                targetBaseZ += kSmallZStep;
+                slopeFlags &= ~SurfaceSlope::requiresHeightAdjustment;
+            }
+        }
+
+        auto result = TileManager::adjustSurfaceHeight(pos, targetBaseZ, slopeFlags, removedBuildings, mtnToolGCFlags);
+        if (result != FAILURE)
+        {
+            mtnToolCost += result;
+        }
     }
 
     // 0x004634B9
-    void sub_4634B9(Pos2 pos, int8_t dl)
+    void sub_4634B9(Pos2 pos, int8_t targetBaseZ, std::set<Pos3, LessThanPos3>& removedBuildings)
     {
         if (!validCoords(pos))
         {
             return;
         }
 
-        auto tile = TileManager::get(*pos);
+        // Logging::info("sub_4634B9 with x={}, y={}", pos.x, pos.y);
+
+        auto tile = TileManager::get(pos);
         const auto* surface = tile.surface();
         auto baseZ = TileManager::getSurfaceCornerHeight(*surface);
 
         // 0x00463513
-        if (baseZ < dl)
+        if (baseZ < targetBaseZ)
         {
-            continue;
+            return;
         }
         else
         {
         }
-        // Logging::info("sub_4634B9 with x={}, y={}", pos.x, pos.y);
     }
 
     // 0x0046357C
-    void sub_46357C(Pos2 pos, int8_t dl)
+    void sub_46357C(Pos2 pos, int8_t targetBaseZ, std::set<Pos3, LessThanPos3>& removedBuildings)
     {
         if (!validCoords(pos))
         {
             return;
         }
 
-        auto tile = TileManager::get(*pos);
+        // Logging::info("sub_46357C with x={}, y={}", pos.x, pos.y);
+
+        auto tile = TileManager::get(pos);
         const auto* surface = tile.surface();
         auto baseZ = TileManager::getSurfaceCornerHeight(*surface);
 
         // 0x004635D6
-        if (baseZ < dl)
+        if (baseZ < targetBaseZ)
         {
-            continue;
+            return;
         }
         else
         {
         }
-        // Logging::info("sub_46357C with x={}, y={}", pos.x, pos.y);
     }
 
     // 0x0046363F
-    void sub_46363F(Pos2 pos, int8_t dl)
+    void sub_46363F(Pos2 pos, int8_t targetBaseZ, std::set<Pos3, LessThanPos3>& removedBuildings)
     {
         if (!validCoords(pos))
         {
             return;
         }
 
-        auto tile = TileManager::get(*pos);
+        // Logging::info("sub_46363F with x={}, y={}", pos.x, pos.y);
+
+        auto tile = TileManager::get(pos);
         const auto* surface = tile.surface();
         auto baseZ = TileManager::getSurfaceCornerHeight(*surface);
 
         // 0x00463699
-        if (baseZ < dl)
+        if (baseZ < targetBaseZ)
         {
-            continue;
+            return;
         }
         else
         {
         }
-        // Logging::info("sub_46363F with x={}, y={}", pos.x, pos.y);
     }
 }
--- src/OpenLoco/src/GameCommands/Terraform/LowerRaiseLandMountain.h
+++ src/OpenLoco/src/GameCommands/Terraform/LowerRaiseLandMountain.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "GameCommands/GameCommands.h"
+#include "Map/Tile.h"
+#include <set>
 
 namespace OpenLoco::GameCommands
 {
@@ -34,8 +36,8 @@ namespace OpenLoco::GameCommands
     };
 
     void registerMountainHooks();
-    void sub_4633F6(World::Pos2 pos, int8_t dl);
-    void sub_4634B9(World::Pos2 pos, int8_t dl);
-    void sub_46357C(World::Pos2 pos, int8_t dl);
-    void sub_46363F(World::Pos2 pos, int8_t dl);
+    void sub_4633F6(World::Pos2 pos, int8_t targetBaseZ, std::set<World::Pos3, World::LessThanPos3>& removedBuildings);
+    void sub_4634B9(World::Pos2 pos, int8_t targetBaseZ, std::set<World::Pos3, World::LessThanPos3>& removedBuildings);
+    void sub_46357C(World::Pos2 pos, int8_t targetBaseZ, std::set<World::Pos3, World::LessThanPos3>& removedBuildings);
+    void sub_46363F(World::Pos2 pos, int8_t targetBaseZ, std::set<World::Pos3, World::LessThanPos3>& removedBuildings);
 }
