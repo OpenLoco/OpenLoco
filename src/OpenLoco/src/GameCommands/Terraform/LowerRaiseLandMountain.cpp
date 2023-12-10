d8056f10474ba0aea77f4b1425b0d7ddc7484aba
Aaron van Geffen <aaron@aaronweb.net>
2023-12-10 23:11:38 +0100

    Implement sub_46357C using the same pattern

--- src/OpenLoco/src/GameCommands/Terraform/LowerRaiseLandMountain.cpp
+++ src/OpenLoco/src/GameCommands/Terraform/LowerRaiseLandMountain.cpp
@@ -210,15 +210,56 @@ namespace OpenLoco::GameCommands
 
         auto tile = TileManager::get(pos);
         const auto* surface = tile.surface();
-        auto baseZ = TileManager::getSurfaceCornerHeight(*surface);
+        auto cornerBaseZ = TileManager::getSurfaceCornerHeight(*surface, SurfaceSlope::CornerUp::north);
+        auto baseZDiff = cornerBaseZ - targetBaseZ;
+        uint8_t slopeFlags;
 
         // 0x004635D6
-        if (baseZ < targetBaseZ)
+        if (baseZDiff == 0)
         {
             return;
         }
-        else
+        else if (baseZDiff > 0)
         {
+            if (baseZDiff <= _F00155)
+            {
+                return;
+            }
+
+            // TODO: only user; integrate
+            static loco_global<uint8_t[32], 0x004FD3DC> _4FD3DC;
+            slopeFlags = _4FD3DC[surface->slope()];
+
+            targetBaseZ = surface->baseZ();
+            if (slopeFlags & SurfaceSlope::requiresHeightAdjustment)
+            {
+                targetBaseZ -= kSmallZStep;
+                slopeFlags &= ~SurfaceSlope::requiresHeightAdjustment;
+            }
+        }
+        else if (baseZDiff < 0)
+        {
+            if (-baseZDiff <= _F00155)
+            {
+                return;
+            }
+
+            // TODO: only user; integrate
+            static loco_global<uint8_t[32], 0x004FD33C> _4FD33C;
+            slopeFlags = _4FD33C[surface->slope()];
+
+            targetBaseZ = surface->baseZ();
+            if (slopeFlags & SurfaceSlope::requiresHeightAdjustment)
+            {
+                targetBaseZ += kSmallZStep;
+                slopeFlags &= ~SurfaceSlope::requiresHeightAdjustment;
+            }
+        }
+
+        auto result = TileManager::adjustSurfaceHeight(pos, targetBaseZ, slopeFlags, removedBuildings, mtnToolGCFlags);
+        if (result != FAILURE)
+        {
+            mtnToolCost += result;
         }
     }
 
