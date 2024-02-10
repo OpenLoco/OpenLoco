6c470bc0950e4e9c31d1256ec4a6689114d3faba
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2024-02-10 13:47:22 +0000

    Fix mistaken replace

--- src/OpenLoco/src/Map/IndustryElement.h
+++ src/OpenLoco/src/Map/IndustryElement.h
@@ -21,7 +21,7 @@ namespace OpenLoco::World
         uint16_t _6;
 
     public:
-        // cargoTransferTime
+        // _4
         IndustryId industryId() const { return _industryId; }
         void setIndustryId(const IndustryId id) { _industryId = id; }
         Industry* industry() const;
