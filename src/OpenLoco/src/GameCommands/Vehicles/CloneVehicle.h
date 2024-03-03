4c053d5373c1243b81b25d68388730d112334064
Aaron van Geffen <aaron@aaronweb.net>
2024-03-03 17:31:35 +0100

    Introduce VehicleCloneArgs struct

--- src/OpenLoco/src/GameCommands/Vehicles/CloneVehicle.h
+++ src/OpenLoco/src/GameCommands/Vehicles/CloneVehicle.h
@@ -4,5 +4,25 @@
 
 namespace OpenLoco::GameCommands
 {
+    struct VehicleCloneArgs
+    {
+        static constexpr auto command = GameCommand::vehicleClone;
+
+        VehicleCloneArgs() = default;
+        explicit VehicleCloneArgs(const registers& regs)
+            : vehicleHeadId(static_cast<EntityId>(regs.ax))
+        {
+        }
+
+        EntityId vehicleHeadId;
+
+        explicit operator registers() const
+        {
+            registers regs;
+            regs.ax = enumValue(vehicleHeadId);
+            return regs;
+        }
+    };
+
     void cloneVehicle(registers& regs);
 }
