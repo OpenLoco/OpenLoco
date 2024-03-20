7dbd0a4d3141d77326e617d7735a1b16e7b9609b
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2024-03-20 08:32:49 +0000

    Start constructing road meta data

--- src/OpenLoco/src/Map/Track/TrackEnum.h
+++ src/OpenLoco/src/Map/Track/TrackEnum.h
@@ -87,4 +87,18 @@ namespace OpenLoco::World::Track
         junction = 1U << 10,
     };
     OPENLOCO_ENABLE_ENUM_OPERATORS(TrackPieceFlags);
+
+    enum class RoadPieceFlags : uint16_t
+    {
+        none = 0U,
+        oneWay = 1U << 0,
+        track = 1U << 1,
+        slope = 1U << 2,
+        steepSlope = 1U << 3,
+        intersection = 1U << 4,
+        oneSided = 1U << 5,
+        overtake = 1U << 6,
+        streetLights = 1U << 8,
+    };
+    OPENLOCO_ENABLE_ENUM_OPERATORS(RoadPieceFlags);
 }
