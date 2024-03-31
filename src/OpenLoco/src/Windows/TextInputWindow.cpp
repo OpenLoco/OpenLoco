41ace810d867d83dab7d2db7c70e76b42373ff11
Aaron van Geffen <aaron@aaronweb.net>
2024-03-31 12:39:19 +0200

    Fix text cursor not rendering at correct position

--- src/OpenLoco/src/Windows/TextInputWindow.cpp
+++ src/OpenLoco/src/Windows/TextInputWindow.cpp
@@ -263,12 +263,9 @@ namespace OpenLoco::Ui::Windows::TextInput
         drawnBuffer[inputSession.cursorPosition] = '\0';
 
         {
-            FormatArguments args{};
-            args.push(StringIds::buffer_2039);
-
-            position = { inputSession.xOffset, 1 };
-            drawingCtx.drawStringLeft(*clipped, position, Colour::black, StringIds::black_stringid, &args);
-            drawingCtx.fillRect(*clipped, position.x, position.y, position.x, position.y + 9, Colours::getShade(window.getColour(WindowColour::secondary).c(), 9), Drawing::RectFlags::none);
+            auto width = drawingCtx.getStringWidth(drawnBuffer);
+            auto cursorPos = Point(inputSession.xOffset + width, 1);
+            drawingCtx.fillRect(*clipped, cursorPos.x, cursorPos.y, cursorPos.x, cursorPos.y + 9, Colours::getShade(window.getColour(WindowColour::secondary).c(), 9), Drawing::RectFlags::none);
         }
     }
 
