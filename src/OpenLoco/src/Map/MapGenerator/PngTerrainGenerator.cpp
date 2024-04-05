c85138bbab69682541aa2419fff856949694eaaf
Benjamin Sutas <benjamin.sutas@gmail.com>
2024-04-06 06:26:35 +1100

    Fix PNG heightmap mirroring (#2382)
    * Fix PNG heightmap mirroring
    
    * Optimise png->heightmap code
    
    * Change logging text to use 'heightmap' instead of 'terrain'
    
    * Revert maxHeightmapLevels from 64 back to 40

--- src/OpenLoco/src/Map/MapGenerator/PngTerrainGenerator.cpp
+++ src/OpenLoco/src/Map/MapGenerator/PngTerrainGenerator.cpp
@@ -12,40 +12,38 @@ using namespace OpenLoco::Diagnostics;
 
 namespace OpenLoco::World::MapGenerator
 {
-    void PngTerrainGenerator::generate(const S5::Options& options, const fs::path& path, HeightMapRange heightMap)
+    void PngTerrainGenerator::generate(const S5::Options& options, const fs::path& path, HeightMap& heightMap)
     {
         if (!fs::is_regular_file(path))
         {
-            Logging::error("Can't find terrain file ({})", path);
+            Logging::error("Can't find heightmap file ({})", path);
             return;
         }
 
         auto pngImage = PngOps::loadPng(path.string());
         if (pngImage == nullptr)
         {
-            Logging::error("Can't load terrain file ({})", path);
+            Logging::error("Can't load heightmap file ({})", path);
             return;
         }
 
-        const int maxHeightmapLevels = 64 - options.minLandHeight;
+        const int maxHeightmapLevels = 40 - options.minLandHeight;
         const float scalingFactor = maxHeightmapLevels / 255.f;
 
-        for (int32_t y = 0; y < World::kMapRows; y++)
-        {
-            for (int32_t x = 0; x < World::kMapColumns; x++)
-            {
-                if (y >= pngImage->height || x >= pngImage->width)
-                {
-                    heightMap[{ x, y }] = options.minLandHeight;
-                }
-                else
-                {
-                    png_byte red, green, blue, alpha;
-                    pngImage->getPixel(x, y, red, green, blue, alpha);
+        std::fill_n(heightMap.data(), heightMap.size(), options.minLandHeight);
 
-                    auto imgHeight = std::max({ red, green, blue });
-                    heightMap[{ x, y }] = options.minLandHeight + (imgHeight * scalingFactor);
-                }
+        auto width = std::min<int>(World::kMapColumns, pngImage->width);
+        auto height = std::min<int>(World::kMapRows, pngImage->height);
+
+        for (int32_t y = 0; y < height; y++)
+        {
+            for (int32_t x = 0; x < width; x++)
+            {
+                png_byte red, green, blue, alpha;
+                pngImage->getPixel(x, y, red, green, blue, alpha);
+
+                auto imgHeight = std::max({ red, green, blue });
+                heightMap[{ y, x }] += imgHeight * scalingFactor; // this must be { y, x } otherwise the heightmap is mirrored
             }
         }
     }
