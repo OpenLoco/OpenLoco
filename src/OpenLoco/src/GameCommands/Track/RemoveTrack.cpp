594017b773b0389f0aea55b705dea9b97f3db28b
Aaron van Geffen <aaron@aaronweb.net>
2024-08-01 18:24:26 +0200

    Finish removeTrack implementation

--- src/OpenLoco/src/GameCommands/Track/RemoveTrack.cpp
+++ src/OpenLoco/src/GameCommands/Track/RemoveTrack.cpp
@@ -4,7 +4,10 @@
 #include "Map/TileManager.h"
 #include "Map/Track/TrackData.h"
 #include "Map/TrackElement.h"
+#include "Objects/BridgeObject.h"
+#include "Objects/TrackExtraObject.h"
 #include "Objects/TrackObject.h"
+#include "Random.h"
 #include "RemoveSignal.h"
 #include "RemoveTrainStation.h"
 #include "Vehicles/Vehicle.h"
@@ -12,9 +15,9 @@
 
 namespace OpenLoco::GameCommands
 {
-    static World::TrackElement* getTrack(const TrackRemovalArgs& args, const uint8_t trackFlags)
+    static World::TrackElement* getElTrackAt(const TrackRemovalArgs& args, const uint8_t trackFlags, const World::Pos3 pos, const uint8_t sequenceIndex)
     {
-        auto tile = World::TileManager::get(args.pos);
+        auto tile = World::TileManager::get(pos);
         for (auto& el : tile)
         {
             auto* elTrack = el.as<World::TrackElement>();
@@ -26,7 +29,7 @@ namespace OpenLoco::GameCommands
             {
                 continue;
             }
-            if (elTrack->sequenceIndex() != args.index)
+            if (elTrack->sequenceIndex() != sequenceIndex)
             {
                 continue;
             }
@@ -47,6 +50,49 @@ namespace OpenLoco::GameCommands
         return nullptr;
     };
 
+    // 0x0049CC23
+    static currency32_t trackRemoveCost(const TrackRemovalArgs& args, const World::TrackData::PreviewTrack trackPiece0, const World::Pos3 trackStart, const uint8_t trackFlags)
+    {
+        const auto trackLoc = trackStart + World::Pos3{ Math::Vector::rotate(World::Pos2{ trackPiece0.x, trackPiece0.y }, args.rotation), trackPiece0.z };
+        auto* pieceElTrack = getElTrackAt(args, trackFlags, trackLoc, trackPiece0.index);
+        if (pieceElTrack == nullptr)
+        {
+            return 0;
+        }
+
+        currency32_t totalCost = 0;
+
+        const auto* trackObj = ObjectManager::get<TrackObject>(pieceElTrack->trackObjectId());
+        totalCost += Economy::getInflationAdjustedCost(trackObj->sellCostFactor, trackObj->costIndex, 10);
+
+        // Check mod removal costs
+        for (auto i = 0U; i < 4; i++)
+        {
+            const auto* trackExtraObj = ObjectManager::get<TrackExtraObject>(trackObj->mods[i]);
+            totalCost += Economy::getInflationAdjustedCost(trackExtraObj->sellCostFactor, trackExtraObj->costIndex, 10);
+        }
+
+        return totalCost;
+    }
+
+    // 0x004795D1
+    // Shared with removeRoad. Not investigated.
+    static void sub_4795D1(const World::Pos3 pos)
+    {
+        registers regs;
+        regs.ax = pos.x;
+        regs.cx = pos.y;
+        regs.dl = pos.z;
+        call(0x004795D1, regs);
+    }
+
+    // 0x0048B04E
+    static void playTrackRemovalSound(const World::Pos3 pos)
+    {
+        const auto frequency = gPrng1().randNext(8191, 17955);
+        Audio::playSound(Audio::SoundId::demolish, pos, 0, frequency);
+    }
+
     // 0x0049C7F2
     static currency32_t removeTrack(const TrackRemovalArgs& args, uint8_t flags)
     {
@@ -65,7 +111,7 @@ namespace OpenLoco::GameCommands
             trackFlags |= 0x20;
         }
 
-        auto* elTrack = getTrack(args, trackFlags);
+        auto* elTrack = getElTrackAt(args, trackFlags, args.pos, args.index);
         if (elTrack == nullptr)
         {
             return FAILURE;
@@ -112,7 +158,7 @@ namespace OpenLoco::GameCommands
 
         // 0x0049C967
         // Fetch track element again; signal removal above might have invalidated the pointer.
-        elTrack = getTrack(args, trackFlags);
+        elTrack = getElTrackAt(args, trackFlags, args.pos, args.index);
 
         // 0x0049C9F0
         if (elTrack->hasStationElement())
@@ -136,9 +182,73 @@ namespace OpenLoco::GameCommands
 
         // 0x0049CA3B
         // Fetch track element again; station removal above might have invalidated the pointer.
-        elTrack = getTrack(args, trackFlags);
+        elTrack = getElTrackAt(args, trackFlags, args.pos, args.index);
 
         // 0x0049CAC4
+        const auto trackPieces = World::TrackData::getTrackPiece(args.trackId);
+        auto& trackPiece = trackPieces[args.index];
+
+        const auto trackStart = args.pos - World::Pos3{ Math::Vector::rotate(World::Pos2{ trackPiece.x, trackPiece.y }, args.rotation), trackPiece.z };
+
+        // NB: moved out of the loop below (was at 0x0049CC1B)
+        const currency32_t pieceRemovalCost = trackRemoveCost(args, trackPieces[0], trackStart, trackFlags);
+        if (static_cast<uint32_t>(pieceRemovalCost) == FAILURE)
+        {
+            return FAILURE;
+        }
+
+        bool trackHadBridge = false; // 0x0113605B
+        int8_t trackBridgeId = -1;   // 0x0113605C
+
+        // 0x0049CB27
+        for (auto& piece : trackPieces)
+        {
+            const auto trackLoc = trackStart + World::Pos3{ Math::Vector::rotate(World::Pos2{ piece.x, piece.y }, args.rotation), piece.z };
+
+            if ((trackFlags & 0x20) == 0) // aiAllocated
+            {
+                World::TileManager::mapInvalidateTileFull(trackLoc); // shift piece.z?
+            }
+
+            // 0x0049CB84
+            auto* pieceElTrack = getElTrackAt(args, trackFlags, trackLoc, piece.index);
+            if (pieceElTrack == nullptr)
+            {
+                return FAILURE;
+            }
+
+            // 0x0049CC02
+            if (pieceElTrack->hasBridge())
+            {
+                trackHadBridge = true;
+                trackBridgeId = pieceElTrack->bridge();
+            }
+
+            if (!(flags & Flags::apply))
+            {
+                continue;
+            }
+
+            World::TileManager::removeElement(*reinterpret_cast<World::TileElement*>(pieceElTrack));
+            sub_4795D1(trackLoc);
+        }
+
+        totalRemovalCost += pieceRemovalCost;
+
+        if (trackHadBridge)
+        {
+            const auto* bridgeObj = ObjectManager::get<BridgeObject>(trackBridgeId);
+            totalRemovalCost += Economy::getInflationAdjustedCost(bridgeObj->sellCostFactor, bridgeObj->costIndex, 10);
+        }
+
+        if (flags & Flags::apply)
+        {
+            if (!(flags & (Flags::aiAllocated | Flags::ghost)))
+            {
+                playTrackRemovalSound(args.pos);
+            }
+        }
+
         return totalRemovalCost;
     }
 
