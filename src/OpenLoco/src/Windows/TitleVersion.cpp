876251520b857ee328d2e3e03324898aacd1f07a
Aaron van Geffen <aaron@aaronweb.net>
2024-08-01 12:35:35 +0200

    TitleVersion: remove unneeded interop include

--- src/OpenLoco/src/Windows/TitleVersion.cpp
+++ src/OpenLoco/src/Windows/TitleVersion.cpp
@@ -6,9 +6,6 @@
 #include "Ui/Widget.h"
 #include "Ui/Window.h"
 #include "Ui/WindowManager.h"
-#include <OpenLoco/Interop/Interop.hpp>
-
-using namespace OpenLoco::Interop;
 
 namespace OpenLoco::Ui::Windows::TitleVersion
 {
