ebda1178c20914c91a3b71742d335f249b2aa08d
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2024-09-21 09:50:06 +0300

    Create Button widget type

---
+++ src/OpenLoco/src/Ui/Widgets/ButtonWidget.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Ui/Widget.h"
+
+namespace OpenLoco::Ui::Widgets
+{
+    struct Button : public Widget
+    {
+        constexpr Button(Ui::Point32 origin, Ui::Size32 size, WindowColour colour, uint32_t content = Widget::kContentNull, StringId tooltip = StringIds::null)
+            : Widget(origin, size, WidgetType::button, colour, content, tooltip)
+        {
+            events.draw = &draw;
+        }
+
+        static void draw(Gfx::DrawingContext& drawingCtx, const Widget& widget, const WidgetState& widgetState);
+    };
+
+}
