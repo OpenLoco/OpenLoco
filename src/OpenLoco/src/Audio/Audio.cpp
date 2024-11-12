4ae39cc5064d60a0ae0dcc7f338a58eed34e05c2
LeeSpork <lblogan@outlook.co.nz>
2024-11-12 16:20:44 +1300

    Audio.cpp: remove currentTrackPathId
    Removes all all occurrences of `currentTrackPathId` in Audio.cpp, and an if block that was checking its value.
    
    Fixes #2667

--- src/OpenLoco/src/Audio/Audio.cpp
+++ src/OpenLoco/src/Audio/Audio.cpp
@@ -1203,8 +1203,6 @@ namespace OpenLoco::Audio
         }
     }
 
-    static PathId currentTrackPathId;
-
     // 0x0048AC66
     // previously called void playTitleScreenMusic()
     void playMusic(PathId sample, int32_t volume, bool loop)
@@ -1215,12 +1213,6 @@ namespace OpenLoco::Audio
             return;
         }
 
-        if (currentTrackPathId == sample)
-        {
-            return;
-        }
-
-        currentTrackPathId = sample;
         channel->stop();
 
         auto musicSample = loadMusicSample(sample);
@@ -1251,7 +1243,6 @@ namespace OpenLoco::Audio
         if (_audioInitialised && channel != nullptr && channel->isPlaying())
         {
             channel->stop();
-            currentTrackPathId = PathId::g1; // there is no 'null' or 'none' so just set it to something that isn't a music track
         }
     }
 
