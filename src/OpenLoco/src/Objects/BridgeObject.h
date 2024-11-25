4db03083d16fd367d64fe7e3b9e8f3c3cf5f18fa
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2024-11-25 09:34:03 +0000

    Start naming image indexs

--- src/OpenLoco/src/Objects/BridgeObject.h
+++ src/OpenLoco/src/Objects/BridgeObject.h
@@ -60,4 +60,58 @@ namespace OpenLoco
 #pragma pack(pop)
 
     static_assert(sizeof(BridgeObject) == 0x2C);
+
+    namespace Bridge::ImageIds
+    {
+        constexpr uint32_t previewImage = 0;
+
+        constexpr uint32_t deckBaseNEspan0 = 36;
+        constexpr uint32_t supportHeaderLhsNEspan0 = 37;
+        constexpr uint32_t supportHeaderRhsNEspan0 = 38;
+        constexpr uint32_t deckWallLhsNEspan0 = 39;
+        constexpr uint32_t deckWallRhsNEspan0 = 40;
+        constexpr uint32_t roofNEspan0 = 41;
+        constexpr uint32_t deckBaseNEspan1 = 42;
+        constexpr uint32_t supportHeaderLhsNEspan1 = 43;
+        constexpr uint32_t supportHeaderRhsNEspan1 = 44;
+        constexpr uint32_t deckWallLhsNEspan1 = 45;
+        constexpr uint32_t deckWallRhsNEspan1 = 46;
+        constexpr uint32_t roofNEspan1 = 47;
+        constexpr uint32_t deckBaseNEspan2 = 48;
+        constexpr uint32_t supportHeaderLhsNEspan2 = 49;
+        constexpr uint32_t supportHeaderRhsNEspan2 = 50;
+        constexpr uint32_t deckWallLhsNEspan2 = 51;
+        constexpr uint32_t deckWallRhsNEspan2 = 52;
+        constexpr uint32_t roofNEspan2 = 53;
+        constexpr uint32_t deckBaseNEspan3 = 54;
+        constexpr uint32_t supportHeaderLhsNEspan3 = 55;
+        constexpr uint32_t supportHeaderRhsNEspan3 = 56;
+        constexpr uint32_t deckWallLhsNEspan3 = 57;
+        constexpr uint32_t deckWallRhsNEspan3 = 58;
+        constexpr uint32_t roofNEspan3 = 59;
+        constexpr uint32_t deckBaseSWspan0 = 60;
+        constexpr uint32_t supportHeaderLhsSWspan0 = 61;
+        constexpr uint32_t supportHeaderRhsSWspan0 = 62;
+        constexpr uint32_t deckWallLhsSWspan0 = 63;
+        constexpr uint32_t deckWallRhsSWspan0 = 64;
+        constexpr uint32_t roofSWspan0 = 65;
+        constexpr uint32_t deckBaseSWspan1 = 66;
+        constexpr uint32_t supportHeaderLhsSWspan1 = 67;
+        constexpr uint32_t supportHeaderRhsSWspan1 = 68;
+        constexpr uint32_t deckWallLhsSWspan1 = 69;
+        constexpr uint32_t deckWallRhsSWspan1 = 70;
+        constexpr uint32_t roofSWspan1 = 71;
+        constexpr uint32_t deckBaseSWspan2 = 72;
+        constexpr uint32_t supportHeaderLhsSWspan2 = 73;
+        constexpr uint32_t supportHeaderRhsSWspan2 = 74;
+        constexpr uint32_t deckWallLhsSWspan2 = 75;
+        constexpr uint32_t deckWallRhsSWspan2 = 76;
+        constexpr uint32_t roofSWspan2 = 77;
+        constexpr uint32_t deckBaseSWspan3 = 78;
+        constexpr uint32_t supportHeaderLhsSWspan3 = 79;
+        constexpr uint32_t supportHeaderRhsSWspan3 = 80;
+        constexpr uint32_t deckWallLhsSWspan3 = 81;
+        constexpr uint32_t deckWallRhsSWspan3 = 82;
+        constexpr uint32_t roofSWspan3 = 83;
+    }
 }
