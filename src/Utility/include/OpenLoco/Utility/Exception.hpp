d59ec5c4cbaacaacbc347a33c10f75d599ea8c4e
Miguel <miguel.leyva@encora.com>
2024-12-30 11:22:57 -0700

    Added std::source_location utilities to exception.hpp

--- src/Utility/include/OpenLoco/Utility/Exception.hpp
+++ src/Utility/include/OpenLoco/Utility/Exception.hpp
@@ -1,15 +1,19 @@
 #pragma once
 
 #include <stdexcept>
+#include <source_location>
+#include <iostream>
 
 namespace OpenLoco
 {
     class NotImplementedException : public std::logic_error
     {
     public:
-        NotImplementedException()
-            : std::logic_error("function or case not implemented yet.")
+        NotImplementedException(std::source_location location)
+            : std::logic_error("function or case not implemented yet")
         {
+            std::clog << "at file: " << location.file_name() << "(" << location.line() << ":" << location.column() << ") "
+                      << "function or case not implemented yet" << std::endl;
         }
     };
 }
