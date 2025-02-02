f84ca7b3c32904006cb856e5d621085136ebf84e
Spacek531 <spacek531@comcast.net>
2025-02-01 20:40:57 -0800

    more tentative names

--- src/OpenLoco/src/Vehicles/Vehicle1.cpp
+++ src/OpenLoco/src/Vehicles/Vehicle1.cpp
@@ -229,7 +229,7 @@ namespace OpenLoco::Vehicles
 
         if (!(_vehicleUpdate_var_1136114 & UpdateVar1136114Flags::noRouteFound))
         {
-            if (_vehicleUpdate_var_1136114 & UpdateVar1136114Flags::unk_m04)
+            if (_vehicleUpdate_var_1136114 & UpdateVar1136114Flags::gradeCrossingSound)
             {
                 railProduceCrossingWhistle(*train.veh2);
             }
