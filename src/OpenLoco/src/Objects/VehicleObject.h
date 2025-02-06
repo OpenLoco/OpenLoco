6e6514e96e1bed09c1795c3588323e9c6f31c3ff
Spacek531 <spacek531@comcast.net>
2025-02-06 00:01:02 -0800

    un-name flag 10

--- src/OpenLoco/src/Objects/VehicleObject.h
+++ src/OpenLoco/src/Objects/VehicleObject.h
@@ -202,7 +202,7 @@ namespace OpenLoco
         flag_08 = 1U << 8,
         aircraftIsTaildragger = 1U << 8,
         anyRoadType = 1U << 9, // set on all road vehicles except trams
-        speedControl = 1U << 10,
+        flag_10 = 1U << 10,
         cannotCoupleToSelf = 1U << 11,
         aircraftFlaresLanding = 1U << 11, // set only on Concorde
         mustHavePair = 1U << 12,          // train requires two or more of this vehicle
