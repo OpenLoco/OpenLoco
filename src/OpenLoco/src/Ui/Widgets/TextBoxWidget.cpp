e34e32aab89f8a219ca3f5d70f0e2c5ffba6263b
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2025-02-07 18:46:11 +0200

    Implement text drawing for TextBox widget

--- src/OpenLoco/src/Ui/Widgets/TextBoxWidget.cpp
+++ src/OpenLoco/src/Ui/Widgets/TextBoxWidget.cpp
@@ -1,9 +1,37 @@
 #include "TextBoxWidget.h"
 #include "Graphics/SoftwareDrawingEngine.h"
+#include "Graphics/TextRenderer.h"
 #include "Ui/Window.h"
 
 namespace OpenLoco::Ui::Widgets
 {
+    // 0x4CB2D6
+    static void drawText(Gfx::DrawingContext& drawingCtx, const Widget& widget, const WidgetState& widgetStated)
+    {
+        if (widget.content == Widget::kContentNull || widget.content == Widget::kContentUnk)
+        {
+            return;
+        }
+
+        auto colour = widgetStated.colour;
+        if (widgetStated.disabled)
+        {
+            colour = colour.inset();
+        }
+        else
+        {
+            colour = colour.FD();
+        }
+
+        auto formatArgs = FormatArguments(widget.textArgs);
+        auto* window = widgetStated.window;
+        auto point = Point(window->x + widget.left + 1, window->y + widget.top);
+        int width = widget.right - widget.left - 2;
+
+        auto tr = Gfx::TextRenderer(drawingCtx);
+        tr.drawStringLeftClipped(point, width, colour, widget.text, formatArgs);
+    }
+
     // 0x4CB29C
     void TextBox::draw(Gfx::DrawingContext& drawingCtx, const Widget& widget, const WidgetState& widgetState)
     {
@@ -17,5 +45,7 @@ namespace OpenLoco::Ui::Widgets
             window->y + widget.bottom,
             widgetState.colour,
             flags);
+
+        drawText(drawingCtx, widget, widgetState);
     }
 }
