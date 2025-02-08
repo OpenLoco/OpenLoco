6f5728035dfdc1187ff7e37fa0965da517737359
Spacek531 <spacek531@comcast.net>
2025-02-07 20:26:40 -0800

    re-add shift-clicking vehicle

--- src/OpenLoco/src/Ui/Windows/Vehicle.cpp
+++ src/OpenLoco/src/Ui/Windows/Vehicle.cpp
@@ -1711,6 +1711,10 @@ namespace OpenLoco::Ui::Windows::Vehicle
                 {
                     args.paintFlags ^= GameCommands::VehicleRepaintFlags::applyToEntireCar;
                 }
+                if (Input::hasKeyModifier(Input::KeyModifier::shift))
+                {
+                    args.paintFlags |= GameCommands::VehicleRepaintFlags::applyToEntireTrain;
+                }
 
                 args.setColours(getPaintToolColour(self), args.paintFlags);
                 args.head = veh->id;
@@ -1728,7 +1732,12 @@ namespace OpenLoco::Ui::Windows::Vehicle
             args.setColours(getPaintToolColour(self), args.paintFlags);
             args.head = car.front->id;
 
-            if (Input::hasKeyModifier(Input::KeyModifier::control))
+            if (Input::hasKeyModifier(Input::KeyModifier::shift))
+            {
+                args.paintFlags |= GameCommands::VehicleRepaintFlags::applyToEntireTrain;
+            }
+
+            if (Input::hasKeyModifier(Input::KeyModifier::control) && !(args.paintFlags && GameCommands::VehicleRepaintFlags::applyToEntireTrain))
             {
                 auto obj = ObjectManager::get<VehicleObject>(car.front->objectId);
 
