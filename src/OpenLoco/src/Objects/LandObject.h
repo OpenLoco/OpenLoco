9fa091786a27541e3d0aade62778ed9e5c7af52a
Spacek531 <spacek531@comcast.net>
2025-03-08 12:05:27 -0800

    reword comment again

--- src/OpenLoco/src/Objects/LandObject.h
+++ src/OpenLoco/src/Objects/LandObject.h
@@ -35,8 +35,8 @@ namespace OpenLoco
 
         StringId name;
         uint8_t costIndex;                // 0x02
-        uint8_t numGrowthStages;          // 0x03 "healing" animation after disturbing land
-        uint8_t numImageAngles;           // 0x04 no vanilla object uses this but is identical to numFarmImageAngles of IndustryObject
+        uint8_t numGrowthStages;          // 0x03 "Healing" animation after disturbing land
+        uint8_t numImageAngles;           // 0x04 How many rotation angles the land has
         LandObjectFlags flags;            // 0x05
         uint8_t cliffEdgeHeader1;         // 0x06
         uint8_t cliffEdgeHeader2;         // 0x07
