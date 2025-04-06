c780ff6c86a05173606f3588ec6bd16f956a4596
Aaron van Geffen <aaron@aaronweb.net>
2025-04-06 14:29:45 +0200

    Split off getRoadElement function

--- src/OpenLoco/src/GameCommands/Road/RemoveRoad.cpp
+++ src/OpenLoco/src/GameCommands/Road/RemoveRoad.cpp
@@ -21,40 +21,11 @@ namespace OpenLoco::GameCommands
     static loco_global<uint8_t, 0x0112C2F3> _byte_112C2F3;  // flags
     static loco_global<uint8_t, 0x0112C2F7> _companyId;     // company id
 
-    // 0x004775A5
-    static uint32_t removeRoad(const RoadRemovalArgs& args, uint8_t flags)
+    static World::RoadElement* getRoadElement(const World::Tile& tile, const RoadRemovalArgs& args, uint8_t flags)
     {
-        setExpenditureType(ExpenditureType::Construction);
-        setPosition(args.pos + World::Pos3{ 16, 16, 0 });
-
-        // TODO: trying to use args instead of these; just for reference
-        _dword_112C298 = 0;
-        _byte_112C2CD = 0;
-        _byte_112C2D0 = 0xFF;
-        _byte_112C2CE = args.sequenceIndex;
-        _byte_112C2CF = args.roadId;
-        _word_112C2B0 = args.objectId;
-
-        // TODO: already obsolete; just for reference
-        _byte_112C2F3 = 0;
-        if (flags & Flags::aiAllocated)
-        {
-            _byte_112C2F3 = _byte_112C2F3 | 0x10;
-        }
-        if (flags & Flags::ghost)
-        {
-            _byte_112C2F3 = _byte_112C2F3 | 0x20;
-        }
-
+        const auto baseZ = args.pos.z / World::kSmallZStep;
         const auto companyId = SceneManager::isEditorMode() ? CompanyId::neutral : getUpdatingCompanyId();
 
-        // 0x0047762D
-        auto tile = World::TileManager::get(args.pos.x, args.pos.y);
-        const auto baseZ = args.pos.z / World::kSmallZStep;
-
-        // This entire loop was made redundant by a no-op write in Hooks.cpp
-        // writeNop(0x004776DD, 6);
-        /*
         for (auto& element : tile)
         {
             auto* elRoad = element.as<World::RoadElement>();
@@ -92,19 +63,59 @@ namespace OpenLoco::GameCommands
             }
             if (flags & Flags::aiAllocated)
             {
-                break; // to 0x004776E3
-            }
-            if (!sub_431E6A(companyId, reinterpret_cast<const World::TileElement*>(&elRoad)))
-            {
-                return FAILURE;
+                return nullptr; // to 0x004776E3
             }
             if (elRoad->owner() != CompanyId::neutral)
             {
-                break; // to 0x004776E3
+                return nullptr; // to 0x004776E3
             }
             if (elRoad->mods())
             {
-                break; // to 0x004776E3
+                return nullptr; // to 0x004776E3
+            }
+
+            return elRoad;
+        }
+
+        return nullptr;
+    }
+
+    // 0x004775A5
+    static uint32_t removeRoad(const RoadRemovalArgs& args, uint8_t flags)
+    {
+        setExpenditureType(ExpenditureType::Construction);
+        setPosition(args.pos + World::Pos3{ 16, 16, 0 });
+
+        // TODO: trying to use args instead of these; just for reference
+        _dword_112C298 = 0;
+        _byte_112C2CD = 0;
+        _byte_112C2D0 = 0xFF;
+        _byte_112C2CE = args.sequenceIndex;
+        _byte_112C2CF = args.roadId;
+        _word_112C2B0 = args.objectId;
+
+        // TODO: already obsolete; just for reference
+        _byte_112C2F3 = 0;
+        if (flags & Flags::aiAllocated)
+        {
+            _byte_112C2F3 = _byte_112C2F3 | 0x10;
+        }
+        if (flags & Flags::ghost)
+        {
+            _byte_112C2F3 = _byte_112C2F3 | 0x20;
+        }
+
+        // 0x0047762D
+        auto tile = World::TileManager::get(args.pos.x, args.pos.y);
+
+        // This part was made redundant by a no-op write in Hooks.cpp
+        // writeNop(0x004776DD, 6);
+        /*
+        if (auto* roadEl = getRoadElement(tile, args, flags); roadEl != nullptr)
+        {
+            if (!sub_431E6A(companyId, reinterpret_cast<const World::TileElement*>(&elRoad)))
+            {
+                return FAILURE;
             }
 
             setErrorText(StringIds::empty);
@@ -123,75 +134,31 @@ namespace OpenLoco::GameCommands
         currency32_t totalCost = 0;
 
         // 0x004776E3
-        for (auto& element : tile)
+        if (auto* roadEl = getRoadElement(tile, args, flags); roadEl != nullptr)
         {
-            auto* elRoad = element.as<World::RoadElement>();
-            if (elRoad == nullptr)
-            {
-                continue;
-            }
-            if (elRoad->baseZ() != baseZ)
-            {
-                continue;
-            }
-            if (elRoad->rotation() != args.rotation)
-            {
-                continue;
-            }
-            if (elRoad->sequenceIndex() != args.sequenceIndex)
-            {
-                continue;
-            }
-            if (elRoad->roadObjectId() != args.objectId)
-            {
-                continue;
-            }
-            if (elRoad->roadId() != args.roadId)
-            {
-                continue;
-            }
-            if ((elRoad->isGhost() && !(flags & Flags::ghost)) || (elRoad->isAiAllocated() && !(flags & Flags::aiAllocated)))
-            {
-                continue;
-            }
-            if ((flags & Flags::aiAllocated) && elRoad->owner() != companyId)
-            {
-                continue;
-            }
-            if (elRoad->isLast())
-            {
-                break; // to 0x004777EB
-            }
-
-            auto* nextEl = elRoad->next();
+            auto* nextEl = roadEl->next();
             auto* nextRoad = nextEl->as<World::RoadElement>();
-            if (nextRoad == nullptr)
+            if (nextRoad != nullptr)
             {
-                break; // to 0x004777EB
+                auto* elStation = tile.roadStation(nextRoad->roadId(), nextRoad->rotation(), nextRoad->baseZ());
+                if (elStation != nullptr)
+                {
+                    RoadStationRemovalArgs srArgs = {};
+                    srArgs.pos = args.pos;
+                    srArgs.rotation = args.rotation;
+                    srArgs.roadId = args.roadId;
+                    srArgs.index = args.sequenceIndex;
+                    srArgs.roadObjectId = args.objectId;
+
+                    auto stationRemovalRes = GameCommands::doCommand(srArgs, flags);
+                    if (stationRemovalRes == FAILURE)
+                    {
+                        return FAILURE;
+                    }
+
+                    totalCost += stationRemovalRes;
+                }
             }
-            // OMITTED: check against tile start / prev tile
-
-            auto* elStation = tile.roadStation(elRoad->roadId(), elRoad->rotation(), elRoad->baseZ());
-            if (elStation == nullptr)
-            {
-                break; // to 0x004777EB
-            }
-
-            RoadStationRemovalArgs srArgs = {};
-            srArgs.pos = args.pos;
-            srArgs.rotation = args.rotation;
-            srArgs.roadId = args.roadId;
-            srArgs.index = args.sequenceIndex;
-            srArgs.roadObjectId = args.objectId;
-
-            auto stationRemovalRes = GameCommands::doCommand(srArgs, flags);
-            if (stationRemovalRes == FAILURE)
-            {
-                return FAILURE;
-            }
-
-            totalCost += stationRemovalRes;
-            break; // to 0x004777EB
         }
 
         // 0x004777EB
