0ef95beca4a6e1be2f0e39e956a294be158d8413
Aaron van Geffen <aaron@aaronweb.net>
2025-05-05 20:59:28 +0200

    Basis for createRoadMod command

---
+++ src/OpenLoco/src/GameCommands/Road/CreateRoadMod.cpp
@@ -0,0 +1,90 @@
+#include "CreateRoadMod.h"
+#include "Map/RoadElement.h"
+#include "Map/TileManager.h"
+
+namespace OpenLoco::GameCommands
+{
+    /*
+    struct RoadModsPlacementArgs
+    {
+        World::Pos3 pos;
+        uint8_t rotation;
+        uint8_t roadId;
+        uint8_t index;
+        uint8_t type;
+        uint8_t roadObjType;
+        World::Track::ModSection modSection;
+
+        registers regs;
+        regs.ax = pos.x;
+        regs.cx = pos.y;
+        regs.bh = rotation;
+        regs.dl = roadId;
+        regs.dh = index;
+        regs.edi = pos.z | (type << 16);
+        regs.ebp = roadObjType | (enumValue(modSection) << 16);
+    };
+    */
+
+    static World::RoadElement* getRoadElement(const RoadModsPlacementArgs& args)
+    {
+        auto tile = World::TileManager::get(args.pos);
+        for (auto& element : tile)
+        {
+            auto* roadEl = element.as<World::RoadElement>();
+            if (roadEl == nullptr)
+            {
+                continue;
+            }
+            if (roadEl->baseHeight() != args.pos.z)
+            {
+                continue;
+            }
+            if (roadEl->rotation() != args.rotation)
+            {
+                continue;
+            }
+            if (roadEl->rotation() != args.rotation)
+            {
+                continue;
+            }
+            if (roadEl->sequenceIndex() != args.index)
+            {
+                continue;
+            }
+            if (roadEl->roadObjectId() != args.roadObjType)
+            {
+                continue;
+            }
+            if (roadEl->roadId() != args.roadId)
+            {
+                continue;
+            }
+            return roadEl;
+        }
+        return nullptr;
+    }
+
+    // 0x0047A21E
+    static uint32_t createRoadMod(const RoadModsPlacementArgs& args, uint8_t flags)
+    {
+        uint32_t totalCost = 0;
+
+        auto* roadEl = getRoadElement(args);
+        if (roadEl == nullptr || !sub_431E6A(roadEl->owner(), reinterpret_cast<const World::TileElement*>(roadEl)))
+        {
+            return FAILURE;
+        }
+
+        auto& roadPieces = World::TrackData::getRoadPiece(args.roadId);
+        const auto roadLoc = args.pos + World::Pos3{ Math::Vector::rotate(World::Pos2{ piece.x, piece.y }, args.rotation), piece.z };
+
+
+        return totalCost;
+    }
+
+    void createRoadMod(registers& regs)
+    {
+        regs.ebx = createRoadMod(RoadModsPlacementArgs(regs), regs.bl);
+    }
+}
