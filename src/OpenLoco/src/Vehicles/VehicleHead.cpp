7f30e858d2184c4702163c3f0ae0ca068a8547e1
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2025-07-22 16:31:58 +0100

    Update name to match future work

--- src/OpenLoco/src/Vehicles/VehicleHead.cpp
+++ src/OpenLoco/src/Vehicles/VehicleHead.cpp
@@ -477,7 +477,7 @@ namespace OpenLoco::Vehicles
     }
 
     // 0x004AF5E1
-    static void sub_4AF5E1(Vehicles::VehicleHead& head)
+    static void connectJacobsBogies(Vehicles::VehicleHead& head)
     {
         registers regs{};
         regs.esi = X86Pointer(&head);
@@ -642,7 +642,7 @@ namespace OpenLoco::Vehicles
                 front ^= true;
             }
         }
-        sub_4AF5E1(*this);
+        connectJacobsBogies(*this);
     }
 
     // 0x004B90F0
