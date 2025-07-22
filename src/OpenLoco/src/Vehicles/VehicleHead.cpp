15f78aa6f05289c2ba28eae494c7295588d13ca9
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2025-07-22 16:28:48 +0100

    Fix rebase issues

--- src/OpenLoco/src/Vehicles/VehicleHead.cpp
+++ src/OpenLoco/src/Vehicles/VehicleHead.cpp
@@ -685,7 +685,7 @@ namespace OpenLoco::Vehicles
                     continue;
                 }
                 car.body->bodyIndex = front ? 0 : 1;
-                car.body->objectSpriteType = vehicleObj->carComponents[car.body->bodyIndex].bodySpriteInd & ~SpriteIndex::flag_unk7;
+                car.body->objectSpriteType = vehicleObj->carComponents[car.body->bodyIndex].bodySpriteInd & ~SpriteIndex::isReversed;
                 front ^= true;
             }
         }
