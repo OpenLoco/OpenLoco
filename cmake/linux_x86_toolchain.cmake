4e9647f5a5067f8f462d18b6714e4c4d53ad10ea
Michał Janiszewski <janisozaur@gmail.com>
2025-07-24 22:48:54 +0200

    Use Arch, btw

--- cmake/linux_x86_toolchain.cmake
+++ cmake/linux_x86_toolchain.cmake
@@ -4,6 +4,18 @@ set(CMAKE_SYSTEM_PROCESSOR "i686")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -m32")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -m32")
 
-set(CMAKE_FIND_ROOT_PATH "/usr/lib/i386-linux-gnu")
+# Support both Debian/Ubuntu (/usr/lib/i386-linux-gnu) and Arch Linux (/usr/lib32) paths
+set(CMAKE_FIND_ROOT_PATH "/usr/lib/i386-linux-gnu" "/usr/lib32")
 
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)
+
+# Auto-detect PNG and ZLIB library paths for different distributions
+if(EXISTS "/usr/lib32/libpng16.so" AND EXISTS "/usr/lib32/libz.so")
+    # Arch Linux paths
+    set(PNG_LIBRARY "/usr/lib32/libpng16.so" CACHE FILEPATH "PNG library")
+    set(ZLIB_LIBRARY "/usr/lib32/libz.so" CACHE FILEPATH "ZLIB library")
+elseif(EXISTS "/usr/lib/i386-linux-gnu/libpng16.so" AND EXISTS "/usr/lib/i386-linux-gnu/libz.so")
+    # Debian/Ubuntu paths
+    set(PNG_LIBRARY "/usr/lib/i386-linux-gnu/libpng16.so" CACHE FILEPATH "PNG library")
+    set(ZLIB_LIBRARY "/usr/lib/i386-linux-gnu/libz.so" CACHE FILEPATH "ZLIB library")
+endif()
