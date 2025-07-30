9b01076c16d5ec225372d422f966d58c9f7fa07d
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2025-07-30 08:18:07 +0100

    Add missing offset

--- src/OpenLoco/src/Vehicles/Routing.cpp
+++ src/OpenLoco/src/Vehicles/Routing.cpp
@@ -702,6 +702,7 @@ namespace OpenLoco::Vehicles
             transformFunction);
     }
 
+    // 0x004A2A58
     uint8_t sub_4A2A58(const World::Pos3& loc, const TrackAndDirection::_TrackAndDirection trackAndDirection, const CompanyId company, const uint8_t trackType)
     {
         // 0x001135F88
