526a68c596652e9c51162c0cae8f29bd5c9be58f
LeeSpork <lblogan@outlook.co.nz>
2025-08-06 13:51:54 +1200

    Create and use PauseFlags enum

--- src/OpenLoco/src/SceneManager.h
+++ src/OpenLoco/src/SceneManager.h
@@ -13,6 +13,15 @@ namespace OpenLoco
         MAX = ExtraFastForward,
     };
 
+    enum class PauseFlags : uint8_t
+    {
+        none = 0U,
+        Standard = 1 << 0,   // Used when the player toggles paused
+        PromptSave = 1 << 1, // Set when PromptSaveWindow is open
+        BrowsePrompt = 1 << 2,
+    };
+    OPENLOCO_ENABLE_ENUM_OPERATORS(PauseFlags);
+
     namespace SceneManager
     {
         enum class Flags : uint16_t
@@ -48,9 +57,9 @@ namespace OpenLoco
         bool isSandboxMode();
         bool isPauseOverrideEnabled();
         bool isPaused();
-        uint8_t getPauseFlags();
-        void setPauseFlag(uint8_t value);
-        void unsetPauseFlag(uint8_t value);
+        PauseFlags getPauseFlags();
+        void setPauseFlag(PauseFlags value);
+        void unsetPauseFlag(PauseFlags value);
         GameSpeed getGameSpeed();
         void setGameSpeed(const GameSpeed speed);
     }
--- src/OpenLoco/src/Ui/Windows/News/Ticker.cpp
+++ src/OpenLoco/src/Ui/Windows/News/Ticker.cpp
@@ -167,7 +167,7 @@ namespace OpenLoco::Ui::Windows::NewsWindow::Ticker
             return;
         }
 
-        if (SceneManager::getPauseFlags() & (1 << 2))
+        if ((SceneManager::getPauseFlags() & PauseFlags::BrowsePrompt) == PauseFlags::BrowsePrompt)
         {
             return;
         }
