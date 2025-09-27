9bf9754571fe0f5961810901b5b8a6fe52412e85
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2025-09-27 21:23:39 +0300

    Remove duplicate and broken code

--- src/OpenLoco/src/Input/MouseInput.cpp
+++ src/OpenLoco/src/Input/MouseInput.cpp
@@ -1910,19 +1910,6 @@ namespace OpenLoco::Input
         }
         else
         {
-            if (Tutorial::state() == Tutorial::State::playing)
-            {
-                auto button = MouseButton(Tutorial::nextInput());
-                if (button == MouseButton::released)
-                {
-                    return rightMouseButtonReleased(x, y);
-                }
-            }
-            else if (!isRightMouseButtonDown())
-            {
-                return rightMouseButtonReleased(x, y);
-            }
-
             // 0x004C704E
             if (Tutorial::state() == Tutorial::State::playing)
             {
