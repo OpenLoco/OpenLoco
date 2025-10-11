133dfd584eb2bec59996cdcb010075a3abab26a7
LeeSpork <lblogan@outlook.co.nz>
2025-10-12 00:06:23 +1300

    Fix #3320

--- src/OpenLoco/src/GameCommands/Vehicles/RenameVehicle.cpp
+++ src/OpenLoco/src/GameCommands/Vehicles/RenameVehicle.cpp
@@ -41,12 +41,9 @@ namespace OpenLoco::GameCommands
 
         static char staticRenameBuffer[37]{};
 
-        if ((flags & Flags::apply) != 0)
-        {
-            static constexpr std::array<int, 3> kTransformTable = { 2, 0, 1 };
-            int arrayIndex = kTransformTable.at(args.i);
-            std::memcpy(staticRenameBuffer + arrayIndex * 12, args.buffer, 12);
-        }
+        static constexpr std::array<int, 3> kTransformTable = { 2, 0, 1 };
+        int arrayIndex = kTransformTable.at(args.i);
+        std::memcpy(staticRenameBuffer + arrayIndex * 12, args.buffer, 12);
 
         if (args.i != 0)
         {
