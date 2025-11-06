5fff9d56bae56623fbc366b9c757cdea060c73b7
LeeSpork <lblogan@outlook.co.nz>
2025-11-06 14:26:32 +1300

    Add sorting to makeAllMusicPlaylist()

--- src/OpenLoco/src/Jukebox.h
+++ src/OpenLoco/src/Jukebox.h
@@ -13,6 +13,7 @@ namespace OpenLoco::Jukebox
     using MusicId = uint8_t;
 
     constexpr int32_t kNumMusicTracks = 29;
+    static constexpr MusicId kNoSong = 0xFF;
 
     struct MusicInfo
     {
@@ -22,14 +23,22 @@ namespace OpenLoco::Jukebox
         uint16_t endYear;
     };
 
-    static constexpr MusicId kNoSong = 0xFF;
+    enum class MusicSortMode : uint8_t
+    {
+        original, // vanilla ordered tracks by interal ID
+        name,
+        nameReverse,
+        year,
+        yearReverse,
+    };
 
     const MusicInfo& getMusicInfo(MusicId track);
     bool isMusicPlaying();
     MusicId getCurrentTrack();
     StringId getSelectedTrackTitleId();
-
+    std::vector<MusicId> makeAllMusicPlaylist(MusicSortMode ordering = MusicSortMode::original);
     std::vector<MusicId> makeSelectedPlaylist();
+
     const MusicInfo& changeTrack();
     bool requestTrack(MusicId track);
     bool skipCurrentTrack();
