ad5dcc6edc31e95669582072ce5e780d3500fc48
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2025-11-06 16:59:04 +0000

    Add a few more exports

--- src/OpenLoco/src/S5/S5Message.h
+++ src/OpenLoco/src/S5/S5Message.h
@@ -1,12 +1,25 @@
 #include <cstdint>
 
+namespace OpenLoco
+{
+    struct Message;
+}
+
 namespace OpenLoco::S5
 {
 #pragma pack(push, 1)
     struct Message
     {
-        uint8_t pad_0[0xD4];
+        uint8_t type;             // 0x00
+        char messageString[198];  // 0x01
+        uint8_t companyId;        // 0xC7
+        uint16_t timeActive;      // 0xC8 (1 << 15) implies manually opened news 0xFFFF implies no longer active
+        uint16_t itemSubjects[3]; // 0xCA
+        uint32_t date;            // 0xD0
     };
     static_assert(sizeof(Message) == 0xD4);
 #pragma pack(pop)
+
+    
+    S5::Message exportMessage(OpenLoco::Message& src);
 }
---
+++ src/OpenLoco/src/S5/S5Station.cpp
@@ -0,0 +1,50 @@
+#include "S5Station.h"
+#include "World/Station.h"
+#include <ranges>
+
+namespace OpenLoco::S5
+{
+    static S5::StationCargoStats exportCargoStats(const OpenLoco::StationCargoStats& src)
+    {
+        S5::StationCargoStats dst{};
+        dst.quantity = src.quantity;
+        dst.origin = enumValue(src.origin);
+        dst.flags = enumValue(src.flags);
+        dst.age = src.age;
+        dst.rating = src.rating;
+        dst.enrouteAge = src.enrouteAge;
+        dst.vehicleSpeed = src.vehicleSpeed.getRaw();
+        dst.vehicleAge = src.vehicleAge;
+        dst.industryId = enumValue(src.industryId);
+        dst.densityPerTile = src.densityPerTile;
+        return dst;
+    }
+
+    S5::Station exportStation(OpenLoco::Station& src)
+    {
+        S5::Station dst{};
+        dst.name = enumValue(src.name);
+        dst.x = src.x;
+        dst.y = src.y;
+        dst.z = src.z;
+        dst.labelFrame = exportLabelFrame(src.labelFrame);
+        dst.owner = enumValue(src.owner);
+        dst.noTilesTimeout = src.noTilesTimeout;
+        dst.flags = enumValue(src.flags);
+        dst.town = enumValue(src.town);
+        for (auto i = 0; i < std::size(dst.cargoStats); ++i)
+        {
+            dst.cargoStats[i] = exportCargoStats(src.cargoStats[i]);
+        }
+        dst.stationTileSize = src.stationTileSize;
+        std::ranges::copy(src.stationTiles, dst.stationTiles);
+        dst.var_3B0 = src.var_3B0;
+        dst.var_3B1 = src.var_3B1;
+        dst.var_3B2 = src.var_3B2;
+        dst.airportRotation = src.airportRotation;
+        dst.airportStartPos = src.airportStartPos;
+        dst.airportMovementOccupiedEdges = src.airportMovementOccupiedEdges;
+
+        return dst;
+    }
+}
