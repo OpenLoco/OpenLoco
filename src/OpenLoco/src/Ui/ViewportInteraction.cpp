24ab0689df204fbd9f42e3462e33425b2d731dee
Aaron van Geffen <aaron@aaronweb.net>
2025-11-24 10:47:20 +0100

    Allow fences to be interacted with outside the editor (#3476)

--- src/OpenLoco/src/Ui/ViewportInteraction.cpp
+++ src/OpenLoco/src/Ui/ViewportInteraction.cpp
@@ -784,10 +784,6 @@ namespace OpenLoco::Ui::ViewportInteraction
             return false;
         }
 
-        if (!SceneManager::isEditorMode())
-        {
-            return false;
-        }
         auto* wallObj = ObjectManager::get<WallObject>(wall->wallObjectId());
         FormatArguments::mapToolTip(StringIds::stringid_right_click_to_remove, wallObj->name);
         return true;
