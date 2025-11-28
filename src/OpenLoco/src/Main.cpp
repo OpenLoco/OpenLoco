ec2f1206618913e496d644f95eaafdf01893e0b5
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2025-11-28 20:59:46 +0000

    Fix assert ignore issue

--- src/OpenLoco/src/Main.cpp
+++ src/OpenLoco/src/Main.cpp
@@ -4,6 +4,10 @@
 
 int main(int argc, const char** argv)
 {
+#ifdef WIN32
+    // Ensures that assert dialogs allow for ignoring them (not the default behaviour for console subsystem)
+    _set_error_mode(_OUT_TO_MSGBOX);
+#endif
     SDL_SetMainReady();
     return OpenLoco::main(OpenLoco::Platform::getCmdLineVector(argc, argv));
 }
