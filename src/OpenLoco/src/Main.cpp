59dc5719cf902d48b480dd2ce41f80dc8ccb0817
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2025-11-28 09:11:25 +0000

    Okay how about now

--- src/OpenLoco/src/Main.cpp
+++ src/OpenLoco/src/Main.cpp
@@ -1,7 +1,9 @@
 #include "OpenLoco.h"
 #include <OpenLoco/Platform/Platform.h>
+#include <SDL2/SDL_main.h>
 
 int main(int argc, const char** argv)
 {
+    SDL_SetMainReady();
     return OpenLoco::main(OpenLoco::Platform::getCmdLineVector(argc, argv));
 }
