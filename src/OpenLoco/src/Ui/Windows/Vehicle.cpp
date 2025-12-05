622bea4e2d966d7f33d61bdd671d9c9a65577d7b
Aaron van Geffen <aaron@aaronweb.net>
2025-12-05 16:09:15 +0100

    Remove paint tool hack

--- src/OpenLoco/src/Ui/Windows/Vehicle.cpp
+++ src/OpenLoco/src/Ui/Windows/Vehicle.cpp
@@ -198,7 +198,6 @@ namespace OpenLoco::Ui::Windows::Vehicle
             pickup,
             remove,
             paintBrush,
-            paintBrushProxy, // hack until a better solution presents itself
             paintColourPrimary,
             paintColourSecondary,
             carList,
@@ -225,7 +224,6 @@ namespace OpenLoco::Ui::Windows::Vehicle
             Widgets::ImageButton({ 240, 68 }, { 24, 24 }, WindowColour::secondary, Widget::kContentNull, StringIds::tooltip_remove_from_track),
             Widgets::ImageButton({ 240, 96 }, { 24, 24 }, WindowColour::secondary, ImageIds::rubbish_bin, StringIds::tooltip_sell_or_drag_vehicle),
             Widgets::ImageButton({ 240, 122 }, { 24, 24 }, WindowColour::secondary, ImageIds::paintbrush, StringIds::vehicleRepaintTooltip),
-            Widgets::Frame({ 0, 0 }, { 0, 0 }, WindowColour::secondary, Widget::kContentNull, StringIds::null),
             Widgets::ColourButton({ 240, 150 }, { 16, 16 }, WindowColour::secondary, Widget::kContentNull, StringIds::tooltip_select_main_colour),
             Widgets::ColourButton({ 258, 150 }, { 16, 16 }, WindowColour::secondary, Widget::kContentNull, StringIds::tooltip_select_secondary_colour),
             Widgets::ScrollView({ 3, 44 }, { 237, 110 }, WindowColour::secondary, Scrollbars::vertical)
@@ -1808,14 +1806,14 @@ namespace OpenLoco::Ui::Windows::Vehicle
 
         static constexpr bool isPaintToolActive(Window& self)
         {
-            return self.activatedWidgets & (1U << widx::paintBrushProxy);
+            return ToolManager::isToolActive(self.type, self.number, widx::paintBrush);
         }
 
         static void paintToolBegin(Window& self)
         {
             bool active = ToolManager::toolSet(self, widx::paintBrush, CursorId::brush);
-            self.activatedWidgets &= ~(1U << widx::paintBrushProxy);
-            self.activatedWidgets |= (1U << widx::paintBrushProxy) * active;
+            self.activatedWidgets &= ~(1U << widx::paintBrush);
+            self.activatedWidgets |= (1U << widx::paintBrush) * active;
             self.invalidate();
         }
 
