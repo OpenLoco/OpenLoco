0bbab2e321762c6ddae967ea43d8326de4370af2
Aaron van Geffen <aaron@aaronweb.net>
2026-01-14 23:58:32 +0100

    Remove stray printf

--- src/OpenLoco/src/Ui/Window.cpp
+++ src/OpenLoco/src/Ui/Window.cpp
@@ -1266,7 +1266,6 @@ namespace OpenLoco::Ui
             {
                 const WidgetIndex_t widgetIndex = Input::getPressedWidgetIndex();
                 pressedWidget = 1ULL << widgetIndex;
-                printf("pressedWidget = %d\n", widgetIndex);
             }
         }
 
