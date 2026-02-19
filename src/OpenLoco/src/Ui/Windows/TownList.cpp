ba1dcc1571127386321f7fc3faf0a71132451767
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2026-02-19 23:41:37 +0200

    Fix #3652: off-by-one in town/industry list scroll click handling

--- src/OpenLoco/src/Ui/Windows/TownList.cpp
+++ src/OpenLoco/src/Ui/Windows/TownList.cpp
@@ -286,7 +286,7 @@ namespace OpenLoco::Ui::Windows::TownList
         static void onScrollMouseDown(Ui::Window& self, [[maybe_unused]] int16_t x, int16_t y, [[maybe_unused]] uint8_t scroll_index)
         {
             uint16_t currentRow = y / kRowHeight;
-            if (currentRow > self.var_83C)
+            if (currentRow >= self.var_83C)
             {
                 return;
             }
