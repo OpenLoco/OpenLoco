f6d47b7d78fc1aaa6dbdf18ea21e3cfe0add894b
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2026-02-22 16:26:16 +0200

    Move SourceLocation impl into SourceLocation.h

--- src/Core/include/OpenLoco/Core/Exception.hpp
+++ src/Core/include/OpenLoco/Core/Exception.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "SourceLocation.h"
 #include <exception>
 #include <fmt/format.h>
 #include <string>
@@ -7,49 +8,6 @@
 
 namespace OpenLoco::Exception
 {
-    namespace Detail
-    {
-        // TODO: Make this consteval for C++20
-        constexpr std::string_view sanitizePath(std::string_view path)
-        {
-#if defined(OPENLOCO_PROJECT_PATH)
-            constexpr std::string_view projectPath = OPENLOCO_PROJECT_PATH;
-            // Removes also the first slash.
-            return path.substr(projectPath.size() + 1);
-#else
-            return path;
-#endif
-        }
-    }
-
-    // Something like std::source_location except this works prior to C++20.
-    class SourceLocation
-    {
-        std::string _function;
-        std::string _file;
-        int _line;
-
-    public:
-        explicit SourceLocation(std::string_view func = __builtin_FUNCTION(), std::string_view file = Detail::sanitizePath(__builtin_FILE()), int line = __builtin_LINE())
-            : _function(func)
-            , _file(file)
-            , _line(line)
-        {
-        }
-        const std::string& file() const noexcept
-        {
-            return _file;
-        }
-        const std::string& function() const noexcept
-        {
-            return _function;
-        }
-        int line() const noexcept
-        {
-            return _line;
-        }
-    };
-
     namespace Detail
     {
         template<typename TExceptionTag>
