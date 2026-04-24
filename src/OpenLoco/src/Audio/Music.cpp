OpenLoco/OpenLoco

commit 04bec825c3f6ffb7967919ff1bf8da51f2d5ab89

Move jukebox into its own window (#3707)

src/OpenLoco/src/Ui/Windows/MusicJukebox.cpp
@@ -1,0 +1,342 @@
+#include "Audio/Audio.h"
+#include "Config.h"
+#include "Jukebox.h"
+#include "Localisation/StringIds.h"
+#include "Objects/InterfaceSkinObject.h"
+#include "Objects/ObjectManager.h"
+#include "SceneManager.h"
+#include "Ui/Dropdown.h"
+#include "Ui/Widgets/ButtonWidget.h"
+#include "Ui/Widgets/CaptionWidget.h"
+#include "Ui/Widgets/DropdownWidget.h"
+#include "Ui/Widgets/FrameWidget.h"
+#include "Ui/Widgets/ImageButtonWidget.h"
+#include "Ui/Widgets/LabelWidget.h"
+#include "Ui/Widgets/PanelWidget.h"
+#include "Ui/Widgets/TabWidget.h"
+#include "Ui/WindowManager.h"
+
+#include <cassert>
+
+namespace OpenLoco::Ui::Windows::MusicJukebox
+{
+    static constexpr Ui::Size kWindowSize = { 366, 109 };
+
+    namespace Widx
+    {
+        enum
+        {
+            frame,
+            caption,
+            close_button,
+            panel,
+            currently_playing_label,
+            currently_playing,
+            currently_playing_btn,
+            music_controls_stop,
+            music_controls_play,
+            music_controls_next,
+            music_playlist,
+            music_playlist_btn,
+            edit_selection
+        };
+    }
+
+    static constexpr auto _widgets = makeWidgets(
+        Widgets::Frame({ 0, 0 }, kWindowSize, WindowColour::primary),
+        Widgets::Caption({ 1, 1 }, { (uint16_t)(kWindowSize.width - 2), 13 }, Widgets::Caption::Style::whiteText, WindowColour::primary, StringIds::jukebox_window_title),
+        Widgets::ImageButton({ (int16_t)(kWindowSize.width - 15), 2 }, { 13, 13 }, WindowColour::primary, ImageIds::close_button, StringIds::tooltip_close_window),
+        Widgets::Panel({ 0, 15 }, { kWindowSize.width, 102 }, WindowColour::secondary),
+        Widgets::Label({ 10, 27 }, { 145, 12 }, WindowColour::secondary, ContentAlign::left, StringIds::currently_playing),
+        Widgets::dropdownWidgets({ 160, 27 }, { 196, 12 }, WindowColour::secondary, StringIds::stringid),
+        Widgets::ImageButton({ 10, 42 }, { 24, 24 }, WindowColour::secondary, ImageIds::music_controls_stop, StringIds::music_controls_stop_tip),
+        Widgets::ImageButton({ 34, 42 }, { 24, 24 }, WindowColour::secondary, ImageIds::music_controls_play, StringIds::music_controls_play_tip),
+        Widgets::ImageButton({ 58, 42 }, { 24, 24 }, WindowColour::secondary, ImageIds::music_controls_next, StringIds::music_controls_next_tip),
+        Widgets::dropdownWidgets({ 10, 69 }, { 346, 12 }, WindowColour::secondary, StringIds::stringid),
+        Widgets::Button({ 183, 86 }, { 173, 12 }, WindowColour::secondary, StringIds::edit_music_selection, StringIds::edit_music_selection_tip)
+
+    );
+
+    static void currentlyPlayingMouseDown(const Window& self);
+    static void currentlyPlayingDropdown(Window& self, int16_t itemIndex);
+    static void stopMusic(Window& self);
+    static void playMusic(Window& self);
+    static void playNextSong(Window& self);
+    static void musicPlaylistMouseDown(const Window& self);
+    static void musicPlaylistDropdown(Window& self, int16_t itemIndex);
+
+    // 0x004C0217, 0x004C0217
+    static void prepareDraw(Window& self)
+    {
+        self.widgets[Widx::frame].right = self.width - 1;
+        self.widgets[Widx::frame].bottom = self.height - 1;
+        self.widgets[Widx::panel].right = self.width - 1;
+        self.widgets[Widx::panel].bottom = self.height - 1;
+        self.widgets[Widx::caption].right = self.width - 2;
+        self.widgets[Widx::close_button].left = self.width - 15;
+        self.widgets[Widx::close_button].right = self.width - 15 + 12;
+
+        // Currently playing music track
+        {
+            StringId songName = StringIds::music_none;
+            if (SceneManager::isPlayMode())
+            {
+                songName = Jukebox::getSelectedTrackTitleId();
+            }
+            else if (SceneManager::isTitleMode())
+            {
+                auto& cfg = Config::get();
+                if (cfg.audio.playTitleMusic)
+                {
+                    songName = StringIds::music_locomotion_title;
+                }
+            }
+            auto args = FormatArguments(self.widgets[Widx::currently_playing].textArgs);
+            args.push(songName);
+        }
+
+        // Jukebox controls (stop/play/skip)
+        if (!SceneManager::isPlayMode())
+        {
+            self.disabledWidgets |= (1ULL << Widx::currently_playing) | (1ULL << Widx::currently_playing_btn) | (1ULL << Widx::music_controls_play) | (1ULL << Widx::music_controls_stop) | (1ULL << Widx::music_controls_next);
+        }
+        else if (Jukebox::isMusicPlaying())
+        {
+            // Play button appears pressed
+            self.activatedWidgets |= (1ULL << Widx::music_controls_play);
+        }
+        else
+        {
+            // Stop button appears pressed
+            self.activatedWidgets |= (1ULL << Widx::music_controls_stop);
+        }
+
+        // Selected playlist
+        {
+            static constexpr StringId playlist_string_ids[] = {
+                StringIds::play_only_music_from_current_era,
+                StringIds::play_all_music,
+                StringIds::play_custom_music_selection,
+            };
+
+            auto args = FormatArguments(self.widgets[Widx::music_playlist].textArgs);
+
+            StringId selectedPlaylistStringId = playlist_string_ids[enumValue(Config::get().audio.playlist)];
+            args.push(selectedPlaylistStringId);
+        }
+
+        // Edit custom playlist music selection button
+        if (Config::get().audio.playlist != Config::MusicPlaylistType::custom)
+        {
+            self.disabledWidgets |= (1ULL << Widx::edit_selection);
+        }
+    }
+
+    static void draw(Window& self, Gfx::DrawingContext& drawingCtx)
+    {
+        self.draw(drawingCtx);
+    }
+
+    static void onMouseUp(Window& self, WidgetIndex_t wi, [[maybe_unused]] const WidgetId id)
+    {
+        switch (wi)
+        {
+            case Widx::close_button:
+                WindowManager::close(&self);
+                return;
+
+            case Widx::music_controls_stop:
+                stopMusic(self);
+                return;
+
+            case Widx::music_controls_play:
+                playMusic(self);
+                return;
+
+            case Widx::music_controls_next:
+                playNextSong(self);
+                return;
+
+            case Widx::edit_selection:
+                MusicSelection::open();
+                return;
+        }
+    }
+
+    static void onMouseDown(Window& self, WidgetIndex_t wi, [[maybe_unused]] const WidgetId id)
+    {
+        switch (wi)
+        {
+            case Widx::music_playlist_btn:
+                musicPlaylistMouseDown(self);
+                break;
+            case Widx::currently_playing_btn:
+                currentlyPlayingMouseDown(self);
+                break;
+        }
+    }
+
+    static void onDropdown(Window& self, WidgetIndex_t widgetIndex, [[maybe_unused]] const WidgetId id, int16_t itemIndex)
+    {
+        switch (widgetIndex)
+        {
+            case Widx::music_playlist_btn:
+                musicPlaylistDropdown(self, itemIndex);
+                break;
+            case Widx::currently_playing_btn:
+                currentlyPlayingDropdown(self, itemIndex);
+                break;
+        }
+    }
+
+    // 0x004C0875
+    static void currentlyPlayingMouseDown(const Window& self)
+    {
+        auto tracks = Jukebox::makeSelectedPlaylist();
+
+        auto& dropdown = self.widgets[Widx::currently_playing];
+        Dropdown::show(self.x + dropdown.left, self.y + dropdown.top, dropdown.width() - 4, dropdown.height(), self.getColour(WindowColour::secondary), tracks.size(), 0x80);
+
+        int index = -1;
+        for (auto track : tracks)
+        {
+            index++;
+            Dropdown::add(index, StringIds::dropdown_stringid, Jukebox::getMusicInfo(track).titleId);
+            if (track == Jukebox::getCurrentTrack())
+            {
+                Dropdown::setItemSelected(index);
+            }
+        }
+    }
+
+    // 0x004C09F8
+    static void currentlyPlayingDropdown(Window& self, int16_t ax)
+    {
+        if (ax == -1)
+        {
+            return;
+        }
+
+        auto track = Jukebox::makeSelectedPlaylist().at(ax);
+        if (Jukebox::requestTrack(track))
+        {
+            self.invalidate();
+        }
+    }
+
+    // 0x004C0778
+    static void stopMusic(Window& self)
+    {
+        if (Jukebox::disableMusic())
+        {
+            self.invalidate();
+        }
+    }
+
+    // 0x004C07A4
+    static void playMusic(Window& self)
+    {
+        if (Jukebox::enableMusic())
+        {
+            self.invalidate();
+        }
+    }
+
+    // 0x004C07C4
+    static void playNextSong(Window& self)
+    {
+        if (Jukebox::skipCurrentTrack())
+        {
+            self.invalidate();
+        }
+    }
+
+    // 0x004C07E4
+    static void musicPlaylistMouseDown(const Window& self)
+    {
+        auto& dropdown = self.widgets[Widx::music_playlist];
+        Dropdown::show(self.x + dropdown.left, self.y + dropdown.top, dropdown.width() - 4, dropdown.height(), self.getColour(WindowColour::secondary), 3, 0x80);
+
+        Dropdown::add(0, StringIds::dropdown_stringid, StringIds::play_only_music_from_current_era);
+        Dropdown::add(1, StringIds::dropdown_stringid, StringIds::play_all_music);
+        Dropdown::add(2, StringIds::dropdown_stringid, StringIds::play_custom_music_selection);
+
+        Dropdown::setItemSelected(enumValue(Config::get().audio.playlist));
+    }
+
+    // 0x004C084A
+    static void musicPlaylistDropdown(Window& self, int16_t index)
+    {
+        if (index == -1)
+        {
+            return;
+        }
+
+        auto& cfg = Config::get().audio;
+        cfg.playlist = Config::MusicPlaylistType(index);
+        Config::write();
+
+        self.invalidate();
+
+        if (!SceneManager::isTitleMode()) // Prevents title music from stopping
+        {
+            Audio::revalidateCurrentTrack();
+        }
+
+        WindowManager::close(WindowType::musicSelection);
+    }
+
+    // 0x004C04E0, 0x004C0A37
+    static void onUpdate(Window& self)
+    {
+        self.frameNo += 1;
+        self.callPrepareDraw();
+        WindowManager::invalidateWidget(self.type, self.number, self.currentTab + 4);
+    }
+
+    static constexpr WindowEventList kEvents = {
+        .onMouseUp = onMouseUp,
+        .onMouseDown = onMouseDown,
+        .onDropdown = onDropdown,
+        .onUpdate = onUpdate,
+        .prepareDraw = prepareDraw,
+        .draw = draw,
+    };
+
+    // 0x004BF823
+    Window* open()
+    {
+        Window* window = WindowManager::bringToFront(WindowType::musicJukebox);
+        if (window != nullptr)
+        {
+            return window;
+        }
+
+        window = WindowManager::createWindowCentred(
+            WindowType::musicJukebox,
+            kWindowSize,
+            WindowFlags::none,
+            kEvents);
+
+        window->setWidgets(_widgets);
+        window->number = 0;
+        window->currentTab = 0;
+        window->frameNo = 0;
+        window->rowHover = -1;
+        window->object = nullptr;
+
+        auto interface = ObjectManager::get<InterfaceSkinObject>();
+        window->setColour(WindowColour::primary, interface->windowTitlebarColour);
+        window->setColour(WindowColour::secondary, interface->windowOptionsColour);
+
+        window->holdableWidgets = 0;
+        window->eventHandlers = &kEvents;
+        window->activatedWidgets = 0;
+
+        window->callOnResize();
+        window->callPrepareDraw();
+        window->initScrollWidgets();
+
+        return window;
+    }
+}

data/language/en-GB.yml
@@ -2474,3 +2474,6 @@ strings:
   2419: "{SMALLFONT}{COLOUR BLACK}Allows selecting an area of track elements to copy"
   2420: "Paste"
   2421: "{SMALLFONT}{COLOUR BLACK}Allows pasting an area of copied track elements to the map"
+  2422: "Open audio options"
+  2423: "Open jukebox"
+  2424: "Show jukebox window"

src/OpenLoco/CMakeLists.txt
@@ -305,6 +305,7 @@ set(OLOCO_SOURCES
     "${CMAKE_CURRENT_SOURCE_DIR}/src/Ui/Windows/MapToolTip.cpp"
     "${CMAKE_CURRENT_SOURCE_DIR}/src/Ui/Windows/MapWindow.cpp"
     "${CMAKE_CURRENT_SOURCE_DIR}/src/Ui/Windows/MessageWindow.cpp"
+    "${CMAKE_CURRENT_SOURCE_DIR}/src/Ui/Windows/MusicJukebox.cpp"
     "${CMAKE_CURRENT_SOURCE_DIR}/src/Ui/Windows/MusicSelection.cpp"
     "${CMAKE_CURRENT_SOURCE_DIR}/src/Ui/Windows/NetworkStatus.cpp"
     "${CMAKE_CURRENT_SOURCE_DIR}/src/Ui/Windows/News/Common.cpp"

src/OpenLoco/src/Input/Shortcuts.cpp
@@ -511,6 +511,11 @@ namespace OpenLoco::Input::Shortcuts
         Windows::MessageWindow::open();
     }
 
+    static void showJukeboxWindow()
+    {
+        Windows::MusicJukebox::open();
+    }
+
     static void showOptionsWindow()
     {
         Windows::Options::open();
@@ -684,7 +689,6 @@ namespace OpenLoco::Input::Shortcuts
         ShortcutManager::add(Shortcut::closeAllFloatingWindows,         StringIds::shortcut_close_all_floating_windows,         closeAllFloatingWindows,        "closeAllFloatingWindows",          "Left Shift+Backspace");
         ShortcutManager::add(Shortcut::cancelConstructionMode,          StringIds::shortcut_cancel_construction_mode,           cancelConstructionMode,         "cancelConstructionMode",           "Escape");
         ShortcutManager::add(Shortcut::pauseUnpauseGame,                StringIds::shortcut_pause_unpause_game,                 pauseUnpauseGame,               "pauseUnpauseGame",                 "Pause");
-        ShortcutManager::add(Shortcut::showOptionsWindow,               StringIds::shortcut_show_options_window,                showOptionsWindow,              "showOptionsWindow",                "");
         ShortcutManager::add(Shortcut::zoomViewOut,                     StringIds::shortcut_zoom_view_out,                      zoomViewOut,                    "zoomViewOut",                      "PageUp");
         ShortcutManager::add(Shortcut::zoomViewIn,                      StringIds::shortcut_zoom_view_in,                       zoomViewIn,                     "zoomViewIn",                       "PageDown");
         ShortcutManager::add(Shortcut::rotateView,                      StringIds::shortcut_rotate_view,                        rotateView,                     "rotateView",                       "Return");
@@ -717,6 +721,8 @@ namespace OpenLoco::Input::Shortcuts
         ShortcutManager::add(Shortcut::showCompanyInformation,          StringIds::shortcut_show_company_information,           showCompanyInformation,         "showCompanyInformation",           "Q");
         ShortcutManager::add(Shortcut::showFinances,                    StringIds::shortcut_show_finances,                      showFinances,                   "showFinances",                     "F");
         ShortcutManager::add(Shortcut::showAnnouncementsList,           StringIds::shortcut_show_announcements_list,            showAnnouncementsList,          "showAnnouncementsList",            "Tab");
+        ShortcutManager::add(Shortcut::showOptionsWindow,               StringIds::shortcut_show_options_window,                showOptionsWindow,              "showOptionsWindow",                "");
+        ShortcutManager::add(Shortcut::showJukeboxWindow,               StringIds::shortcut_show_jukebox_window,                showJukeboxWindow,              "showJukeboxWindow",                "");
         ShortcutManager::add(Shortcut::screenshot,                      StringIds::shortcut_screenshot,                         makeScreenshot,                 "makeScreenshot",                   "Left Ctrl+S");
         ShortcutManager::add(Shortcut::toggleLastAnnouncement,          StringIds::shortcut_toggle_last_announcement,           toggleLastAnnouncement,         "toggleLastAnnouncement",           "Space");
         ShortcutManager::add(Shortcut::sendMessage,                     StringIds::shortcut_send_message,                       sendMessage,                    "sendMessage",                      "F1");

src/OpenLoco/src/Input/Shortcuts.h
@@ -43,6 +43,7 @@ namespace OpenLoco::Input
         showFinances,
         showAnnouncementsList,
         showOptionsWindow,
+        showJukeboxWindow,
         screenshot,
         toggleLastAnnouncement,
         sendMessage,

src/OpenLoco/src/Localisation/StringIds.h
@@ -1266,7 +1266,7 @@ namespace OpenLoco::StringIds
     constexpr StringId checkmark = 1546;
     constexpr StringId volume = UNUSED_STR(1547);
     constexpr StringId set_music_volume_tip = UNUSED_STR(1548);
-    constexpr StringId menu_music_options = 1549;
+    constexpr StringId menu_music_options = UNUSED_STR(1549);
     constexpr StringId company_face_selection_title = 1550;
     constexpr StringId tooltip_company_face_selection = 1551;
     constexpr StringId already_selected_for_another_company = 1552;
@@ -2083,7 +2083,7 @@ namespace OpenLoco::StringIds
     constexpr StringId tooltip_audio_options = 2369;
     constexpr StringId options_title_audio = 2370;
     constexpr StringId frame_sound = 2371;
-    constexpr StringId frame_jukebox = 2372;
+    constexpr StringId jukebox_window_title = 2372;
     constexpr StringId music_volume = UNUSED_STR(2373);
     constexpr StringId show_company_ai_planning = 2374;
     constexpr StringId show_company_ai_planning_tip = 2375;
@@ -2129,11 +2129,13 @@ namespace OpenLoco::StringIds
     constexpr StringId ambient_volume = 2415;
     constexpr StringId set_ambient_volume_tip = 2416;
     constexpr StringId music_locomotion_title = 2417;
-
     constexpr StringId construction_copy = 2418;
     constexpr StringId construction_copy_tooltip = 2419;
     constexpr StringId construction_paste = 2420;
     constexpr StringId construction_paste_tooltip = 2421;
+    constexpr StringId menu_open_audio_options = 2422;
+    constexpr StringId menu_open_jukebox = 2423;
+    constexpr StringId shortcut_show_jukebox_window = 2424;
 
     constexpr StringId temporary_object_load_str_0 = 8192;
     constexpr StringId temporary_object_load_str_1 = 8193;

src/OpenLoco/src/Ui/WindowManager.h
@@ -253,6 +253,11 @@ namespace OpenLoco::Ui::Windows
         void open();
     }
 
+    namespace MusicJukebox
+    {
+        Window* open();
+    }
+
     namespace MusicSelection
     {
         Window* open();
@@ -290,8 +295,7 @@ namespace OpenLoco::Ui::Windows
     namespace Options
     {
         Window* open();
-        Window* openMusicSettings();
-        constexpr uint8_t kTabOffsetMusic = 2;
+        Window* openAudioSettings();
     }
 
     namespace PlayerInfoPanel

src/OpenLoco/src/Ui/WindowType.h
@@ -53,7 +53,7 @@ namespace OpenLoco::Ui
         landscapeGeneration = 43,
         // 44 was landscapeGenerationConfirm, which is no longer used
         scenarioOptions = 45,
-
+        musicJukebox = 46,
         progressBar = 47,
         companyList = 48,
         tutorial = 49,

commit 63eedc3cadbf1223e26d58be3c8fd7b98fdcfd6d

Address regressions from moving jukebox to its own window (#3716)
When changing jukebox tracks, the game would still invalidate the Options window rather than the Jukebox. This is an oversight from #3707.

The playback buttons would stay pressed/activated even after toggling had already happened. This was due to `.activatedWidgets` not being reset.

Similarly, the 'edit music playlist' button would not correctly toggle away from its disabled state. This was due to `.disabledWidgets` not being reset.

Regressions from #3707

src/OpenLoco/src/Audio/Music.cpp
@@ -68,7 +68,7 @@ namespace OpenLoco::Audio
         {
             const auto& mi = Jukebox::changeTrack();
             playMusic(mi.pathId, cfg.audio.mainVolume, false);
-            Ui::WindowManager::invalidate(Ui::WindowType::options);
+            Ui::WindowManager::invalidate(Ui::WindowType::musicJukebox);
         }
     }
 

src/OpenLoco/src/Ui/Windows/MusicJukebox.cpp
@@ -95,6 +95,9 @@ namespace OpenLoco::Ui::Windows::MusicJukebox
             args.push(songName);
         }
 
+        self.activatedWidgets = 0;
+        self.disabledWidgets = 0;
+
         // Jukebox controls (stop/play/skip)
         if (!SceneManager::isPlayMode())
         {