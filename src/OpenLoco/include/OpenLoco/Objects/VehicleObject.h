6c8f42cf3d39d940645ff9b3b78c0107611adaa5
Aaron van Geffen <aaron@aaronweb.net>
2026-07-16 15:43:06 +0200

    Apply clang-format suggestion

--- src/OpenLoco/include/OpenLoco/Objects/VehicleObject.h
+++ src/OpenLoco/include/OpenLoco/Objects/VehicleObject.h
@@ -202,7 +202,7 @@ namespace OpenLoco
         canReverse = 1U << 8,
         aircraftIsTaildragger = 1U << 8,
         anyRoadType = 1U << 9, // set on all road vehicles except trams
-        flag_10 = 1U << 10, // unused?
+        flag_10 = 1U << 10,    // unused?
         cannotCoupleToSelf = 1U << 11,
         aircraftFlaresLanding = 1U << 11, // set only on Concorde
         mustHavePair = 1U << 12,          // train requires two or more of this vehicle
