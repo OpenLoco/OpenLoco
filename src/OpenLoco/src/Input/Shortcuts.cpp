059195a58097b9c02284c54ca9c5aa5fab9d36c3
Aaron van Geffen <aaron@aaronweb.net>
2026-07-24 14:54:21 +0200

    Swap map shortcut with companies list to group all list windows

--- src/OpenLoco/src/Input/Shortcuts.cpp
+++ src/OpenLoco/src/Input/Shortcuts.cpp
@@ -721,8 +721,8 @@ namespace OpenLoco::Input::Shortcuts
         ShortcutManager::add(Shortcut::showStationsList,                StringIds::shortcut_show_stations_list,                 showStationsList,               "showStationsList",                 "S");
         ShortcutManager::add(Shortcut::showTownsList,                   StringIds::shortcut_show_towns_list,                    showTownsList,                  "showTownsList",                    "U");
         ShortcutManager::add(Shortcut::showIndustriesList,              StringIds::shortcut_show_industries_list,               showIndustriesList,             "showIndustriesList",               "I");
-        ShortcutManager::add(Shortcut::showMap,                         StringIds::shortcut_show_map,                           showMap,                        "showMap",                          "M");
         ShortcutManager::add(Shortcut::showCompaniesList,               StringIds::shortcut_show_companies_list,                showCompaniesList,              "showCompaniesList",                "C");
+        ShortcutManager::add(Shortcut::showMap,                         StringIds::shortcut_show_map,                           showMap,                        "showMap",                          "M");
         ShortcutManager::add(Shortcut::showCompanyInformation,          StringIds::shortcut_show_company_information,           showCompanyInformation,         "showCompanyInformation",           "Q");
         ShortcutManager::add(Shortcut::showFinances,                    StringIds::shortcut_show_finances,                      showFinances,                   "showFinances",                     "F");
         ShortcutManager::add(Shortcut::showAnnouncementsList,           StringIds::shortcut_show_announcements_list,            showAnnouncementsList,          "showAnnouncementsList",            "Tab");
