d6858784965def513522c7f826488d63654bf84e
allensmith81 <v-allens@microsoft.com>
2026-07-24 14:05:57 +0100

    Replace breakpad integration with crashpad and enable ARM64 Windows build path

--- src/Platform/src/Crash.cpp
+++ src/Platform/src/Crash.cpp
@@ -1,83 +1,18 @@
 #include "Crash.h"
 
-#if defined(USE_BREAKPAD)
+#if defined(USE_CRASHPAD)
 #include "Platform.h"
 #include <OpenLoco/Utility/String.hpp>
-#include <ShlObj.h>
-#include <client/windows/handler/exception_handler.h>
+#include <base/files/file_path.h>
+#include <client/crashpad_client.h>
+#include <windows.h>
+#include <map>
+#include <vector>
 #endif
 
 namespace OpenLoco::CrashHandler
 {
-#if defined(USE_BREAKPAD)
-    static AppInfo _appInfo;
-
-    [[maybe_unused]] static bool onCrash(
-        const wchar_t* dumpPath, const wchar_t* miniDumpId,
-        [[maybe_unused]] void* context,
-        [[maybe_unused]] EXCEPTION_POINTERS* exinfo,
-        [[maybe_unused]] MDRawAssertionInfo* assertion,
-        bool succeeded)
-    {
-        if (!succeeded)
-        {
-            constexpr const char* dumpFailedMessage = "Failed to create the dump. Please file an issue with OpenLoco on GitHub and "
-                                                      "provide latest save, and provide "
-                                                      "information about what you did before the crash occurred.";
-            printf("%s\n", dumpFailedMessage);
-            MessageBoxA(nullptr, dumpFailedMessage, "OpenLoco", MB_OK | MB_ICONERROR);
-            return succeeded;
-        }
-        wchar_t dumpFilePath[MAX_PATH];
-        swprintf_s(dumpFilePath, std::size(dumpFilePath), L"%s\\%s.dmp", dumpPath, miniDumpId);
-
-        std::wstring version = Utility::toUtf16(_appInfo.version);
-
-        wchar_t dumpFilePathNew[MAX_PATH];
-        swprintf_s(
-            dumpFilePathNew, std::size(dumpFilePathNew), L"%s\\%s(%s).dmp", dumpPath, miniDumpId, version.c_str());
-
-        // Try to rename the files
-        if (_wrename(dumpFilePath, dumpFilePathNew) == 0)
-        {
-            wcscpy_s(dumpFilePath, dumpFilePathNew);
-        }
-
-        // Log information to output
-        wprintf(L"Dump Path: %s\n", dumpPath);
-        wprintf(L"Dump File Path: %s\n", dumpFilePath);
-        wprintf(L"Dump Id: %s\n", miniDumpId);
-        wprintf(L"Version: %s\n", version.c_str());
-
-        constexpr const wchar_t* MessageFormat = L"A crash has occurred and a dump was created at\n%s.\n\nPlease file an issue "
-                                                 L"with %S on GitHub and provide "
-                                                 L"the dump and most recently saved game there.\n\n\nVersion: %s\n\n";
-        wchar_t message[MAX_PATH * 2];
-        swprintf_s(message, MessageFormat, dumpFilePath, _appInfo.name.c_str(), version.c_str());
-        MessageBoxW(nullptr, message, L"OpenLoco", MB_OK | MB_ICONERROR);
-
-        HRESULT coInitializeResult = CoInitialize(nullptr);
-        if (SUCCEEDED(coInitializeResult))
-        {
-            LPITEMIDLIST pidl = ILCreateFromPathW(dumpPath);
-            LPITEMIDLIST files[6];
-            uint32_t numFiles = 0;
-
-            files[numFiles++] = ILCreateFromPathW(dumpFilePath);
-            if (pidl != nullptr)
-            {
-                SHOpenFolderAndSelectItems(pidl, numFiles, (LPCITEMIDLIST*)files, 0);
-                ILFree(pidl);
-                for (uint32_t i = 0; i < numFiles; i++)
-                {
-                    ILFree(files[i]);
-                }
-            }
-            CoUninitialize();
-        }
-        return succeeded;
-    }
-
+#if defined(USE_CRASHPAD)
     [[maybe_unused]] static std::wstring getDumpDirectory()
     {
         auto crashDir = Platform::getUserDirectory() / "crashes";
@@ -88,18 +23,56 @@ namespace OpenLoco::CrashHandler
         return crashDir.wstring();
     }
 
-#endif // USE_BREAKPAD
+    [[maybe_unused]] static std::wstring getHandlerPath()
+    {
+        wchar_t modulePath[MAX_PATH] = {};
+        auto length = GetModuleFileNameW(nullptr, modulePath, static_cast<DWORD>(std::size(modulePath)));
+        if (length == 0 || length == std::size(modulePath))
+        {
+            return {};
+        }
+        fs::path exePath(modulePath);
+        return (exePath.parent_path() / L"crashpad_handler.exe").wstring();
+    }
+
+#endif // USE_CRASHPAD
 
     Handle init([[maybe_unused]] const AppInfo& appInfo)
     {
-#if !defined(DEBUG) && defined(USE_BREAKPAD)
-        _appInfo = appInfo;
+#if !defined(DEBUG) && defined(USE_CRASHPAD)
+        const auto handlerPath = getHandlerPath();
+        const auto crashDir = getDumpDirectory();
 
-        const auto pipeName = Utility::toUtf16(appInfo.name + "_breakpad");
+        if (handlerPath.empty() || !fs::exists(handlerPath))
+        {
+            wprintf(L"Crashpad handler not found at '%s'\n", handlerPath.c_str());
+            return nullptr;
+        }
 
-        // Path must exist and be RW!
-        auto exHandler = new google_breakpad::ExceptionHandler(
-            getDumpDirectory(), 0, onCrash, 0, google_breakpad::ExceptionHandler::HANDLER_ALL, MiniDumpWithDataSegs, pipeName.c_str(), 0);
+        auto exHandler = new crashpad::CrashpadClient();
+
+        std::map<std::string, std::string> annotations = {
+            { "prod", appInfo.name },
+            { "ver", appInfo.version },
+        };
+
+        const bool started = exHandler->StartHandler(
+            base::FilePath(handlerPath),
+            base::FilePath(crashDir),
+            base::FilePath(crashDir),
+            std::string(),
+            annotations,
+            std::vector<std::string>(),
+            true,
+            true);
+
+        if (!started || !exHandler->WaitForHandlerStart(INFINITE))
+        {
+            wprintf(L"Crashpad failed to start from '%s'\n", handlerPath.c_str());
+            delete exHandler;
+            return nullptr;
+        }
+
         return exHandler;
 #else
         return nullptr;
@@ -108,13 +81,13 @@ namespace OpenLoco::CrashHandler
 
     void shutdown([[maybe_unused]] Handle exHandler)
     {
-#if !defined(DEBUG) && defined(USE_BREAKPAD)
+#if !defined(DEBUG) && defined(USE_CRASHPAD)
         if (exHandler == nullptr)
         {
             return;
         }
 
-        delete static_cast<google_breakpad::ExceptionHandler*>(exHandler);
+        delete static_cast<crashpad::CrashpadClient*>(exHandler);
 #endif
     }
 }
