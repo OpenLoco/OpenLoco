3dae5b0a947dcffb0861d6a5134d9129e06c6647
spacek531 <spacek531@comcast.net>
2026-08-15 13:49:46 -0700

    rename flag_10 to flag10Unused

--- src/OpenLoco/include/OpenLoco/Objects/VehicleObject.h
+++ src/OpenLoco/include/OpenLoco/Objects/VehicleObject.h
@@ -201,8 +201,8 @@ namespace OpenLoco
         alternatingBody = 1U << 7,
         mustFlipOnReverse = 1U << 8,
         aircraftIsTaildragger = 1U << 8,
-        anyRoadType = 1U << 9, // set on all road vehicles except trams
-        flag_10 = 1U << 10,
+        anyRoadType = 1U << 9,       // set on all road vehicles except trams
+        flag10Unused = 1U << 10,
         cannotCoupleToSelf = 1U << 11,
         aircraftFlaresLanding = 1U << 11, // set only on Concorde
         mustHavePair = 1U << 12,          // train requires two or more of this vehicle
