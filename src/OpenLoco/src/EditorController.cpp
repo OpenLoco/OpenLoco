7fd2850f18997ff52dfadd89c67279fa5cab483b
Aaron van Geffen <aaron@aaronweb.net>
2026-08-22 20:58:05 +0200

    Fix out of bounds in case of EditorController::Step::null

--- src/OpenLoco/src/EditorController.cpp
+++ src/OpenLoco/src/EditorController.cpp
@@ -153,6 +153,7 @@ namespace OpenLoco::EditorController
     }
 
     static constexpr auto kStepNames = Utility::buildLookupTable<EditorController::Step, StringId>({
+        { EditorController::Step::null, StringIds::empty },
         { EditorController::Step::objectSelection, StringIds::editor_step_object_selection },
         { EditorController::Step::landscapeEditor, StringIds::editor_step_landscape },
         { EditorController::Step::scenarioOptions, StringIds::editor_step_options },
