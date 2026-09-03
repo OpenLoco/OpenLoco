e5578e567bf637430b41e218beabed14002852ab
Aaron van Geffen <aaron@aaronweb.net>
2026-09-03 19:30:43 +0200

    Adhere to event order in kEvents

--- src/OpenLoco/src/Ui/Windows/ViewClipping.cpp
+++ src/OpenLoco/src/Ui/Windows/ViewClipping.cpp
@@ -1,3 +1,4 @@
+#include "Config.h"
 #include "Graphics/Colour.h"
 #include "Graphics/Gfx.h"
 #include "Graphics/ImageIds.h"
@@ -9,6 +10,7 @@
 #include "Ui/Widgets/CaptionWidget.h"
 #include "Ui/Widgets/FrameWidget.h"
 #include "Ui/Widgets/ImageButtonWidget.h"
+#include "Ui/Widgets/LabelWidget.h"
 #include "Ui/Widgets/PanelWidget.h"
 #include "Ui/Widgets/SliderWidget.h"
 #include "Ui/Window.h"
@@ -23,6 +25,7 @@ namespace OpenLoco::Ui::Windows::ViewClipping
         closeButton,
         panel,
         slider,
+        height,
     };
 
     namespace Widx
@@ -32,6 +35,7 @@ namespace OpenLoco::Ui::Windows::ViewClipping
         constexpr WidgetId kCloseButton{ "close_button" };
         constexpr WidgetId kPanel{ "panel" };
         constexpr WidgetId kSlider{ "slider" };
+        constexpr WidgetId kHeight{ "height" };
     }
 
     static constexpr Ui::Size kWindowSize = { 350, 60 };
@@ -43,9 +47,12 @@ namespace OpenLoco::Ui::Windows::ViewClipping
         Widgets::Caption(Widx::kCaption, { 1, 1 }, { 348, 13 }, Widgets::Caption::Style::whiteText, WindowColour::primary, StringIds::title_view_clipping),
         Widgets::ImageButton(Widx::kCloseButton, { kWindowSize.width - 15, 2 }, { 13, 13 }, WindowColour::primary, ImageIds::close_button, StringIds::tooltip_close_window),
         Widgets::Panel(Widx::kPanel, { 0, 15 }, { 350, kWindowSize.height - 15 }, WindowColour::secondary),
-        Widgets::Slider(Widx::kSlider, { (kWindowSize.width - kSliderSize.width) / 2, 25 }, kSliderSize, WindowColour::secondary)
+        Widgets::Slider(Widx::kSlider, { 5, 25 }, kSliderSize, WindowColour::secondary),
+        Widgets::Label(Widx::kHeight, { 5 + kSliderSize.width + 5, 25 }, { 100, 10 }, WindowColour::secondary, ContentAlign::left, StringIds::stringptr)
 
     );
+
+    static char _heightLabel[100];
 
     static const WindowEventList& getEvents();
 
@@ -71,12 +78,36 @@ namespace OpenLoco::Ui::Windows::ViewClipping
         return window;
     }
 
+    static void updateHeightLabel(Window& self)
+    {
+        auto height = getMaxClipHeight() - 128;
+
+        auto& config = Config::get();
+        if (config.showHeightAsUnits)
+        {
+            std::snprintf(_heightLabel, sizeof(_heightLabel), "%d units", height);
+        }
+        else if (config.measurementFormat == Config::MeasurementFormat::imperial)
+        {
+            std::snprintf(_heightLabel, sizeof(_heightLabel), "%d ft", height * 16);
+        }
+        else if (config.measurementFormat == Config::MeasurementFormat::metric)
+        {
+            std::snprintf(_heightLabel, sizeof(_heightLabel), "%d m", height * 5);
+        }
+
+        FormatArguments args{ self.widgets[widx::height].textArgs };
+        args.push(_heightLabel);
+    }
+
     static void prepareDraw(Window& self)
     {
         auto sliderWidth = self.widgets[widx::slider].width() - 5;
         auto height = getMaxClipHeight() * sliderWidth / 255;
 
         self.widgets[widx::slider].content = height;
+
+        updateHeightLabel(self);
     }
 
     static void draw(Window& self, Gfx::DrawingContext& drawingCtx)
@@ -110,10 +141,10 @@ namespace OpenLoco::Ui::Windows::ViewClipping
     }
 
     static constexpr WindowEventList kEvents = {
+        .onMouseUp = onMouseUp,
+        .onMouseDown = onMouseDown,
         .prepareDraw = prepareDraw,
         .draw = draw,
-        .onMouseDown = onMouseDown,
-        .onMouseUp = onMouseUp,
     };
 
     static const WindowEventList& getEvents()
