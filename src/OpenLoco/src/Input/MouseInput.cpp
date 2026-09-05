dc7807dc4ec52da9f407c6bce9f5adf36078291c
Aaron van Geffen <aaron@aaronweb.net>
2026-09-05 11:44:21 +0200

    Keep map tooltip open while map selection tool is active

--- src/OpenLoco/src/Input/MouseInput.cpp
+++ src/OpenLoco/src/Input/MouseInput.cpp
@@ -1643,6 +1643,7 @@ namespace OpenLoco::Input
                 const auto area = World::getMapSelectionArea();
                 const auto length = std::abs(area.first.x - area.second.x) / 32 + 1;
                 const auto depth = std::abs(area.first.y - area.second.y) / 32 + 1;
+
                 auto args = FormatArguments::mapToolTip();
                 args.push<StringId>(StringIds::map_tooltip_int_x_int);
                 args.push<int16_t>(length);
