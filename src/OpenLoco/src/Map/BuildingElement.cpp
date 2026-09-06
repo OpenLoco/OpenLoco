add6e39f2c982114c375ff831682fb444ac5f6da
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2026-09-06 16:40:04 +0100

    Fix crash on corrupted saves when showing map tooltips of buildings

--- src/OpenLoco/src/Map/BuildingElement.cpp
+++ src/OpenLoco/src/Map/BuildingElement.cpp
@@ -172,6 +172,10 @@ namespace OpenLoco::World
             const uint8_t randArr[2] = { static_cast<uint8_t>(randVal), static_cast<uint8_t>(randVal >> 8) };
             for (auto i = 0; i < 2; ++i)
             {
+                if (buildingObj->producedCargoType[i] == 0xFFU)
+                {
+                    continue;
+                }
                 if (randArr[i] >= buildingObj->producedQuantity[i])
                 {
                     continue;
